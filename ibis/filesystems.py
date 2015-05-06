2a3a1a0ad292956c8de381588f6dc33ac95069c4
Wes McKinney <wes@cloudera.com>
2015-05-06 15:24:02 -0700

    Start implementing HDFS wrapper for webhdfs 3rd party lib, add CSV test data loading script

--- ibis/filesystems.py
+++ ibis/filesystems.py
@@ -12,34 +12,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hdfs
 
-class HDFSInterface(object):
+
+class HDFS(object):
 
     """
-
+    Interface class to HDFS for ibis that abstracts away (and protects
+    user/developer against) various 3rd party library API differences.
     """
 
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, host, port, params=None, protocol='webhdfs'):
+        self.host = host
+        self.port = port
+        self.params = params
+        self.protocol = protocol
+
+        if self.protocol == 'webhdfs':
+            url = 'http://{}:{}'.format(self.host, self.port)
+            self.client = hdfs.client.Client(url, params=self.params)
+        else:
+            raise NotImplementedError
+
+    def path_exists(self, path):
+        try:
+            self.client.status(path)
+            return True
+        except Exception:
+            return False
+
+    def ls(self, hdfs_path):
+        if self.protocol == 'webhdfs':
+            return self.client.list(hdfs_path)
 
     def delete(self, hdfs_path):
         pass
 
-    def put(self, local_path, hdfs_path):
+    def head(self, hdfs_path, nbytes=1024, offset=0):
+        if self.protocol == 'webhdfs':
+            gen = self.client.read(hdfs_path, offset=offset, length=nbytes)
+            return ''.join(gen)
+
+    def tail(self, hdfs_path, nbytes=1024):
+        raise NotImplementedError
+
+    def get(self, hdfs_path, local_path, overwrite=False):
+        """
+        Download a file from HDFS to local
+        """
         pass
+
+    def put(self, hdfs_path, local_path, overwrite=False, **kwargs):
+        """
+
+        Parameters
+        ----------
+
+        Other keywords forwarded to .write API.
+        """
+        if self.protocol == 'webhdfs':
+            self.client.upload(hdfs_path, local_path,
+                               overwrite=overwrite, **kwargs)
+
+    def write(self, hdfs_path, buf, overwrite=False, blocksize=None,
+              replication=None, buffersize=None):
+        """
+        Write a buffer-like object to indicated HDFS path
+
+        Parameters
+        ----------
+        """
+        if self.protocol == 'webhdfs':
+            self.client.write(buf, hdfs_path, overwrite=overwrite,
+                              blocksize=blocksize, replication=replication,
+                              buffersize=buffersize)
 
     def mkdir(self, path, create_parent=False):
         pass
 
     def rmdir(self, path):
-        pass
+        self.client.delete(path, recursive=True)
 
 
 
 def connect(host, port=8020, hadoop_version=9):
     from snakebite.client import Client
     client = Client(host, port=port, hadoop_version=hadoop_version)
-    return HDFSInterface(client)
+    return HDFS(client)
 
 
 def connect_ha(namenodes):
