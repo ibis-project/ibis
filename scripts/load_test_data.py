11429476de2bffae53dee77ee1a8e78cacec64e7
Wes McKinney <wes@cloudera.com>
2015-06-12 14:01:48 -0700

    Add code for generating the test data folder, too

--- scripts/load_test_data.py
+++ scripts/load_test_data.py
@@ -15,8 +15,11 @@
 # Populates the ibis_testing Impala database
 
 from posixpath import join as pjoin
+import os
 import posixpath
+import shutil
 
+from ibis.util import guid
 import ibis
 
 IMPALA_HOST = 'localhost'
@@ -33,6 +36,89 @@ def make_connection():
     con = ibis.make_client(ic, hdfs_client=hdfs)
 
     return con
+
+# ----------------------------------------------------------------------
+# Functions for creating the test data archive to begin with
+
+TMP_DB_LOCATION = '/__ibis/{0}'.format(guid())
+TMP_DB = guid()
+
+def make_temp_database(con):
+    if con.exists_database(TMP_DB):
+        con.drop_database(TMP_DB, drop_tables=True)
+    con.create_database(TMP_DB, path=TMP_DB_LOCATION)
+    print('Created database {0} at {1}'.format(TMP_DB, TMP_DB_LOCATION))
+
+
+def cleanup_temporary_stuff(con):
+    con.drop_database(TMP_DB, drop_tables=True)
+    assert not con.hdfs.exists(TMP_DB_LOCATION)
+
+def download_parquet_files(con):
+    parquet_path = pjoin(TEST_DATA_DIR, 'parquet')
+    print("Downloading {0}".format(parquet_path))
+    con.hdfs.get(TMP_DB_LOCATION, parquet_path)
+
+
+def download_avro_files(con):
+    avro_path = '/test-warehouse/tpch.region_avro'
+    os.mkdir(os.path.join(TEST_DATA_DIR, 'avro'))
+    print("Downloading {0}".format(avro_path))
+    con.hdfs.get(avro_path, pjoin(TEST_DATA_DIR, 'avro', 'tpch.region'))
+
+
+def generate_csv_files():
+    import numpy as np
+    import pandas as pd
+    import pandas.util.testing as tm
+
+    N = 10
+    nfiles = 10
+
+    csv_base = os.path.join(TEST_DATA_DIR, 'csv')
+    os.mkdir(csv_base)
+
+    df = pd.DataFrame({
+        'foo': [tm.rands(10) for _ in xrange(N)],
+        'bar': np.random.randn(N),
+        'baz': np.random.randint(0, 100, size=N)
+    }, columns=['foo', 'bar', 'baz'])
+
+    for i in xrange(nfiles):
+        csv_path = os.path.join(csv_base, '{}.csv'.format(i))
+        print('Writing {}'.format(csv_path))
+        df.to_csv(csv_path, index=False, header=False)
+
+
+def scrape_parquet_files(con):
+    to_scrape = [('tpch', x) for x in con.list_tables(database='tpch')]
+    to_scrape.append(('functional', 'alltypes'))
+    for db, tname in to_scrape:
+        table = con.table(tname, database=db)
+        new_name = '{}_{}'.format(db, tname)
+        print('Creating {}'.format(new_name))
+        con.create_table(new_name, table, database=TMP_DB)
+
+
+def make_local_test_archive():
+    con = make_connection()
+    make_temp_database(con)
+
+    try:
+        scrape_parquet_files(con)
+
+        if os.path.exists(TEST_DATA_DIR):
+            shutil.rmtree(TEST_DATA_DIR)
+        os.mkdir(TEST_DATA_DIR)
+
+        download_parquet_files(con)
+        download_avro_files(con)
+        generate_csv_files()
+    finally:
+        cleanup_temporary_stuff(con)
+
+# ----------------------------------------------------------------------
+#
 
 
 def write_data_to_hdfs(con):
@@ -57,8 +143,12 @@ def create_parquet_tables(con):
         con.parquet_file(path, name=table_name, database=TEST_DB, persist=True)
 
 
-if __name__ == '__main__':
+def setup_test_data():
     con = make_connection()
     write_data_to_hdfs(con)
     create_test_database(con)
     create_parquet_tables(con)
+
+
+if __name__ == '__main__':
+    setup_test_data()
