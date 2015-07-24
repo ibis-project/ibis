6792eb304f4cfcb61fe89a484880632e3e0defad
Wes McKinney <wes@cloudera.com>
2015-07-24 12:05:49 -0700

    Adapt merge-pr script for upstream main branch and no authorization
    Author: Wes McKinney <wes@cloudera.com>
    
    Closes #455 from wesm/merge-pr-upstream and squashes the following commits:
    
    9419724 [Wes McKinney] Leave authorization code in merge-pr in case ever needed
    ce1167a [Wes McKinney] DEV: Reference upstream instead of origin in merge-pr
    86ee41e [Wes McKinney] DEV: Do not require authorization in merge-py.py

--- dev/merge-pr.py
+++ dev/merge-pr.py
@@ -37,10 +37,10 @@ PROJECT_NAME = 'ibis'
 print "IBIS_HOME = " + IBIS_HOME
 
 # Remote name with the PR
-PR_REMOTE_NAME = os.environ.get("PR_REMOTE_NAME", "origin")
+PR_REMOTE_NAME = os.environ.get("PR_REMOTE_NAME", "upstream")
 
 # Remote name where results pushed
-PUSH_REMOTE_NAME = os.environ.get("PUSH_REMOTE_NAME", "origin")
+PUSH_REMOTE_NAME = os.environ.get("PUSH_REMOTE_NAME", "upstream")
 
 GITHUB_BASE = "https://github.com/cloudera/" + PROJECT_NAME + "/pull"
 GITHUB_API_BASE = "https://api.github.com/repos/cloudera/" + PROJECT_NAME
@@ -50,15 +50,26 @@ BRANCH_PREFIX = "PR_TOOL"
 
 os.chdir(IBIS_HOME)
 
-GITHUB_USERNAME = os.environ['GITHUB_USER']
-import getpass
-GITHUB_PASSWORD = getpass.getpass('Enter github.com password for %s:'
-                                  % GITHUB_USERNAME)
+auth_required = False
 
-def get_json(url):
-    auth = HTTPBasicAuth(GITHUB_USERNAME, GITHUB_PASSWORD)
-    req = requests.get(url, auth=auth)
-    return req.json()
+if auth_required:
+    GITHUB_USERNAME = os.environ['GITHUB_USER']
+    import getpass
+    GITHUB_PASSWORD = getpass.getpass('Enter github.com password for %s:'
+                                      % GITHUB_USERNAME)
+
+    def get_json_auth(url):
+        auth = HTTPBasicAuth(GITHUB_USERNAME, GITHUB_PASSWORD)
+        req = requests.get(url, auth=auth)
+        return req.json()
+
+    get_json = get_json_auth
+else:
+    def get_json_no_auth(url):
+        req = requests.get(url)
+        return req.json()
+
+    get_json = get_json_no_auth
 
 
 def fail(msg):
