ebded30ca40d72fda49f80f63d446b35b6d02dcc
Phillip Cloud <cpcloud@gmail.com>
2016-10-02 15:15:56 -0700

    Make sure child contexts have extracted refs

--- ibis/sql/compiler.py
+++ ibis/sql/compiler.py
@@ -147,12 +147,17 @@ class SelectBuilder(object):
         node = expr.op()
         if isinstance(node, ops.Join):
             for arg in node.args:
-                if not isinstance(arg, ir.TableExpr):
-                    continue
-                self._make_table_aliases(arg)
+                if isinstance(arg, ir.TableExpr):
+                    self._make_table_aliases(arg)
         else:
             if not ctx.is_extracted(expr):
                 ctx.make_alias(expr)
+            else:
+                # The compiler will apply a prefix only if the current context
+                # contains two or more table references. So, if we've extracted
+                # a subquery into a CTE, we need to propagate that reference
+                # down to child contexts so that they aren't missing any refs.
+                ctx.set_ref(expr, ctx.top_context.get_ref(expr))
 
     # ---------------------------------------------------------------------
     # Expr analysis / rewrites
