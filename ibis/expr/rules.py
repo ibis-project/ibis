ffef0c15d58b1831b961c4b3fa31f74d116f6a7f
Phillip Cloud <cpcloud@gmail.com>
2016-11-13 11:23:39 -0800

    Raise type error instead of value error for failed implicit casting

--- ibis/expr/rules.py
+++ ibis/expr/rules.py
@@ -143,6 +143,9 @@ def highest_precedence_type(exprs):
     # Return the highest precedence type from the passed expressions. Also
     # verifies that there are valid implicit casts between any of the types and
     # the selected highest precedence type
+    if not exprs:
+        raise ValueError('Must pass at least one expression')
+
     type_counts = Counter(expr.type() for expr in exprs)
     scores = (
         (_TYPE_PRECEDENCE[k.name.lower()], k) for k, v in type_counts.items()
@@ -151,7 +154,7 @@ def highest_precedence_type(exprs):
 
     for expr in exprs:
         if not expr._can_cast_implicit(highest_type):
-            raise ValueError(
+            raise TypeError(
                 'Expression with type {0} cannot be implicitly casted to {1}'
                 .format(expr.type(), highest_type)
             )
