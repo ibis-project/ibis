eedbc73cc61db9e29dca75cd23dd9575ec33a159
Phillip Cloud <cpcloud@gmail.com>
2017-01-01 13:47:45 -0800

    Pluck out value_type

--- ibis/expr/datatypes.py
+++ ibis/expr/datatypes.py
@@ -309,13 +309,15 @@ def parametric(cls):
     def array_type(self):
         def constructor(op, name=None):
             import ibis.expr.types as ir
-            return getattr(ir, array_type_name)(op, self, name=name)
+            return getattr(ir, array_type_name)(op, self.value_type, name=name)
         return constructor
 
     def scalar_type(self):
         def constructor(op, name=None):
             import ibis.expr.types as ir
-            return getattr(ir, scalar_type_name)(op, self, name=name)
+            return getattr(ir, scalar_type_name)(
+                op, self.value_type, name=name
+            )
         return constructor
 
     cls.array_type = array_type
