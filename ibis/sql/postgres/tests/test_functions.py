7e30b93e484da45327aef267133ba949c832d58a
Phillip Cloud <cpcloud@gmail.com>
2017-01-27 21:20:38 -0500

    Implement SEMI JOIN and ANTI JOIN for SQLAlchemy
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #903 from cpcloud/anti-semi-alchemy and squashes the following commits:
    
    70dbd1d [Phillip Cloud] Importing os would help
    a91ee1b [Phillip Cloud] Union appears to be working
    cf26df2 [Phillip Cloud] Assert that the frame is empty
    a6bef8e [Phillip Cloud] Fill out test cases
    35d058f [Phillip Cloud] Remove bogus pg implementations of drop and create
    59a77f2 [Phillip Cloud] Test skeletons
    ea2a9fb [Phillip Cloud] Implement create, truncate, drop and anti and semi join for sqlalchemy

--- ibis/sql/postgres/tests/test_functions.py
+++ ibis/sql/postgres/tests/test_functions.py
@@ -14,6 +14,8 @@
 
 import math
 import os
+import uuid
+import os
 
 import pytest  # noqa
 
@@ -412,10 +414,6 @@ class TestPostgreSQLFunctions(PostgreSQLTests, unittest.TestCase):
         ]
         self._execute_projection(t, exprs)
 
-    @pytest.mark.xfail(
-        raises=sa.exc.ProgrammingError,
-        reason='union not working yet'
-    )
     def test_union(self):
         t = self.alltypes
 
@@ -779,3 +777,94 @@ def test_array_concat(array_types, catop):
 def test_array_concat_mixed_types(array_types):
     with pytest.raises(TypeError):
         array_types.x + array_types.x.cast('array<double>')
+
+
+@pytest.yield_fixture
+def t(con):
+    name = 'left_t'
+    con.raw_sql(
+        """
+        CREATE TABLE {} (
+          id SERIAL PRIMARY KEY,
+          name TEXT
+        )
+        """.format(name)
+    )
+    try:
+        yield con.table(name)
+    finally:
+        con.drop_table(name)
+
+
+@pytest.yield_fixture
+def s(con, t):
+    name = 'right_t'
+    con.raw_sql(
+        """
+        CREATE TABLE {} (
+          id SERIAL PRIMARY KEY,
+          left_t_id INTEGER REFERENCES left_t,
+          cost DOUBLE PRECISION
+        )
+        """.format(name)
+    )
+    try:
+        yield con.table(name)
+    finally:
+        con.drop_table(name)
+
+
+@pytest.yield_fixture
+def trunc(con):
+    name = str(uuid.uuid1())
+    con.raw_sql(
+        """
+        CREATE TABLE "{}" (
+          id SERIAL PRIMARY KEY,
+          name TEXT
+        )
+        """.format(name)
+    )
+    con.raw_sql(
+        """INSERT INTO "{}" (name) VALUES ('a'), ('b'), ('c')""".format(name)
+    )
+    try:
+        yield con.table(name)
+    finally:
+        con.drop_table(name)
+
+
+def test_semi_join(t, s):
+    t_a, s_a = t.op().sqla_table.alias('t0'), s.op().sqla_table.alias('t1')
+    expr = t.semi_join(s, t.id == s.id)
+    result = expr.compile().compile(compile_kwargs=dict(literal_binds=True))
+    base = sa.select([t_a.c.id, t_a.c.name]).where(
+        sa.exists(sa.select([1]).where(t_a.c.id == s_a.c.id))
+    )
+    expected = sa.select([base.c.id, base.c.name])
+    assert str(result) == str(expected)
+
+
+def test_anti_join(t, s):
+    t_a, s_a = t.op().sqla_table.alias('t0'), s.op().sqla_table.alias('t1')
+    expr = t.anti_join(s, t.id == s.id)
+    result = expr.compile().compile(compile_kwargs=dict(literal_binds=True))
+    expected = sa.select([sa.column('id'), sa.column('name')]).select_from(
+        sa.select([t_a.c.id, t_a.c.name]).where(
+            ~sa.exists(sa.select([1]).where(t_a.c.id == s_a.c.id))
+        )
+    )
+    assert str(result) == str(expected)
+
+
+def test_create_table(con, trunc):
+    name = str(uuid.uuid1())
+    con.create_table(name, expr=trunc)
+    t = con.table(name)
+    assert list(t.name.execute()) == list('abc')
+
+
+def test_truncate_table(con, trunc):
+    assert list(trunc.name.execute()) == list('abc')
+    con.truncate_table(trunc.op().name)
+    assert not len(trunc.execute())
