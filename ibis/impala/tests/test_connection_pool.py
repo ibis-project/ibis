0b5c59d312034ed659894eafbd15b7e954859906
Phillip Cloud <cpcloud@gmail.com>
2017-02-26 18:28:13 -0500

    Add tests for connection pool accounting

---
+++ ibis/impala/tests/test_connection_pool.py
@@ -0,0 +1,48 @@
+import pytest
+
+pytest.importorskip('sqlalchemy')
+pytest.importorskip('impala.dbapi')
+
+import ibis
+from ibis.impala.tests.common import IbisTestEnv
+
+
+@pytest.fixture
+def env():
+    return IbisTestEnv()
+
+
+@pytest.mark.impala
+def test_connection_pool_size(env):
+    hdfs_client = ibis.hdfs_connect(
+        host=env.nn_host,
+        port=int(env.webhdfs_port),
+        auth_mechanism=env.auth_mechanism,
+        user=env.webhdfs_user,
+    )
+    client = ibis.impala.connect(
+        port=int(env.impala_port),
+        hdfs_client=hdfs_client,
+        host=env.impala_host,
+        database=env.test_data_db,
+    )
+    assert client.con.connection_pool_size == 1
+
+
+@pytest.mark.impala
+def test_connection_pool_size_after_close(env):
+    hdfs_client = ibis.hdfs_connect(
+        host=env.nn_host,
+        port=int(env.webhdfs_port),
+        auth_mechanism=env.auth_mechanism,
+        user=env.webhdfs_user,
+    )
+    client = ibis.impala.connect(
+        port=int(env.impala_port),
+        hdfs_client=hdfs_client,
+        host=env.impala_host,
+        database=env.test_data_db,
+    )
+    client.close()
+    assert client.con.connection_pool_size == 0
+
