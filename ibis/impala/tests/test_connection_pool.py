15ceb71bc4a6049c4ace8f45cddf7871f9ceeb6b
Phillip Cloud <cpcloud@gmail.com>
2017-02-26 18:31:10 -0500

    Reuse hdfs_client

--- ibis/impala/tests/test_connection_pool.py
+++ ibis/impala/tests/test_connection_pool.py
@@ -12,14 +12,18 @@ def env():
     return IbisTestEnv()
 
 
-@pytest.mark.impala
-def test_connection_pool_size(env):
-    hdfs_client = ibis.hdfs_connect(
+@pytest.fixture
+def hdfs_client(env):
+    return ibis.hdfs_connect(
         host=env.nn_host,
         port=int(env.webhdfs_port),
         auth_mechanism=env.auth_mechanism,
         user=env.webhdfs_user,
     )
+
+
+@pytest.mark.impala
+def test_connection_pool_size(env, hdfs_client):
     client = ibis.impala.connect(
         port=int(env.impala_port),
         hdfs_client=hdfs_client,
@@ -30,13 +34,7 @@ def test_connection_pool_size(env):
 
 
 @pytest.mark.impala
-def test_connection_pool_size_after_close(env):
-    hdfs_client = ibis.hdfs_connect(
-        host=env.nn_host,
-        port=int(env.webhdfs_port),
-        auth_mechanism=env.auth_mechanism,
-        user=env.webhdfs_user,
-    )
+def test_connection_pool_size_after_close(env, hdfs_client):
     client = ibis.impala.connect(
         port=int(env.impala_port),
         hdfs_client=hdfs_client,
