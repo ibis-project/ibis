5e7e520c3f79132ae1f1a5f8ff72437eb3676fae
Phillip Cloud <cpcloud@gmail.com>
2017-05-22 16:10:13 -0400

    Add support for connecting to different databases with PostgreSQL
    @mariusvniekerk Here's one implementation of connecting to multiple
    databases with postgres.
    
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #987 from cpcloud/pg-multi-db and squashes the following commits:
    
    f1cedec [Phillip Cloud] Fix doctest
    98fee21 [Phillip Cloud] Use an absolute import
    7d3571d [Phillip Cloud] Add simple doctest examples
    cb23bc3 [Phillip Cloud] Cleaner URL constructor call
    1d81d85 [Phillip Cloud] Use URL and add multiple database functionality

--- ibis/sql/postgres/client.py
+++ ibis/sql/postgres/client.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import getpass
+
 import sqlalchemy as sa
 
 from ibis.client import Database
-from .compiler import PostgreSQLDialect
+from ibis.sql.postgres.compiler import PostgreSQLDialect
 import ibis.expr.types as ir
 import ibis.sql.alchemy as alch
 
@@ -30,45 +32,80 @@ class PostgreSQLDatabase(Database):
 
 class PostgreSQLClient(alch.AlchemyClient):
 
-    """
-    The Ibis PostgreSQL client class
-    """
+    """The Ibis PostgreSQL client class"""
 
     dialect = PostgreSQLDialect
     database_class = PostgreSQLDatabase
+    default_database_name = 'public'
 
-    def __init__(self, host=None, user=None, password=None, port=None,
-                 database=None, url=None, driver=None):
+    def __init__(
+        self,
+        host=None,
+        user=None,
+        password=None,
+        port=None,
+        database=None,
+        url=None,
+        driver=None
+    ):
         if url is None:
-            if user is not None:
-                if password is None:
-                    userpass = user
-                else:
-                    userpass = '{0}:{1}'.format(user, password)
-
-                address = '{0}@{1}'.format(userpass, host)
-            else:
-                address = host
-
-            if port is not None:
-                address = '{0}:{1}'.format(address, port)
-
-            if database is not None:
-                address = '{0}/{1}'.format(address, database)
-
             if driver is not None and driver != 'psycopg2':
-                raise NotImplementedError(driver)
-
-            url = 'postgresql://{0}'.format(address)
-
-        url = sa.engine.url.make_url(url)
+                raise NotImplementedError(
+                    'psycopg2 is currently the only supported driver'
+                )
+            url = sa.engine.url.URL(
+                'postgresql+psycopg2',
+                username=user or getpass.getuser(),
+                password=password,
+                host=host or 'localhost',
+                port=port,
+                database=database or self.__class__.default_database_name,
+            )
+        else:
+            url = sa.engine.url.make_url(url)
         self.name = url.database
-        self.database_name = 'public'
+        self.database_name = self.__class__.default_database_name
         self.con = sa.create_engine(url)
         self.meta = sa.MetaData(bind=self.con)
 
+    def database(self, name=None):
+        """Connect to a database called `name`.
+
+        Parameters
+        ----------
+        name : str, optional
+            The name of the database to connect to. If ``None``, return the
+            database named ``self.current_database``.
+
+        Returns
+        -------
+        db : Database
+            An :class:`ibis.client.Database` instance.
+
+        Notes
+        -----
+        This creates a new connection if `name` is both not ``None`` and not
+        equal to the current database.
+        """
+        if name == self.current_database or (
+            name is None and name != self.current_database
+        ):
+            return self.database_class(self.current_database, self)
+        else:
+            url = self.con.url
+            client_class = type(self)
+            new_client = client_class(
+                host=url.host,
+                user=url.username,
+                port=url.port,
+                password=url.password,
+                database=name,
+            )
+            return self.database_class(name, new_client)
+
     @property
     def current_database(self):
+        """The name of the current database this client is connected to."""
         return self.database_name
 
     def list_databases(self):
@@ -79,29 +116,39 @@ class PostgreSQLClient(alch.AlchemyClient):
             )
         ]
 
-    def set_database(self):
-        raise NotImplementedError
+    def set_database(self, name):
+        raise NotImplementedError(
+            'Cannot set database with PostgreSQL client. To use a different'
+            ' database, use client.database({!r})'.format(name)
+        )
 
     @property
     def client(self):
         return self
 
     def table(self, name, database=None):
-        """
-        Create a table expression that references a particular table in the
-        PostgreSQL database
+        """Create a table expression that references a particular a table
+        called `name` in a PostgreSQL database called `database`.
 
         Parameters
         ----------
-        name : string
+        name : str
+            The name of the table to retrieve.
+        database : str, optional
+            The database in which the table referred to by `name` resides. If
+            ``None`` then the ``current_database`` is used.
 
         Returns
         -------
         table : TableExpr
+            A table expression.
         """
-        alch_table = self._get_sqla_table(name)
-        node = PostgreSQLTable(alch_table, self)
-        return self._table_expr_klass(node)
+        if database is not None and database != self.current_database:
+            return self.database(name=database).table(name=name)
+        else:
+            alch_table = self._get_sqla_table(name)
+            node = PostgreSQLTable(alch_table, self)
+            return self._table_expr_klass(node)
 
     @property
     def _table_expr_klass(self):
