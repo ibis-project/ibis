073d7b8b15864b6bc5fc19e653e565790d969cbd
Phillip Cloud <cpcloud@gmail.com>
2017-06-12 14:03:18 -0400

    TST: Test that there are no ambiguous dispatch definitions

---
+++ ibis/pandas/tests/test_core.py
@@ -0,0 +1,11 @@
+import pytest
+
+pytest.importorskip('multipledispatch')
+
+from ibis.pandas.execution import execute, execute_node  # noqa: E402
+from multipledispatch.conflict import ambiguities  # noqa: E402
+
+
+@pytest.mark.parametrize('func', [execute, execute_node])
+def test_no_execute_ambiguities(func):
+    assert not ambiguities(func.funcs)
