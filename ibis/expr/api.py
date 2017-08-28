236ead879a94f5841ef959348d0444af2326a9ae
Phillip Cloud <cpcloud@gmail.com>
2017-08-28 16:53:55 -0400

    Add comment [ci skip]

--- ibis/expr/api.py
+++ ibis/expr/api.py
@@ -1884,6 +1884,12 @@ def between_time(arg, lower, upper, timezone=None):
     """
 
     if isinstance(arg.op(), _ops.Time):
+        # Here we pull out the first argument to the underlying Time operation
+        # which is by definition (in _timestamp_value_methods) a
+        # TimestampValue. We do this so that we can potentially specialize the
+        # "between time" operation for timestamp_value_expr.time().between().
+        # A similar mechanism is triggered when creating expressions like
+        # t.column.distinct().count(), which is turned into t.column.nunique().
         arg = arg.op().args[0]
         if timezone is not None:
             arg = arg.cast(dt.Timestamp(timezone=timezone))
