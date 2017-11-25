437feaa7d05e1b148618f3b8e9e72cf85a36bb67
Phillip Cloud <cpcloud@gmail.com>
2017-11-25 10:55:13 -0500

    Fix imports

--- ibis/tests/all/__init__.py
+++ ibis/tests/all/__init__.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 
-from ibis.tests.all.impala import Impala
-from ibis.tests.all.sqlite import SQLite
-from ibis.tests.all.postgres import Postgres
-from ibis.tests.all.clickhouse import Clickhouse
-from ibis.tests.all.bigquery import BigQuery
-from ibis.tests.all.pandas import Pandas
-from ibis.tests.all.csv import CSV
-from ibis.tests.all.hdf5 import HDF5
-from ibis.tests.all.parquet import Parquet
+from ibis.tests.all.config.impala import Impala
+from ibis.tests.all.config.sqlite import SQLite
+from ibis.tests.all.config.postgres import Postgres
+from ibis.tests.all.config.clickhouse import Clickhouse
+from ibis.tests.all.config.bigquery import BigQuery
+from ibis.tests.all.config.pandas import Pandas
+from ibis.tests.all.config.csv import CSV
+from ibis.tests.all.config.hdf5 import HDF5
+from ibis.tests.all.config.parquet import Parquet
 
 backends = (
     Impala,
