32dd3ad7ba2b4ebe04235287caac7a16b37d1ad4
Phillip Cloud <cpcloud@gmail.com>
2017-11-25 15:24:53 -0500

    Fix bigquery ops

--- ibis/bigquery/compiler.py
+++ ibis/bigquery/compiler.py
@@ -2,7 +2,7 @@ import ibis.expr.types as ir
 
 import ibis.sql.compiler as comp
 import ibis.expr.operations as ops
-from ibis.impala.compiler import ImpalaSelect
+from ibis.impala.compiler import ImpalaSelect, unary, fixed_arity
 from ibis.impala import compiler as impala_compiler
 
 
@@ -70,10 +70,6 @@ def _extract_field(sql_attr):
     return extract_field_formatter
 
 
-def _ifnull(translator, expr):
-    return 'IFNULL({}, {})'.format(*map(translator.translate, expr.op().args))
-
-
 _sql_type_names = {
     'int8': 'int64',
     'int16': 'int64',
@@ -102,10 +98,6 @@ def _struct_field(translator, expr):
     return '{}.`{}`'.format(arg_formatted, field)
 
 
-def _array_collect(translator, expr):
-    return 'ARRAY_AGG({})'.format(*map(translator.translate, expr.op().args))
-
-
 def _array_concat(translator, expr):
     return 'ARRAY_CONCAT({})'.format(
         ', '.join(map(translator.translate, expr.op().args))
@@ -115,12 +107,6 @@ def _array_concat(translator, expr):
 def _array_index(translator, expr):
     # SAFE_OFFSET returns NULL if out of bounds
     return '{}[SAFE_OFFSET({})]'.format(
-        *map(translator.translate, expr.op().args)
-    )
-
-
-def _array_length(translator, expr):
-    return 'ARRAY_LENGTH({})'.format(
         *map(translator.translate, expr.op().args)
     )
 
@@ -174,22 +160,6 @@ def _regex_replace(translator, expr):
     return result
 
 
-def _string_repeat(translator, expr):
-    return 'REPEAT({}, {})'.format(*map(translator.translate, expr.op().args))
-
-
-def _string_replace(translator, expr):
-    return 'REPLACE({}, {}, {})'.format(
-        *map(translator.translate, expr.op().args)
-    )
-
-
-def _string_split(translator, expr):
-    return 'SPLIT({}, {})'.format(
-        *map(translator.translate, expr.op().args)
-    )
-
-
 def _string_concat(translator, expr):
     return 'CONCAT({})'.format(
         ', '.join(map(translator.translate, expr.op().args))
@@ -204,10 +174,18 @@ def _string_join(translator, expr):
     )
 
 
-def _group_concat(translator, expr):
-    arg, sep = expr.op().args
-    return 'STRING_AGG({}, {})'.format(
-        translator.translate(arg), translator.translate(sep)
+def _string_ascii(translator, expr):
+    arg, = expr.op().args
+    return 'TO_CODE_POINTS({})[SAFE_OFFSET(0)]'.format(
+        translator.translate(arg)
+    )
+
+
+def _string_right(translator, expr):
+    arg, nchars = map(translator.translate, expr.op().args)
+    return 'SUBSTR({arg}, -LEAST(LENGTH({arg}), {nchars}))'.format(
+        arg=arg,
+        nchars=nchars,
     )
 
 
@@ -221,32 +199,44 @@ _operation_registry.update({
     ops.ExtractSecond: _extract_field('second'),
     ops.ExtractMillisecond: _extract_field('millisecond'),
 
-    ops.StringReplace: _string_replace,
-    ops.StringSplit: _string_split,
+    ops.StringReplace: fixed_arity('REPLACE', 3),
+    ops.StringSplit: fixed_arity('SPLIT', 2),
     ops.StringConcat: _string_concat,
     ops.StringJoin: _string_join,
-    ops.GroupConcat: _group_concat,
+    ops.StringAscii: _string_ascii,
+    ops.StringFind: _string_find,
+    ops.StrRight: _string_right,
+    ops.Repeat: fixed_arity('REPEAT', 2),
+    ops.RegexSearch: _regex_search,
+    ops.RegexExtract: _regex_extract,
+    ops.RegexReplace: _regex_replace,
+    ops.GroupConcat: fixed_arity('STRING_AGG', 2),
 
-    ops.IfNull: _ifnull,
+    ops.IfNull: fixed_arity('IFNULL', 2),
     ops.Cast: _cast,
 
     ops.StructField: _struct_field,
 
-    ops.ArrayCollect: _array_collect,
+    ops.ArrayCollect: unary('ARRAY_AGG'),
     ops.ArrayConcat: _array_concat,
     ops.ArrayIndex: _array_index,
-    ops.ArrayLength: _array_length,
+    ops.ArrayLength: unary('ARRAY_LENGTH'),
 
     # BigQuery doesn't have these operations built in.
     # ops.ArrayRepeat: _array_repeat,
     # ops.ArraySlice: _array_slice,
-
-    ops.StringFind: _string_find,
-    ops.Repeat: _string_repeat,
-    ops.RegexSearch: _regex_search,
-    ops.RegexExtract: _regex_extract,
-    ops.RegexReplace: _regex_replace,
 })
+
+_invalid_operations = {
+    ops.Translate,
+    ops.FindInSet,
+    ops.Capitalize,
+}
+
+_operation_registry = {
+    k: v for k, v in _operation_registry.items()
+    if k not in _invalid_operations
+}
 
 
 class BigQueryExprTranslator(impala_compiler.ImpalaExprTranslator):
