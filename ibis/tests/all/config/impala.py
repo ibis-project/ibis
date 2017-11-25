ae7f79a92ebb5e85aee51ec44fe49220db76724d
Phillip Cloud <cpcloud@gmail.com>
2017-11-25 18:05:54 -0500

    Connect to impala

--- ibis/tests/all/config/impala.py
+++ ibis/tests/all/config/impala.py
@@ -1,9 +1,31 @@
-import pytest
+from ibis.tests.all.config.backendtestconfiguration import (
+    BackendTestConfiguration
+)
 
-from ibis.tests.all.config.backendtestconfiguration import BackendTestConfiguration
+import ibis
+from ibis.impala.tests.common import IbisTestEnv
+
+
+ENV = IbisTestEnv()
 
 
 class Impala(BackendTestConfiguration):
     @classmethod
     def connect(cls, backend):
-        pytest.skip('Skipping {}'.format(backend.__name__))
+        hc = ibis.hdfs_connect(
+            host=ENV.nn_host,
+            port=ENV.webhdfs_port,
+            auth_mechanism=ENV.auth_mechanism,
+            verify=ENV.auth_mechanism not in ['GSSAPI', 'LDAP'],
+            user=ENV.webhdfs_user
+        )
+        auth_mechanism = ENV.auth_mechanism
+        if auth_mechanism == 'GSSAPI' or auth_mechanism == 'LDAP':
+            print("Warning: ignoring invalid Certificate Authority errors")
+        return ibis.impala.connect(
+            host=ENV.impala_host,
+            port=ENV.impala_port,
+            auth_mechanism=ENV.auth_mechanism,
+            hdfs_client=hc,
+            database='ibis_testing'
+        )
