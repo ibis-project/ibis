8c51a4ed2ccc96c710f5f8f98e62b498c3892930
Phillip Cloud <cpcloud@gmail.com>
2017-12-07 12:02:00 -0500

    Add ability to specify a column's type

--- ibis/file/csv.py
+++ ibis/file/csv.py
@@ -1,12 +1,45 @@
+import numpy as np
 import pandas as pd
 
+import ibis.expr.datatypes as dt
 import ibis.expr.operations as ops
 
 from ibis.file.client import FileClient
 
+from ibis.pandas.api import PandasDialect
 from ibis.pandas.core import pre_execute, execute  # noqa
-from ibis.pandas.client import pandas_dtypes_to_ibis_schema, PandasDialect
+from ibis.pandas.client import pandas_dtypes_to_ibis_schema
 from ibis.pandas.execution.selection import physical_tables
+
+
+_IBIS_TO_PANDAS_DTYPE = {
+    # dt.Any: None,
+    # dt.Null: None,
+    dt.Boolean: bool,
+
+    dt.Int8: np.int8,
+    dt.UInt8: np.uint8,
+    dt.Int16: np.int16,
+    dt.UInt16: np.uint16,
+    dt.Int32: np.int32,
+    dt.UInt32: np.uint32,
+    dt.Int64: np.int64,
+    dt.UInt64: np.uint64,
+
+    dt.Float: np.float32,
+    dt.Double: np.float64,
+    dt.Halffloat: np.float16,
+
+    dt.String: str,
+    dt.Binary: bytes,
+
+    dt.Date: 'datetime64[D]',
+}
+
+
+def ibis_schema_to_pandas_dtypes(schema):
+    items = zip(schema.names, schema.types)
+    return {name: _IBIS_TO_PANDAS_DTYPE[type(t)] for name, t in items}
 
 
 def connect(path):
@@ -38,7 +71,7 @@ class CSVClient(FileClient):
         data = execute(expr)
         data.to_csv(str(path), index=index, **kwargs)
 
-    def table(self, name, path=None):
+    def table(self, name, path=None, schema=None):
         if name not in self.list_tables(path):
             raise AttributeError(name)
 
@@ -47,7 +80,12 @@ class CSVClient(FileClient):
 
         # get the schema
         f = path / "{}.{}".format(name, self.extension)
-        df = pd.read_csv(str(f), header=0, nrows=10)
+
+        dtype = None
+        if schema is not None:
+            dtype = ibis_schema_to_pandas_dtypes(schema)
+
+        df = pd.read_csv(str(f), header=0, nrows=10, dtype=dtype)
         schema = pandas_dtypes_to_ibis_schema(df, {})
 
         t = CSVTable(name, schema, self).to_expr()
@@ -59,6 +97,9 @@ class CSVClient(FileClient):
 
     def list_databases(self, path=None):
         return self._list_databases_dirs(path)
+
+    def compile(self, expr, *args, **kwargs):
+        return expr
 
 
 @pre_execute.register(CSVTable, CSVClient)
