a234aa6dbf90c50a4085baca5a23cabb15fd54ac
petobens <ferrari_pedro@yahoo.com>
2017-12-08 12:18:11 -0300

    Pass 'user' argument to impala cursor
    This prevents authorization errors when using NOSASL

--- ibis/impala/client.py
+++ ibis/impala/client.py
@@ -159,7 +159,7 @@ class ImpalaConnection(object):
         self._connections.add(con)
 
         # make sure the connection works
-        cursor = con.cursor(convert_types=True)
+        cursor = con.cursor(user=params.get('user'), convert_types=True)
         cursor.ping()
 
         wrapper = ImpalaCursor(cursor, self, con, self.database,
