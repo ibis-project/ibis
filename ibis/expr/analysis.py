123b15665328913c79e377ed7e02339c00b9f865
Phillip Cloud <cpcloud@gmail.com>
2018-01-24 17:00:35 -0500

    If we look for a column that does not exist in the root's selection we cannot fuse so return None

--- ibis/expr/analysis.py
+++ ibis/expr/analysis.py
@@ -821,7 +821,7 @@ class Projector(object):
 def _maybe_resolve_exprs(table, exprs):
     try:
         return table._resolve(exprs)
-    except AttributeError:
+    except (AttributeError, IbisTypeError):
         return None
 
 
