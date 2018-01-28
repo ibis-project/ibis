46fcb1ddd9c01ba23206e4431a5045e4d435f4c7
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-01-28 10:55:01 +0100

    attempt37

--- testing/load-all.sh
+++ testing/load-all.sh
@@ -7,9 +7,11 @@ export IBIS_TEST_WEBHDFS_PORT=50070
 export IBIS_TEST_WEBHDFS_USER=ubuntu
 export IBIS_TEST_DATA_DIRECTORY=/tmp/ibis-testing-data
 
-python datamgr.py download
-python datamgr.py sqlite
-python datamgr.py postgres
-python datamgr.py clickhouse
-python impalamgr.py load --data --no-udf --data-dir /tmp/ibis-testing-data
+CWD=$(dirname $0)
+
+python $CWD/datamgr.py download
+python $CWD/datamgr.py sqlite
+python $CWD/datamgr.py postgres
+python $CWD/datamgr.py clickhouse
+python $CWD/impalamgr.py load --data --no-udf --data-dir $IBIS_TEST_DATA_DIRECTORY
 
