82ffd6cd458950fdb532b7bb44858e56f96aa320
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-01-31 02:33:30 +0100

    double asv config print

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -4,7 +4,6 @@ CWD=$(dirname $0)
 
 pip install asv
 $CWD/asvconfig.py $1 | tee $HOME/.asv-machine.json
-cat $HOME/.asv-machine.json
 git remote add upstream https://github.com/ibis-project/ibis
 git fetch upstream refs/heads/master
 asv continuous -f 1.5 -e upstream/master $2 || echo > /dev/null
