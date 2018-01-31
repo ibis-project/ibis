fb542d828801f03e8144e92f4e6d57218aba5b05
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-01-31 01:35:30 +0100

    benchmark

---
+++ ci/benchmark.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CWD=$(dirname $0)
+
+pip install asv
+$CWD/asvconfig.py $1 | tee $HOME/.asv-machine.json
+cat $HOME/.asv-machine.json
+git remote add upstream https://github.com/ibis-project/ibis
+git fetch upstream refs/heads/master
+asv continuous -f 1.5 -e upstream/master $2 || echo > /dev/null
