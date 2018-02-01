7a592b25c9face9703847387471d9774bb13892e
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-02-01 23:46:59 +0100

    datadir

--- ci/load-data.sh
+++ ci/load-data.sh
@@ -9,7 +9,7 @@ python $CWD/datamgr.py sqlite &
 python $CWD/datamgr.py parquet &
 python $CWD/datamgr.py postgres &
 python $CWD/datamgr.py clickhouse &
-python $CWD/impalamgr.py load --data --data-dir $IBIS_TEST_DATA_DIRECTORY &
+python $CWD/impalamgr.py load --data &
 
 
 FAIL=0
