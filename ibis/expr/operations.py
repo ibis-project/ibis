edf10f4b18892478464ae03c74f940bebb84e6e8
Phillip Cloud <cpcloud@gmail.com>
2018-04-12 10:19:44 -0400

    BUG: Precedence computation of case statements fails
    Closes #1412
    
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #1414 from cpcloud/fix-case and squashes the following commits:
    
    0a5e8779 [Phillip Cloud] BUG: Precedence computation of case statements fails

--- ibis/expr/operations.py
+++ ibis/expr/operations.py
@@ -1,18 +1,3 @@
-# Copyright 2015 Cloudera Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import six
 import toolz
 import operator
@@ -1312,10 +1297,14 @@ class SimpleCase(ValueOp):
         assert len(self.cases) == len(self.results)
 
     def root_tables(self):
-        all_exprs = [self.base] + self.cases + self.results + (
-            [] if self.default is None else [self.default]
+        return distinct_roots(
+            *itertools.chain(
+                [self.base],
+                self.cases,
+                self.results,
+                [] if self.default is None else [self.default]
+            )
         )
-        return distinct_roots(*all_exprs)
 
     def output_type(self):
         exprs = self.results + [self.default]
@@ -1332,10 +1321,13 @@ class SearchedCase(ValueOp):
 
     def root_tables(self):
         cases, results, default = self.args
-        all_exprs = cases.values + results.values + (
-            [] if default is None else [default]
+        return distinct_roots(
+            *itertools.chain(
+                cases.values,
+                results.values,
+                [] if default is None else [default]
+            )
         )
-        return distinct_roots(*all_exprs)
 
     def output_type(self):
         exprs = self.results + [self.default]
