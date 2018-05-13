a756da8e529045cb0c4acffcc17a760ff26cc514
Phillip Cloud <cpcloud@gmail.com>
2018-05-13 16:18:32 -0400

    Fix postgres numerics

--- ibis/sql/postgres/compiler.py
+++ ibis/sql/postgres/compiler.py
@@ -1,17 +1,3 @@
-# Copyright 2014 Cloudera Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import re
 import locale
 import string
@@ -401,11 +387,10 @@ def _reduction(func_name):
 
 def _log(t, expr):
     arg, base = expr.op().args
-    arg = t.translate(arg)
+    sa_arg = t.translate(arg)
     if base is not None:
-        return sa.func.log(t.translate(base), arg)
-    else:
-        return sa.func.ln(arg)
+        return sa.func.ln(sa_arg) / sa.func.ln(t.translate(base))
+    return sa.func.ln(sa_arg)
 
 
 class regex_extract(GenericFunction):
