7810ec35150308e80fb7171cfec39cb5f36d2f7e
Phillip Cloud <cpcloud@gmail.com>
2018-05-15 17:20:41 -0400

    PY2 places

--- ibis/pandas/execution/generic.py
+++ ibis/pandas/execution/generic.py
@@ -309,7 +309,7 @@ def execute_cast_string_literal(op, data, type, **kwargs):
     (six.integer_types, type(None))
 )
 def execute_round_scalars(op, data, places, **kwargs):
-    return round(data, places)
+    return round(data) if not places else round(data, places)
 
 
 @execute_node.register(
