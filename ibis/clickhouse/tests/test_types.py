ecdaf91e9f75577b5ef827b593cc81f0259d775d
Phillip Cloud <cpcloud@gmail.com>
2018-06-01 18:32:02 -0400

    Fix clickhouse

--- ibis/clickhouse/tests/test_types.py
+++ ibis/clickhouse/tests/test_types.py
@@ -14,4 +14,4 @@ def test_column_types(alltypes):
     assert df.bigint_col.dtype.name == 'int64'
     assert df.float_col.dtype.name == 'float32'
     assert df.double_col.dtype.name == 'float64'
-    assert pd.core.common.is_datetime64_dtype(df.timestamp_col.dtype)
+    assert df.timestamp_col.dtype.name == 'datetime64[ns]'
