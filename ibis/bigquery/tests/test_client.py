1f0d8992968ada9894b46e529ded8da656e1c97f
Scott Lustig <scott.lustig@twosigma.com>
2018-06-05 23:20:35 -0400

    PEP8

--- ibis/bigquery/tests/test_client.py
+++ ibis/bigquery/tests/test_client.py
@@ -531,12 +531,14 @@ def test_large_timestamp(client):
 
 
 def test_string_to_timestamp(client):
-    timestamp = pd.Timestamp(datetime(year=2017, month=2, day=6), tz=pytz.timezone('UTC'))
+    timestamp = pd.Timestamp(datetime(year=2017, month=2, day=6),
+                             tz=pytz.timezone('UTC'))
     expr = ibis.literal('2017-02-06').to_timestamp('%F')
     result = client.execute(expr)
     assert result == timestamp
 
-    timestamp_tz = pd.Timestamp(datetime(year=2017, month=2, day=6, hour=5), tz=pytz.timezone('UTC'))
+    timestamp_tz = pd.Timestamp(datetime(year=2017, month=2, day=6, hour=5),
+                                tz=pytz.timezone('UTC'))
     expr_tz = ibis.literal('2017-02-06').to_timestamp('%F', 'America/New_York')
     result_tz = client.execute(expr_tz)
     assert result_tz == timestamp_tz
