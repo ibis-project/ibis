eab31e251a5abbd62a089a4a2645cbe872b70daf
Phillip Cloud <cpcloud@gmail.com>
2018-06-09 19:00:08 -0400

    Pass loglevel to scripts and CI

--- ci/build.sh
+++ ci/build.sh
@@ -4,4 +4,4 @@ docker-compose rm --force --stop
 docker-compose up -d --no-build mapd postgres mysql clickhouse impala
 docker-compose run --rm waiter
 docker-compose build --pull ibis
-docker-compose run --rm ibis ci/load-data.sh
+docker-compose run -e LOGLEVEL --rm ibis ci/load-data.sh
