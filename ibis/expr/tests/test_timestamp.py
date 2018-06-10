64288eefdd52753bba8323becab234393bc56577
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-06-10 16:14:10 +0200

    time field accessors

--- ibis/expr/tests/test_timestamp.py
+++ ibis/expr/tests/test_timestamp.py
@@ -136,7 +136,7 @@ def test_timestamp_precedence():
 def test_timestamp_field_access_on_date(
     field, expected_operation, expected_type, alltypes
 ):
-    date_col = alltypes.i.cast('date')
+    date_col = alltypes.i.date()
     result = getattr(date_col, field)()
     assert isinstance(result, expected_type)
     assert isinstance(result.op(), expected_operation)
@@ -154,6 +154,40 @@ def test_timestamp_field_access_on_date(
 def test_timestamp_field_access_on_date_failure(
     field, expected_operation, expected_type, alltypes
 ):
-    date_col = alltypes.i.cast('date')
+    time_col = alltypes.i.date()
+    with pytest.raises(AttributeError):
+        getattr(time_col, field)
+
+
+@pytest.mark.parametrize(
+    ('field', 'expected_operation', 'expected_type'),
+    [
+        ('hour', ops.ExtractHour, ir.IntegerColumn),
+        ('minute', ops.ExtractMinute, ir.IntegerColumn),
+        ('second', ops.ExtractSecond, ir.IntegerColumn),
+        ('millisecond', ops.ExtractMillisecond, ir.IntegerColumn),
+    ]
+)
+def test_timestamp_field_access_on_time(
+    field, expected_operation, expected_type, alltypes
+):
+    time_col = alltypes.i.time()
+    result = getattr(time_col, field)()
+    assert isinstance(result, expected_type)
+    assert isinstance(result.op(), expected_operation)
+
+
+@pytest.mark.parametrize(
+    ('field', 'expected_operation', 'expected_type'),
+    [
+        ('year', ops.ExtractYear, ir.IntegerColumn),
+        ('month', ops.ExtractMonth, ir.IntegerColumn),
+        ('day', ops.ExtractDay, ir.IntegerColumn),
+    ]
+)
+def test_timestamp_field_access_on_time_failure(
+    field, expected_operation, expected_type, alltypes
+):
+    date_col = alltypes.i.cast('time')
     with pytest.raises(AttributeError):
         getattr(date_col, field)
