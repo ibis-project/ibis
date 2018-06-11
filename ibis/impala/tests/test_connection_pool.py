d9d9ee46c114756d5743d1e47d09b8fb0c59afd8
Phillip Cloud <cpcloud@gmail.com>
2018-06-11 16:03:46 -0400

    Refactor conftest

--- ibis/impala/tests/test_connection_pool.py
+++ ibis/impala/tests/test_connection_pool.py
@@ -8,24 +8,22 @@ pytest.importorskip('impala.dbapi')
 pytestmark = pytest.mark.impala
 
 
-def test_connection_pool_size(impala_port, hdfs, impala_host, test_data_db):
+def test_connection_pool_size(hdfs, env, test_data_db):
     client = ibis.impala.connect(
-        port=impala_port,
+        port=env.impala_port,
         hdfs_client=hdfs,
-        host=impala_host,
+        host=env.impala_host,
         database=test_data_db,
     )
     assert len(client.con.connection_pool) == 1
 
 
-def test_connection_pool_size_after_close(
-    impala_port, hdfs, impala_host, test_data_db
-):
+def test_connection_pool_size_after_close(hdfs, env, test_data_db):
     client = ibis.impala.connect(
-        port=impala_port,
+        port=env.impala_port,
         hdfs_client=hdfs,
-        host=impala_host,
+        host=env.impala_host,
         database=test_data_db,
     )
     client.close()
-    assert len(client.con.connection_pool) == 0
+    assert not client.con.connection_pool
