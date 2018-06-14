979cb1090284f005fa3c77b3bb47138ad19623ea
Phillip Cloud <cpcloud@gmail.com>
2018-06-13 21:53:50 -0400

    Remove assertion

--- ibis/bigquery/tests/test_client.py
+++ ibis/bigquery/tests/test_client.py
@@ -383,10 +383,8 @@ def test_parted_column_rename(parted_alltypes):
 
 
 def test_scalar_param_partition_time(parted_alltypes):
-    assert 'PARTITIONTIME' in parted_alltypes.columns, \
-        str(parted_alltypes.columns)
-    assert 'PARTITIONTIME' in parted_alltypes.schema(), \
-        str(parted_alltypes.schema())
+    assert 'PARTITIONTIME' in parted_alltypes.columns
+    assert 'PARTITIONTIME' in parted_alltypes.schema()
     param = ibis.param('timestamp').name('time_param')
     expr = parted_alltypes[parted_alltypes.PARTITIONTIME < param]
     df = expr.execute(params={param: '2017-01-01'})
