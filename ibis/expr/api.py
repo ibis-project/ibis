06dc52b556b965ac67fbca3e6385a77cfe839617
Scott Lustig <scott.lustig@twosigma.com>
2018-06-20 20:18:12 -0400

    Add docstring

--- ibis/expr/api.py
+++ ibis/expr/api.py
@@ -2302,7 +2302,8 @@ _timestamp_value_methods = dict(
 
     __rsub__=_timestamp_sub,
     rsub=_timestamp_sub,
-    day_of_week=property(lambda self: ops.DayOfWeekNode(self).to_expr()),
+    day_of_week=property(lambda self: ops.DayOfWeekNode(self).to_expr(),
+                         doc='The day of the week with Monday=0, Sunday=6'),
 )
 
 _add_methods(ir.TimestampValue, _timestamp_value_methods)
@@ -2351,7 +2352,8 @@ _date_value_methods = dict(
     year=_extract_field('year', ops.ExtractYear),
     month=_extract_field('month', ops.ExtractMonth),
     day=_extract_field('day', ops.ExtractDay),
-    day_of_week=property(lambda self: ops.DayOfWeekNode(self).to_expr()),
+    day_of_week=property(lambda self: ops.DayOfWeekNode(self).to_expr(),
+                         doc='The day of the week with Monday=0, Sunday=6'),
 
     truncate=_date_truncate,
 
