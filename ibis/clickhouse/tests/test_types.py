4434b5055a9436d766edd75dfcc83bbb6b25b032
之东 <0x4ec7@gmail.com>
2018-07-02 22:36:20 +0800

    Updated: add test case for column types with additional argument.
    1. DateTime with timezone, DateTime('UTC');
    2. FixedString with length, FixedString(8)

--- ibis/clickhouse/tests/test_types.py
+++ ibis/clickhouse/tests/test_types.py
@@ -14,3 +14,14 @@ def test_column_types(alltypes):
     assert df.float_col.dtype.name == 'float32'
     assert df.double_col.dtype.name == 'float64'
     assert df.timestamp_col.dtype.name == 'datetime64[ns]'
+
+
+def test_columns_types_with_additional_argument(con):
+    datetime_with_tz_sql = (
+        "SELECT toDateTime('2018-07-02 00:00:00', 'UTC') AS datetime_with_tz")
+    fixedstring_with_len_sql = (
+        "SELECT toFixedString('foo', 8) as fixedstring_with_len")
+    assert con.sql(datetime_with_tz_sql).execute()\
+              .datetime_with_tz.dtype.name == 'datetime64[ns]'
+    assert con.sql(fixedstring_with_len_sql).execute()\
+              .fixedstring_with_len.dtype.name == 'object'
