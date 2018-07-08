09c507953551b88fbaaee31e9ef45db359652dc4
Phillip Cloud <cpcloud@gmail.com>
2018-07-08 17:08:37 -0400

    Add kudu to docker-compose

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 docker-compose rm --force --stop
-docker-compose up -d --no-build mapd postgres mysql clickhouse impala
+docker-compose up -d --no-build \
+    mapd postgres mysql clickhouse impala kudu-master kudu-tserver
 docker-compose run --rm waiter
 docker-compose build --pull ibis
 docker-compose run -e LOGLEVEL --rm ibis ci/load-data.sh
