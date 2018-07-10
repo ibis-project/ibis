8bd9d9fa5558171be1c6aee12204b57a82315c33
Phillip Cloud <cpcloud@gmail.com>
2018-07-09 20:14:42 -0400

    Comments

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -e
 
+# stop all running docker compose services
 docker-compose rm --force --stop
+
+# build the ibis image
+docker-compose build --pull ibis
+
+# start all docker compose services
 docker-compose up -d --no-build \
     mapd postgres mysql clickhouse impala kudu-master kudu-tserver
+
+# wait for services to start
 docker-compose run --rm waiter
-docker-compose build --pull ibis
+
+# load data
 docker-compose run -e LOGLEVEL --rm ibis ci/load-data.sh
