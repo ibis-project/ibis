6ac351c868d14d00911b960dc7ee2f32981fa92e
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-08-05 23:02:16 +0200

    pass docker-compose file explicitly

--- ci/build.sh
+++ ci/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -e
 
+compose_file=$(dirname "$0")/docker-compose.yml
+
 # stop all running docker compose services
-docker-compose rm --force --stop
+docker-compose -f $compose_file rm --force --stop
 
 # build the ibis image
-docker-compose build --pull ibis
+docker-compose -f $compose_file build --pull ibis
 
 # start all docker compose services
-docker-compose up -d --no-build \
+docker-compose -f $compose_file up -d --no-build \
     mapd postgres mysql clickhouse impala kudu-master kudu-tserver
 
 # wait for services to start
-docker-compose run --rm waiter
+docker-compose -f $compose_file run --rm waiter
 
 # load data
-docker-compose run -e LOGLEVEL --rm ibis ci/load-data.sh
+docker-compose -f $compose_file run -e LOGLEVEL --rm ibis ci/load-data.sh
