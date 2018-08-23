76248518eaf92ec8529a52482c99b6985b3044fe
Scott Lustig <scott.lustig@twosigma.com>
2018-08-22 22:28:18 -0400

    Implement approx_median for BQ backend

--- ibis/bigquery/compiler.py
+++ ibis/bigquery/compiler.py
@@ -586,6 +586,19 @@ def compiles_floor(t, e):
     return 'CAST(FLOOR({}) AS {})'.format(t.translate(arg), bigquery_type)
 
 
+@compiles(ops.CMSMedian)
+def compiles_approx(translator, expr):
+    arg = expr.op().args[0]
+    where = expr.op().args[1]
+
+    if where is not None:
+        arg = where.ifelse(arg, ibis.NA)
+
+    return 'APPROX_QUANTILES({}, 2)[OFFSET(1)]'.format(
+        translator.translate(arg)
+    )
+
+
 class BigQueryDialect(impala_compiler.ImpalaDialect):
 
     translator = BigQueryExprTranslator
