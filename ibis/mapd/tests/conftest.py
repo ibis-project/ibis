36de943e73627469fe9f0809dfce47ee1d125d89
Tony Fast <tony.fast@gmail.com>
2018-10-16 13:22:49 -0400

    Remove parameters from mapd Fixes #1610
    Closes #1610
    
    Author: Tony Fast <tony.fast@gmail.com>
    
    Closes #1648 from tonyfast/master and squashes the following commits:
    
    d561787 [Tony Fast] Merge branch 'master' of https://github.com/ibis-project/ibis
    a7eb782 [Tony Fast] Trigger build
    f9b859f [Tony Fast] Remove fragment_size, page_size, partitions, and shard_count from the  create_table client.
    8a27534 [Tony Fast] Add tests to cover the create_table function.
    e47d988 [Tony Fast] Add fixtures test_data_db, temp_table, temp_database
    a95941d [Tony Fast] Remove parameters from mapd Fixes #1610

--- ibis/mapd/tests/conftest.py
+++ ibis/mapd/tests/conftest.py
@@ -1,4 +1,5 @@
 import ibis
+import ibis.util as util
 import os
 import pytest
 
@@ -50,3 +51,33 @@ def translate():
     dialect = MapDDialect()
     context = dialect.make_context()
     return lambda expr: dialect.translator(expr, context).get_result()
+
+
+def _random_identifier(suffix):
+    return '__ibis_test_{}_{}'.format(suffix, util.guid())
+
+
+@pytest.fixture
+def temp_table(con):
+    name = _random_identifier('table')
+    try:
+        yield name
+    finally:
+        assert con.exists_table(name), name
+        con.drop_table(name)
+
+
+@pytest.fixture(scope='session')
+def test_data_db():
+    return MAPD_DB
+
+
+@pytest.fixture
+def temp_database(con, test_data_db):
+    name = _random_identifier('database')
+    con.create_database(name)
+    try:
+        yield name
+    finally:
+        con.set_database(test_data_db)
+        con.drop_database(name, force=True)
