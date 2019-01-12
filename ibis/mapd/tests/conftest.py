bf9f7e4585e16f6fe88928e7bc3c3b9323c90ca9
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-01-12 07:22:27 -0500

    Updated mapd ci to v4.4.1
    Updated mapd ci to v4.4.0
    
    Author: Ivan Ogasawara <ivan.ogasawara@gmail.com>
    Author: Ivan <ivan.ogasawara@gmail.com>
    
    Closes #1681 from xmnlab/update_mapd_ci_440 and squashes the following commits:
    
    3b8d2ef [Ivan Ogasawara] small fixes from PR feedback
    9b0778a [Ivan] Merge branch 'master' into update_mapd_ci_440
    e07599e [Ivan Ogasawara] Added cpu-buffer-mem-bytes = 1000000000 mapd conf
    b1ebb46 [Ivan Ogasawara] Moved pymapd dep from req3.6 to pip section
    7b56afb [Ivan Ogasawara] Added mapd log when any step fails
    e634aaf [Ivan Ogasawara] on py35 ci, use pymapd 0.6 from pip
    2334c3c [Ivan Ogasawara] Changed pymapd version
    0a947ea [Ivan Ogasawara] changed version for pyarrow and pymapd
    9d4e424 [Ivan Ogasawara] Pinned pymapd and pyarrow on req py36 yml
    42c1a3f [Ivan Ogasawara] Changed con fixture for mapd
    3d359bf [Ivan Ogasawara] Rerun CI
    71e1443 [Ivan Ogasawara] Updated to 4.4.1
    7267ccf [Ivan Ogasawara] Updated mapd ci to v4.4.0

--- ibis/mapd/tests/conftest.py
+++ ibis/mapd/tests/conftest.py
@@ -13,26 +13,12 @@ MAPD_DB = os.environ.get('IBIS_TEST_DATA_DB', 'ibis_testing')
 
 @pytest.fixture(scope='module')
 def con():
-    if MAPD_DB != 'mapd':
-        import pymapd
-        conn = pymapd.connect(
-            host=MAPD_HOST,
-            user=MAPD_USER,
-            password=MAPD_PASS,
-            port=MAPD_PORT, dbname='mapd'
-        )
-        try:
-            conn.execute('CREATE DATABASE {}'.format(MAPD_DB))
-        except Exception as e:
-            print(e)
-        conn.close()
-
     return ibis.mapd.connect(
         host=MAPD_HOST,
         port=MAPD_PORT,
         user=MAPD_USER,
         password=MAPD_PASS,
-        database=MAPD_DB,
+        database=MAPD_DB
     )
 
 
