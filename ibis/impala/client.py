6fb78082e056bae3a519876a20f1183f658f307f
Phillip Cloud <cpcloud@gmail.com>
2019-03-10 14:28:37 -0400

    Fix docstring

--- ibis/impala/client.py
+++ ibis/impala/client.py
@@ -882,16 +882,16 @@ class ImpalaClient(SQLClient):
         return self._execute(statement)
 
     def drop_database(self, name, force=False):
-        """
-        Drop an Impala database
+        """Drop an Impala database.
 
         Parameters
         ----------
         name : string
           Database name
-        force : boolean, default False
+        force : bool, default False
           If False and there are any tables in this database, raises an
           IntegrityError
+
         """
         if not force or self.exists_database(name):
             tables = self.list_tables(database=name)
