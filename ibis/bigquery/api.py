eb3c98c4532cb4146be64cad6b2603a7cb845b96
Phillip Cloud <cpcloud@gmail.com>
2019-03-17 09:58:46 -0400

    Implement bigquery auth using pydata_google_auth
    Also allow project_id and dataset_id to be optional  Closes #1583
    
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #1728 from cpcloud/gh-1583 and squashes the following commits:
    
    bd27a9a [Phillip Cloud] Add credentials cache
    5b9622d [Phillip Cloud] Add credentials cache
    5c4ddd3 [Phillip Cloud] Hack around missing python3.7 in conda-forge google-auth-oauthlib
    dabf43c [Phillip Cloud] Fix annotation
    873c79b [Phillip Cloud] Implement bigquery auth using pydata_google_auth and allow project_id and dataset_id to be optional
    8e6102a [Phillip Cloud] Add test for an operation that requires a dataset
    4672311 [Phillip Cloud] Checkpoint

--- ibis/bigquery/api.py
+++ ibis/bigquery/api.py
@@ -1,6 +1,11 @@
-"""BigQuery public API"""
+"""BigQuery public API."""
 
-import google.cloud.bigquery  # noqa: F401 fail early if bigquery is missing
+from typing import Optional
+
+import google.cloud.bigquery  # noqa: F401, fail early if bigquery is missing
+import google.auth.credentials
+import pydata_google_auth
+
 import ibis.common as com
 
 from ibis.config import options  # noqa: F401
@@ -22,23 +27,23 @@ __all__ = (
 
 
 def compile(expr, params=None):
-    """
-    Force compilation of expression as though it were an expression depending
-    on BigQuery. Note you can also call expr.compile()
+    """Compile an expression for BigQuery.
 
     Returns
     -------
-    compiled : string
+    compiled : str
+
+    See Also
+    --------
+    ibis.expr.types.Expr.compile
+
     """
     from ibis.bigquery.compiler import to_sql
     return to_sql(expr, dialect.make_context(params=params))
 
 
 def verify(expr, params=None):
-    """
-    Determine if expression can be successfully translated to execute on
-    BigQuery
-    """
+    """Check if an expression can be compiled using BigQuery."""
     try:
         compile(expr, params=params)
         return True
@@ -46,18 +51,45 @@ def verify(expr, params=None):
         return False
 
 
-def connect(project_id, dataset_id, credentials=None):
-    """Create a BigQueryClient for use with Ibis
+SCOPES = ["https://www.googleapis.com/auth/bigquery"]
+CLIENT_ID = (
+    "546535678771-gvffde27nd83kfl6qbrnletqvkdmsese.apps.googleusercontent.com"
+)
+CLIENT_SECRET = "iU5ohAF2qcqrujegE3hQ1cPt"
+
+
+def connect(
+    project_id: Optional[str] = None,
+    dataset_id: Optional[str] = None,
+    credentials: Optional[google.auth.credentials.Credentials] = None,
+) -> BigQueryClient:
+    """Create a BigQueryClient for use with Ibis.
 
     Parameters
     ----------
-    project_id: str
-    dataset_id: str
-    credentials : google.auth.credentials.Credentials, optional, default None
+    project_id : str
+        A BigQuery project id.
+    dataset_id : str
+        A dataset id that lives inside of the project indicated by
+        `project_id`.
+    credentials : google.auth.credentials.Credentials
 
     Returns
     -------
     BigQueryClient
-    """
 
-    return BigQueryClient(project_id, dataset_id, credentials=credentials)
+    """
+    if credentials is None:
+        credentials_cache = pydata_google_auth.cache.ReadWriteCredentialsCache(
+            filename="ibis.json"
+        )
+        credentials, project_id = pydata_google_auth.default(
+            SCOPES,
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            credentials_cache=credentials_cache,
+        )
+
+    return BigQueryClient(
+        project_id, dataset_id=dataset_id, credentials=credentials
+    )
