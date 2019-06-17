deed6aac57cb6e233216995b63dafef91cb317ea
Phillip Cloud <cpcloud@gmail.com>
2019-06-17 15:38:26 -0400

    Revert select parts

--- ibis/file/client.py
+++ ibis/file/client.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 import ibis
 import ibis.expr.types as ir
-from ibis.pandas.core import execute
-from ibis.pandas.dispatch import execute_last
+from ibis.pandas.core import execute_and_reset
 
 
 class FileClient(ibis.client.Client):
@@ -35,15 +34,9 @@ class FileClient(ibis.client.Client):
         return FileDatabase(name, self, path=path)
 
     def execute(self, expr, params=None, **kwargs):  # noqa
-        assert isinstance(expr, ir.Expr), "Expected ir.Expr, got {}".format(
-            type(expr)
-        )
-        return execute_last(
-            expr.op(),
-            execute(expr, params=params, **kwargs),
-            params=params,
-            **kwargs,
-        )
+        assert isinstance(expr, ir.Expr)
+        scope = kwargs.pop('scope', {})
+        return execute_and_reset(expr, params=params, scope=scope, **kwargs)
 
     def list_tables(self, path=None):
         raise NotImplementedError
