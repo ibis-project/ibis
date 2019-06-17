92f64b43bddd00ac6a2058cc62f765adb0ebe27c
Scott Hajek <shajek@pivotal.io>
2019-06-17 11:26:55 -0400

    Postgres UDF: define a UDF in-database based on a Python function

--- ibis/sql/postgres/tests/test_udf.py
+++ ibis/sql/postgres/tests/test_udf.py
@@ -2,79 +2,88 @@
 
 import pytest
 
+import sqlalchemy.exc
 import ibis.expr.datatypes
 import ibis.sql.postgres.udf.api
 
 
+datatypes = ibis.expr.datatypes
+
 # Database setup (tables and UDFs)
 
 
+test_schema = 'test'
+
 table_name = 'udf_test_users'
 
-sql_table_setup = """DROP TABLE IF EXISTS public.{table_name};
-CREATE TABLE public.{table_name} (
+sql_table_setup = """DROP TABLE IF EXISTS {schema}.{table_name};
+CREATE TABLE {schema}.{table_name} (
     user_id integer,
     user_name varchar,
     name_length integer
 );
-INSERT INTO {table_name} VALUES
+INSERT INTO {schema}.{table_name} VALUES
 (1, 'Raj', 3),
 (2, 'Judy', 4),
 (3, 'Jonathan', 8)
 ;
-""".format(table_name=table_name)
+""".format(table_name=table_name, schema=test_schema)
 
-sql_create_plpython = "CREATE EXTENSION plpythonu"
+sql_create_plpython = "CREATE EXTENSION plpythonu "
 
-sql_define_py_udf = """CREATE OR REPLACE FUNCTION public.pylen(x varchar)
+sql_define_py_udf = """CREATE OR REPLACE FUNCTION {schema}.pylen(x varchar)
 RETURNS integer
 LANGUAGE plpythonu
 AS
 $$
 return len(x)
-$$;"""
+$$;""".format(schema=test_schema)
 
-sql_define_udf = """CREATE OR REPLACE FUNCTION public.custom_len(x varchar)
+sql_define_udf = """CREATE OR REPLACE FUNCTION {schema}.custom_len(x varchar)
 RETURNS integer
 LANGUAGE SQL
 AS
 $$
 SELECT length(x);
-$$;"""
+$$;""".format(schema=test_schema)
 
 
 @pytest.fixture
 def con_for_udf(con):
-    # with con.con.begin() as cur_conn:
-    #     cur_conn.execute(sql_table_setup)
-    #     yield con
-    #     con.rollback()
+    con.con.execute("DROP SCHEMA IF EXISTS {} CASCADE".format(test_schema))
+    con.con.execute("CREATE SCHEMA {}".format(test_schema))
     con.con.execute(sql_table_setup)
     con.con.execute(sql_define_udf)
+    try:
+        con.con.execute(sql_create_plpython)
+    except sqlalchemy.exc.ProgrammingError as e:
+        if '"plpythonu" already exists' not in str(e):
+            raise Exception('PL/Python extension creation failed') from e
     con.con.execute(sql_define_py_udf)
-    # con.con.execute(sql_create_plpython)
     yield con
+    # teardown
+    con.con.execute("DROP SCHEMA IF EXISTS {} CASCADE".format(test_schema))
 
 
 @pytest.fixture
 def table(con_for_udf):
-    return con_for_udf.table(table_name, schema='public')
+    return con_for_udf.table(table_name, schema=test_schema)
 
 
 # Create ibis UDF objects referring to UDFs already created in the database
 
 custom_length_udf = ibis.sql.postgres.udf.api.existing_udf(
     'custom_len',
-    input_type=[ibis.expr.datatypes.String()],
+    input_types=[ibis.expr.datatypes.String()],
     output_type=ibis.expr.datatypes.Integer(),
-    schema='public'
+    schema=test_schema
 )
 
 py_length_udf = ibis.sql.postgres.udf.api.existing_udf(
     'pylen',
-    input_type=[ibis.expr.datatypes.String()],
+    input_types=[ibis.expr.datatypes.String()],
     output_type=ibis.expr.datatypes.Integer(),
-    schema='public'
+    schema=test_schema
 )
 
 
@@ -96,3 +105,29 @@ def test_py_length_udf_worked(table):
     ]
     result = result_obj.execute()
     assert result['custom_len'].sum() == result['name_length'].sum()
+
+
+def mult_a_b(a, b):
+    """Test function to be defined in-database as a UDF
+    and used via ibis UDF"""
+    return a * b
+
+
+def test_func_to_udf_smoke(con_for_udf, table):
+    mult_a_b_udf = ibis.sql.postgres.udf.api.func_to_udf(
+        con_for_udf.con,
+        mult_a_b,
+        (datatypes.Int32(), datatypes.Int32()),
+        datatypes.Int32(),
+        schema=test_schema,
+        overwrite=True
+    )
+    table_filt = table.filter(table['user_id'] == 2)
+    expr = table_filt[
+        mult_a_b_udf(
+            table_filt['user_id'],
+            table_filt['name_length']
+        ).name('mult_result')
+    ]
+    result = expr.execute()
+    assert result['mult_result'].iloc[0] == 8
--- ibis/sql/postgres/udf/api.py
+++ ibis/sql/postgres/udf/api.py
@@ -1,14 +1,63 @@
+import inspect
+from decimal import Decimal
 import collections
 import itertools
 
 import sqlalchemy
 
 import ibis.expr.rules as rlz
+from ibis.expr import datatypes
 from ibis.expr.signature import Argument as Arg
 from ibis.sql.postgres.compiler import PostgresUDFNode, add_operation
 
 
 _udf_name_cache = collections.defaultdict(itertools.count)
+
+
+# type mapping based on: https://www.postgresql.org/docs/10/plpython-data.html
+sql_default_type = 'VARCHAR'
+
+pytype_sql = {
+    bool: 'BOOLEAN',
+    int: "INTEGER",
+    float: 'DOUBLE',
+    Decimal: 'NUMERIC',
+    bytes: 'BYTEA',
+    str: sql_default_type,
+}
+
+pytype_ibistype = {
+    bool: datatypes.Boolean(),
+    int: datatypes.Int32(),
+    float: datatypes.Float(),
+    Decimal: datatypes.Float(),
+    bytes: datatypes.Binary(),
+    str: datatypes.String(),
+}
+
+ibistype_pytype = {v: k for k, v in pytype_ibistype.items()}
+
+
+def get_sqltype(type_):
+    """Map input to the string specifying the Postgres data type
+    (as is used in SQL defining UDF signatures)
+
+    :param type_: str, a Python data type,
+                  or an ibis DataType (instance or class)
+    :return: string symbol of Postgres data type
+    """
+    if isinstance(type_, str) and type_ in pytype_sql.values():
+        return type_
+    elif type_ in pytype_sql.keys():
+        return pytype_sql[type_]
+    elif type_ in ibistype_pytype:
+        return pytype_sql[ibistype_pytype[type_]]
+    elif type_ in set(map(type, ibistype_pytype.keys())):
+        return pytype_sql[ibistype_pytype[type_()]]
+    else:
+        raise ValueError(
+            "Postgres data type not defined for: {}".format(type_)
+        )
 
 
 def create_udf_node(name, fields):
@@ -31,14 +80,18 @@ def create_udf_node(name, fields):
     return type(external_name, (PostgresUDFNode,), fields)
 
 
-def existing_udf(name, input_type, output_type, schema=None, parameter_names=None):
+def existing_udf(name,
+                 input_types,
+                 output_type,
+                 schema=None,
+                 parameter_names=None):
     """Create a ibis function that refers to an existing Postgres UDF already
     defined in database
 
     Parameters
     ----------
     name: str
-    input_type : List[DataType]
+    input_types : List[DataType]
     output_type : DataType
     schema: str - optionally specify the schema that the UDF is defined in
     parameter_names: str - give names to the arguments of the UDF
@@ -49,13 +102,13 @@ def existing_udf(name, input_type, output_type, schema=None, parameter_names=Non
         The wrapped function
     """
     if parameter_names is None:
-        parameter_names = ['v{}'.format(i) for i in range(len(input_type))]
+        parameter_names = ['v{}'.format(i) for i in range(len(input_types))]
     else:
-        assert len(input_type) == len(parameter_names)
+        assert len(input_types) == len(parameter_names)
 
     udf_node_fields = collections.OrderedDict([
-        (name, Arg(rlz.value(type)))
-        for name, type in zip(parameter_names, input_type)
+        (name, Arg(rlz.value(type_)))
+        for name, type_ in zip(parameter_names, input_types)
     ] + [
         (
             'output_type',
@@ -85,3 +138,81 @@ def existing_udf(name, input_type, output_type, schema=None, parameter_names=Non
         return node.to_expr()
 
     return wrapped
+
+
+def func_to_udf(conn,
+                python_func,
+                in_types=None,
+                out_type=None,
+                schema=None,
+                name=None,
+                overwrite=False):
+    """Defines a UDF in the database
+
+    Parameters
+    ----------
+    conn: sqlalchemy engine
+    python_func: python function
+    in_types: List[DataType]; if left None, will try to infer datatypes from
+    function signature
+    out_type : DataType
+    schema: str - optionally specify the schema in which to define the UDF
+    name: str - name for the UDF to be defined in database
+    overwrite: bool - replace UDF in database if already exists
+
+    Returns
+    -------
+    wrapper : Callable
+        The ibis UDF object as a wrapped function
+    """
+    if name is None:
+        internal_name = python_func.__name__
+    else:
+        internal_name = name
+    signature = inspect.signature(python_func)
+    parameter_names = signature.parameters.keys()
+    if in_types is None:
+        raise NotImplementedError('inferring in_types not implemented')
+    if out_type is None:
+        raise NotImplementedError('inferring out_type not implemented')
+    replace = ' OR REPLACE ' if overwrite else ''
+    schema_fragment = (schema + '.') if schema else ''
+    template = """CREATE {replace} FUNCTION
+{schema_fragment}{name}({signature})
+RETURNS {return_type}
+LANGUAGE plpythonu
+AS $$
+{func_definition}
+return {internal_name}({args})
+$$;
+"""
+
+    postgres_signature = ', '.join(
+        '{name} {type}'.format(
+            name=name,
+            type=get_sqltype(type_),
+        )
+        for name, type_ in zip(parameter_names, in_types)
+    )
+    return_type = get_sqltype(out_type)
+    func_definition = inspect.getsource(python_func)
+    formatted_sql = template.format(
+        replace=replace,
+        schema_fragment=schema_fragment,
+        name=internal_name,
+        signature=postgres_signature,
+        return_type=return_type,
+        func_definition=func_definition,
+        # for internal_name, need to make sure this works if passing
+        # name parameter
+        internal_name=python_func.__name__,
+        args=', '.join(parameter_names)
+    )
+    conn.execute(formatted_sql)
+    return existing_udf(
+        name=internal_name,
+        input_types=in_types,
+        output_type=out_type,
+        schema=schema,
+        parameter_names=parameter_names
+    )
