bd32b8a98bd53883e1a9d4d286c7db9b45246095
emilyreff7 <50638962+emilyreff7@users.noreply.github.com>
2019-06-26 19:02:54 -0400

    Remove pandas exception for rows_with_max_lookback
    Without this exception external clients will be able to use
    rows_with_max_lookback with pandas
    Author: Emily Reff <emily.reff@twosigma.com>
    
    Closes #1859 from emilyreff7/remove-exception and squashes the following commits:
    
    6364110 [Emily Reff] Remove pandas exception for rows_with_max_lookback

--- ibis/pandas/execution/window.py
+++ ibis/pandas/execution/window.py
@@ -90,10 +90,6 @@ def execute_window_op(
         **kwargs,
     )
 
-    if window.max_lookback is not None:
-        raise NotImplementedError('Rows with max lookback is not implemented '
-                                  'for pandas backend.')
-
     following = window.following
     order_by = window._order_by
 
