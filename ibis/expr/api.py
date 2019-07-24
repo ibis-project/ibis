efbc0c7c56d1e6b60b73a1f2b7d0e0181400db69
Phillip Cloud <cpcloud@gmail.com>
2019-07-24 13:48:14 -0400

    BUG: Fix according to bug in pd.to_datetime when passing the unit flag
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #1893 from cpcloud/pandas-0.25 and squashes the following commits:
    
    45796fbb [Phillip Cloud] BUG: Fix according to bug in pd.to_datetime when passing the unit flag

--- ibis/expr/api.py
+++ ibis/expr/api.py
@@ -3295,15 +3295,15 @@ def between_time(arg, lower, upper, timezone=None):
     -------
     BooleanValue
     """
-
-    if isinstance(arg.op(), ops.Time):
+    op = arg.op()
+    if isinstance(op, ops.Time):
         # Here we pull out the first argument to the underlying Time operation
         # which is by definition (in _timestamp_value_methods) a
         # TimestampValue. We do this so that we can potentially specialize the
         # "between time" operation for timestamp_value_expr.time().between().
         # A similar mechanism is triggered when creating expressions like
         # t.column.distinct().count(), which is turned into t.column.nunique().
-        arg = arg.op().args[0]
+        arg = op.arg
         if timezone is not None:
             arg = arg.cast(dt.Timestamp(timezone=timezone))
         op = ops.BetweenTime(arg, lower, upper)
