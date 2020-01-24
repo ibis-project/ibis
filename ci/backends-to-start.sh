0576de2f9f0fdc23754322d537250a26666482a8
Anatoly Myachev <anatoly.myachev@intel.com>
2020-01-24 13:56:10 +0300

    add documentation for ./ci/backends-to-start

--- ci/backends-to-start.sh
+++ ci/backends-to-start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Among the backends, the script finds those that will need to be launched
+# by the `docker-compose`. The choice depends on comparing each backend with
+# elements inside the SERVICES variable.
+#
+# Usage:
+#  $ ./ci/backends-to-start.sh param1 param2
+# * param1: string of backends
+# * param2: string of backends, which then need to launched by `docker-compose`
+#     (as docker's services) before testing them
+#
+# Example:
+# current_backends=`./ci/backends-to-start.sh "omniscidb impala parquet" "omniscidb impala"`  && echo $current_backends
+#
+# Output:
+# 'omniscidb impala'
+
 # convert strings to arrays
 BACKENDS=($(echo $1))
 SERVICES=($(echo $2))
