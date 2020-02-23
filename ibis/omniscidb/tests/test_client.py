c5c07a09e4ad2aff3149a57094e0021289244dcd
Anatoly Myachev <anatoly.myachev@intel.com>
2020-02-23 19:01:41 +0300

    add test

--- ibis/omniscidb/tests/test_client.py
+++ ibis/omniscidb/tests/test_client.py
@@ -148,3 +148,49 @@ def test_sql(con, sql):
 def test_explain(con, alltypes):
     # execute the expression using SQL query
     con.explain(alltypes)
+
+
+def test_read_csv(con):
+    t_name = "test_read_csv"
+
+    # omnisci specific
+    filename = "/omnisci/test_read_csv.csv"
+
+    con.drop_table(t_name, force=True)
+    schema = ibis.schema(
+        [
+            ('index', 'int64'),
+            ('Unnamed__0', 'int64'),
+            ('id', 'int32'),
+            ('bool_col', 'bool'),
+            ('tinyint_col', 'int16'),
+            ('smallint_col', 'int16'),
+            ('int_col', 'int32'),
+            ('bigint_col', 'int64'),
+            ('float_col', 'float32'),
+            ('double_col', 'double'),
+            ('date_string_col', 'string'),
+            ('string_col', 'string'),
+            ('timestamp_col', 'timestamp'),
+            ('year_', 'int32'),
+            ('month_', 'int32'),
+        ]
+    )
+    con.create_table(t_name, schema=schema)
+
+    # prepare csv file inside omnisci docker container for read_csv test
+    con._execute(
+        "COPY (SELECT * FROM functional_alltypes) TO '{}'".format(filename)
+    )
+
+    try:
+        db = con.database()
+        table = db.table(t_name)
+        table.read_csv(filename, header=False, quoted=True, delimiter=",")
+
+        df_read_csv = table.execute()
+        df_expected = db.table("functional_alltypes").execute()
+
+        pd.testing.assert_frame_equal(df_expected, df_read_csv)
+    finally:
+        con.drop_table(t_name)
