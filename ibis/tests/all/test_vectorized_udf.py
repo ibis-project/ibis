df199e5c9435966cf87e04eeb1596277f718e9b9
Li Jin <ice.xelloss@gmail.com>
2020-05-28 08:37:30 -0400

    FEAT: Move ibis.pandas.udf.reduction and ibis.pandas.udf.analytics to ibis.udf.vectorized namespace (#2210)

--- ibis/tests/all/test_vectorized_udf.py
+++ ibis/tests/all/test_vectorized_udf.py
@@ -16,3 +16,56 @@ def test_elementwise_udf(backend, alltypes, df):
     result = add_one(alltypes['double_col']).execute()
     expected = add_one.func(df['double_col'])
     backend.assert_series_equal(result, expected, check_names=False)
+
+
+@pytest.mark.only_on_backends([Pandas, PySpark])
+@pytest.mark.xfail_unsupported
+def test_valid_kwargs(backend, alltypes, df):
+    # Test different forms of UDF definition
+
+    @elementwise(input_type=[dt.double], output_type=dt.double)
+    def foo1(v):
+        # Basic UDF with kwargs
+        return v + 1
+
+    @elementwise(input_type=[dt.double], output_type=dt.double)
+    def foo2(v, *, amount):
+        # UDF with keyword only arguments
+        return v + amount
+
+    @elementwise(input_type=[dt.double], output_type=dt.double)
+    def foo3(v, **kwargs):
+        # UDF with kwargs
+        return v + kwargs.get('amount', 1)
+
+    result = alltypes.mutate(
+        v1=foo1(alltypes['double_col']),
+        v2=foo2(alltypes['double_col'], amount=1),
+        v3=foo2(alltypes['double_col'], amount=2),
+        v4=foo3(alltypes['double_col']),
+        v5=foo3(alltypes['double_col'], amount=2),
+        v6=foo3(alltypes['double_col'], amount=3),
+    ).execute()
+
+    expected = df.assign(
+        v1=df['double_col'] + 1,
+        v2=df['double_col'] + 1,
+        v3=df['double_col'] + 2,
+        v4=df['double_col'] + 1,
+        v5=df['double_col'] + 2,
+        v6=df['double_col'] + 3,
+    )
+
+    backend.assert_frame_equal(result, expected)
+
+
+@pytest.mark.only_on_backends([Pandas, PySpark])
+@pytest.mark.xfail_unsupported
+def test_invalid_kwargs(backend, alltypes):
+    # Test different invalid UDFs
+
+    with pytest.raises(TypeError, match=".*must be defined as keyword only.*"):
+
+        @elementwise(input_type=[dt.double], output_type=dt.double)
+        def foo1(v, amount):
+            return v + 1
