1b432be5660f5f2fcdd7ac2999d117aa0b799eee
Li Jin <ice.xelloss@gmail.com>
2020-06-10 15:30:59 -0400

    Second implementation

--- ibis/pandas/dispatcher.py
+++ ibis/pandas/dispatcher.py
@@ -1,26 +1,21 @@
 from multipledispatch import Dispatcher
 
 
-def _create_dispatcher_func(dispatcher):
-    return lambda _: dispatcher
-
-
 def _create_register_func(dispatcher_funcs, types, **kwargs):
     def _(func):
         for dispatcher_func in dispatcher_funcs:
-            dispatcher_func(None).add(types, func, **kwargs)
+            dispatcher_func.add(types, func, **kwargs)
 
         return func
 
     return _
 
 
-class TwoLevelDispatcher(object):
+class TwoLevelDispatcher(Dispatcher):
     """A faster implementation of Dispatch."""
 
     def __init__(self, name, doc=None):
-        self.name = self.__name__ = name
-        self.doc = doc
+        super().__init__(name, doc)
         # The return value of _first_level_dispatcher(arg0) is a single arg
         # function that returns the dispatcher for type(arg0).
         self._meta_dispatcher = Dispatcher(f'{name}_meta')
@@ -37,15 +32,40 @@ class TwoLevelDispatcher(object):
             if (t,) in self._meta_dispatcher.funcs:
                 dispatcher_func = self._meta_dispatcher.funcs[(t,)]
             else:
-                dispatcher_func = _create_dispatcher_func(
-                    Dispatcher(f"{self.name}_{t.__name__}")
-                )
+                # dispatcher_func = _create_dispatcher_func(
+                # )
+                dispatcher_func = Dispatcher(f"{self.name}_{t.__name__}")
+
                 self._meta_dispatcher.register(t)(dispatcher_func)
 
             dispatcher_funcs.append(dispatcher_func)
 
         return _create_register_func(dispatcher_funcs, types, **kwargs)
 
-    def __call__(self, *args, **kwargs):
-        dispatcher = self._meta_dispatcher(args[0])
-        return dispatcher(*args, **kwargs)
+    def dispatch_iter(self, *types):
+        for dispatcher_func in self._meta_dispatcher.dispatch_iter(types[0]):
+            func = dispatcher_func.dispatch(*types)
+            if func is not None:
+                yield func
+
+    def dispatch(self, *types):
+        try:
+            func = next(self.dispatch_iter(*types))
+            return func
+        except StopIteration:
+            return None
+
+    # def __call__(self, *args, **kwargs):
+    #     types = tuple(type(arg) for arg in args)
+
+    #     try:
+    #         func = self._cache[types]
+    #     except KeyError:
+    #         func = self.dispatch(*types)
+    #         if not func:
+    #             raise NotImplementedError(
+    #                 'Could not find signature for %s: <%s>' %
+    #                 (self.name, str_signature(types)))
+    #         self._cache[types] = func
+
+    #     return func(*args, **kwargs)
