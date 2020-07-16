a846f204191f5e990ab4887ae8b5e0faa389a49a
Sambhav Reddy <sambhavreddy@Sambhavs-MacBook-Air.local>
2020-07-16 16:07:11 -0700

    format

--- ibis/tests/all/test_speedcheck.py
+++ ibis/tests/all/test_speedcheck.py
@@ -4,23 +4,21 @@ import ibis
 import ibis.bigquery as bq
 import ibis.expr.operations as ops
 from ibis.expr.types import TableExpr
-import pytest
 
-@pytest.mark.timeout(10)
+
 def test_large_compile():
     num_columns = 20
     num_joins = 7
-    
+
     names = [f"col_{i}" for i in range(num_columns)]
     schema = ibis.Schema(names, ['string'] * num_columns)
     ibis_client = bq.BigQueryClient.__new__(bq.BigQueryClient)
-    t = TableExpr(ops.SQLQueryResult("select * from t", schema, ibis_client))
-    for i in range(num_joins):
-        t = t.mutate(dummy=ibis.literal(""))
-        t = t.left_join(t, ["dummy"])[[t]]
-    
-    start = datetime.now()
-    t.compile()
-    delta = datetime.now() - start
-    # assert delta.total_seconds() < 10
+    table = TableExpr(ops.SQLQueryResult("select * from t", schema, ibis_client))
+    for _ in range(num_joins):
+        table = table.mutate(dummy=ibis.literal(""))
+        table = table.left_join(table, ["dummy"])[[table]]
 
+    start = datetime.now()
+    table.compile()
+    delta = datetime.now() - start
+    assert delta.total_seconds() < 10
