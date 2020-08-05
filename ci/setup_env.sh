1d4f2db372834da7fb33b53c60b59d3f3e40cf7c
Marc Garcia <garcia.marc@gmail.com>
2020-08-05 18:54:26 +0100

    Moving environment creation to script, dependencies to environment.yml, and trying to fix feedstock error

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
+# Set up conda environment for Ibis in GitHub Actions
+# The base environment of the provided conda is used
+# This script only installs the base dependencies.
+# Dependencies for the backends need to be installed separately.
 
-PYTHON_VERSION="$1"
+PYTHON_VERSION="${1:-3.7}"
 CONDA_PATH="/usr/share/miniconda/bin"
 PATH=${CONDA_PATH}:${PATH}
 
 echo "::add-path::${CONDA_PATH}"
 
-conda update -n base -c anaconda conda
-conda install -n base -c anaconda python=${PYTHON_VERSION}
-conda env update -n base --file=ci/requirements-dev-3.7-main.yml
+conda update -n base -c anaconda --all --yes conda
+conda install -n base -c anaconda --yes  python=${PYTHON_VERSION}
+conda env update -n base --yes --file=environment.yml
 python -m pip install -e .
