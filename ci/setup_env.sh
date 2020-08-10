f139fadcdb8ed9222f5a504da3cbb7157d0e7f78
Marc Garcia <garcia.marc@gmail.com>
2020-08-10 22:21:49 +0100

    Moving backend setup to setup_env

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -5,6 +5,7 @@
 # Dependencies for the backends need to be installed separately.
 
 PYTHON_VERSION="${1:-3.7}"
+BACKENDS="$2"
 CONDA_PATH="/usr/share/miniconda/bin"
 PATH=${CONDA_PATH}:${PATH}
 
@@ -14,3 +15,11 @@ conda update -n base -c anaconda --all --yes conda
 conda install -n base -c anaconda --yes  python=${PYTHON_VERSION}
 conda env update -n base --file=environment.yml
 python -m pip install -e .
+
+if [[ -z "$BACKENDS" ]]; then
+    python ci/datamgr.py download
+    for BACKEND in $BACKENDS; do
+        conda install -n base -c conda-forge --file="ci/deps/$BACKEND.yml"
+        python ci/datamgr.py $BACKEND
+    done
+fi
