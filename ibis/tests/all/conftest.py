d41f67d6f995b28eb7aa14a4fbf5bb9685d572a8
Marc Garcia <garcia.marc@gmail.com>
2020-08-17 22:15:17 +0100

    Fixing errors

--- ibis/tests/all/conftest.py
+++ ibis/tests/all/conftest.py
@@ -129,10 +129,13 @@ params_backend = [
     if backend.__name__.lower() in pytest_backends or not pytest_backends
 ]
 if len(pytest_backends) != len(params_backend):
-    unknown_backends = set(pytest_backends) - set(b.__name__.lower()
-                                                  for b in ALL_BACKENDS)
-    raise ValueError('PYTEST_BACKENDS environment variable contain unknown '
-                     f'backends {unknown_backends}')
+    unknown_backends = set(pytest_backends) - set(
+        b.__name__.lower() for b in ALL_BACKENDS
+    )
+    raise ValueError(
+        'PYTEST_BACKENDS environment variable contain unknown '
+        f'backends {unknown_backends}'
+    )
 
 
 @pytest.fixture(params=params_backend, scope='session')
