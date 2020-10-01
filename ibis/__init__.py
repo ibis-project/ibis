e1ff7da33ad834413b2d298fa5aae9982b89e0fd
Jesús Corrius <jcorrius@gmail.com>
2020-10-01 20:50:00 +0200

    fix import

--- ibis/__init__.py
+++ ibis/__init__.py
@@ -37,7 +37,7 @@ with suppress(ImportError):
 
 with suppress(ImportError):
     # pip install ibis-framework[postgres]
-    import ibis.backends.postgres.api as postgres  # noqa: F401
+    from ibis.backends import postgres  # noqa: F401
 
 with suppress(ImportError):
     # pip install ibis-framework[mysql]
