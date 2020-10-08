a3fa3262cfa66bd1198ac3179fc4382f3abb8990
joy-rosie <joytolia@hotmail.com>
2020-10-08 23:53:22 +0100

    adjust for https://github.com/ibis-project/ibis/pull/2440#discussion_r501868852

--- ibis/backends/pandas/execution/__init__.py
+++ ibis/backends/pandas/execution/__init__.py
@@ -9,3 +9,17 @@ from .structs import *  # noqa: F401,F403
 from .temporal import *  # noqa: F401,F403
 from .timecontext import *  # noqa: F401,F403
 from .window import *  # noqa: F401,F403
+
+__all__ = (
+    'arrays',
+    'decimal',
+    'generic',
+    'join',
+    'maps',
+    'selection',
+    'strings',
+    'structs',
+    'temporal',
+    'timecontext',
+    'window',
+)
