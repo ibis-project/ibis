a20d239cd46cc163ab219278d62b809779dfc63f
joy-rosie <joytolia@hotmail.com>
2020-10-08 23:57:37 +0100

    remove #noqa for https://github.com/ibis-project/ibis/pull/2440#discussion_r501868852

--- ibis/backends/pandas/execution/__init__.py
+++ ibis/backends/pandas/execution/__init__.py
@@ -1,14 +1,14 @@
-from .arrays import *  # noqa: F401,F403
-from .decimal import *  # noqa: F401,F403
-from .generic import *  # noqa: F401,F403
-from .join import *  # noqa: F401,F403
-from .maps import *  # noqa: F401,F403
-from .selection import *  # noqa: F401,F403
-from .strings import *  # noqa: F401,F403
-from .structs import *  # noqa: F401,F403
-from .temporal import *  # noqa: F401,F403
-from .timecontext import *  # noqa: F401,F403
-from .window import *  # noqa: F401,F403
+from .arrays import *
+from .decimal import *
+from .generic import *
+from .join import *
+from .maps import *
+from .selection import *
+from .strings import *
+from .structs import *
+from .temporal import *
+from .timecontext import *
+from .window import *
 
 __all__ = (
     'arrays',
