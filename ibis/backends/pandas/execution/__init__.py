a23b2cf4b4dde2e1442ede3265d1d14dbcb8770b
joy-rosie <joytolia@hotmail.com>
2020-10-09 18:16:32 +0100

    remove __all__ after https://github.com/ibis-project/ibis/pull/2440#discussion_r502410021

--- ibis/backends/pandas/execution/__init__.py
+++ ibis/backends/pandas/execution/__init__.py
@@ -9,17 +9,3 @@ from .structs import *  # noqa: F401,F403
 from .temporal import *  # noqa: F401,F403
 from .timecontext import *  # noqa: F401,F403
 from .window import *  # noqa: F401,F403
-
-__all__ = (
-    'arrays',
-    'decimal',
-    'generic',
-    'join',
-    'maps',
-    'selection',
-    'strings',
-    'structs',
-    'temporal',
-    'timecontext',
-    'window',
-)
