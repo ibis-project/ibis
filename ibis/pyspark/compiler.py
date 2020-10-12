4fa04e70fc41d78bca9def69bcd8a75ba2f7c931
Javier León <jel1284@gmail.com>
2020-10-12 20:46:41 -0300

    Add missing import on pyspark

--- ibis/pyspark/compiler.py
+++ ibis/pyspark/compiler.py
@@ -10,6 +10,7 @@ from pyspark.sql.functions import PandasUDFType, pandas_udf
 import ibis.common.exceptions as com
 import ibis.expr.datatypes as dtypes
 import ibis.expr.operations as ops
+import ibis.expr.types as ir
 import ibis.expr.types as types
 from ibis import interval
 from ibis.backends.pandas.execution import execute
