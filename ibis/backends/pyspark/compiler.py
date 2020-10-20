e6642323cfa3b6ab16dfb58a26d151650c382489
Li Jin <ice.xelloss@gmail.com>
2020-10-20 16:24:32 -0400

    Fix tests

--- ibis/backends/pyspark/compiler.py
+++ ibis/backends/pyspark/compiler.py
@@ -3,7 +3,6 @@ import enum
 import functools
 import operator
 
-import pandas as pd
 import pyspark.sql.functions as F
 from pyspark.sql import Window
 from pyspark.sql.functions import PandasUDFType, pandas_udf
@@ -1648,12 +1647,11 @@ def compile_not_null(t, expr, scope, timecontext, **kwargs):
 # ------------------------- User defined function ------------------------
 
 
-def _wrap_elementwise_func(func, output_cols):
+def _wrap_struct_func(func, output_cols):
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
         result = func(*args, **kwargs)
-        if isinstance(result, pd.DataFrame):
-            result.columns = output_cols
+        result.columns = output_cols
         return result
 
     return wrapped
@@ -1663,7 +1661,11 @@ def _wrap_elementwise_func(func, output_cols):
 def compile_elementwise_udf(t, expr, scope, timecontext, **kwargs):
     op = expr.op()
     spark_output_type = spark_dtype(op._output_type)
-    func = _wrap_elementwise_func(op.func, spark_output_type.names)
+    if isinstance(expr, (types.StructColumn, types.DestructColumn)):
+        func = _wrap_struct_func(op.func, spark_output_type.names)
+    else:
+        func = op.func
+
     spark_udf = pandas_udf(func, spark_output_type, PandasUDFType.SCALAR)
     func_args = (t.translate(arg, scope, timecontext) for arg in op.func_args)
     return spark_udf(*func_args)
