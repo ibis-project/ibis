1651fe9c98de6872ee9b1b644b27a73095389c7c
Edoardo Abati <abati.edoardo@gmail.com>
2020-10-30 13:28:18 +0100

    do not add conda to path if CONDA variable not present

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -7,26 +7,31 @@
 PYTHON_VERSION="${1:-3.7}"
 BACKENDS="$2"
 
-# Add conda to path
-OS_NAME=$(uname)
-case $OS_NAME in
-    Linux)
-        CONDA_PATH="$CONDA/bin"
-        ;;
-    MINGW*)
-        CONDA_POSIX=$(cygpath -u "$CONDA")
-        CONDA_PATH="$CONDA_POSIX:$CONDA_POSIX/Scripts:$CONDA_POSIX/Library:$CONDA_POSIX/Library/bin:$CONDA_POSIX/Library/mingw-w64/bin"
-        ;;
-    *)
-        echo "$OS_NAME not supported."
-        exit 1
-esac
-PATH=${CONDA_PATH}:${PATH}
-
 echo "PYTHON_VERSION: $PYTHON_VERSION"
 echo "BACKENDS: $BACKENDS"
-# Prepend conda path to system path for the subsequent actions
-echo "${CONDA_PATH}" >> $GITHUB_PATH
+
+if [ -z "$CONDA" ]; then
+    # Add conda to Path
+    OS_NAME=$(uname)
+    case $OS_NAME in
+        Linux)
+            CONDA_PATH="$CONDA/bin"
+            ;;
+        MINGW*)
+            # Windows
+            CONDA_POSIX=$(cygpath -u "$CONDA")
+            CONDA_PATH="$CONDA_POSIX:$CONDA_POSIX/Scripts:$CONDA_POSIX/Library:$CONDA_POSIX/Library/bin:$CONDA_POSIX/Library/mingw-w64/bin"
+            ;;
+        *)
+            echo "$OS_NAME not supported."
+            exit 1
+    esac
+    PATH=${CONDA_PATH}:${PATH}
+    # Prepend conda path to system path for the subsequent GitHub Actions
+    echo "${CONDA_PATH}" >> $GITHUB_PATH
+else
+    echo "Running without adding conda to PATH."
+fi
 
 conda update -n base -c anaconda --all --yes conda
 conda install -n base -c anaconda --yes  python=${PYTHON_VERSION}
