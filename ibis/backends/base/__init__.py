21ef73e5b3fe0b56f039623cb056c568c43e6733
Marc Garcia <garcia.marc@gmail.com>
2021-03-04 17:13:23 -0500

    Fix error when builder doesn't return a list

--- ibis/backends/base/__init__.py
+++ ibis/backends/base/__init__.py
@@ -55,8 +55,10 @@ class BaseBackend(abc.ABC):
         context = self.dialect.make_context(params=params)
         builder = self.builder(expr, context=context)
         query_ast = builder.get_result()
-        # TODO bigquery doesn't use [0]
-        compiled = query_ast[0].compile()
+        # TODO make all builders return a QueryAST object
+        if isinstance(query_ast, list):
+            query_ast = query_ast[0]
+        compiled = query_ast.compile()
         return compiled
 
     def verify(self, expr, params=None):
