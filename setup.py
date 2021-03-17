16c562ff803e9a1f5349f396c013d523d8806e1b
Marc Garcia <garcia.marc@gmail.com>
2021-03-17 12:57:57 -0500

    Requiring minimum sqlalchemy 1.3 (tests fail in 1.2)

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ See http://ibis-project.org
 
 VERSION = sys.version_info.major, sys.version_info.minor
 
-impala_requires = ['hdfs>=2.0.16', 'sqlalchemy>=1.2', 'requests']
+impala_requires = ['hdfs>=2.0.16', 'sqlalchemy>=1.3', 'requests']
 impala_requires.append('impyla[kerberos]>=0.15.0')
 
-sqlite_requires = ['sqlalchemy>=1.2']
+sqlite_requires = ['sqlalchemy>=1.3']
 postgres_requires = sqlite_requires + ['psycopg2']
 mysql_requires = sqlite_requires + ['pymysql']
 
