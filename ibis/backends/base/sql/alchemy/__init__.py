3e658e784c4a2e0b89332ab7820306c0f0533d6a
Marc Garcia <garcia.marc@gmail.com>
2021-04-12 14:49:42 -0500

    Fixing import

--- ibis/backends/base/sql/alchemy/__init__.py
+++ ibis/backends/base/sql/alchemy/__init__.py
@@ -1,5 +1,5 @@
 from .client import AlchemyClient, AlchemyDialect
-from .database import AlchemyDatabaseSchema, AlchemyTable
+from .database import AlchemyDatabase, AlchemyDatabaseSchema, AlchemyTable
 from .datatypes import schema_from_table, table_from_schema, to_sqla_type
 from .query_builder import AlchemyQueryBuilder, build_ast, to_sqlalchemy
 from .registry import (
@@ -23,6 +23,7 @@ __all__ = (
     'AlchemyClient',
     'AlchemyTable',
     'AlchemyDatabaseSchema',
+    'AlchemyDatabase',
     'AlchemyContext',
     'sqlalchemy_operation_registry',
     'sqlalchemy_window_functions_registry',
