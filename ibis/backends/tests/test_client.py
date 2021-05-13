cbc1ddd878727617aaf35799dccc2f7a9c351b49
harsharaj96 <harsharaj96@gmail.com>
2021-05-13 11:29:05 +0530

    Reduction in code for testing insert

--- ibis/backends/tests/test_client.py
+++ ibis/backends/tests/test_client.py
@@ -225,9 +225,7 @@ def test_insert_from_dataframe(con, df):
     assert len(temporary.execute()) == 0
     con.insert('temporary_alltypes', data_obj=records)
 
-    temporary_post_insert = con.table('temporary_alltypes')
-
-    tm.assert_frame_equal(temporary_post_insert.execute(), records)
+    tm.assert_frame_equal(temporary.execute(), records)
 
 
 @pytest.mark.only_on_backends(
@@ -249,10 +247,8 @@ def test_insert_from_table(con, from_table_name):
     assert len(temporary.execute()) == 0
     con.insert('temporary_alltypes', from_table_name=from_table_name)
 
-    temporary_post_insert = con.table('temporary_alltypes')
-
     from_table = con.table(from_table_name)
 
     tm.assert_frame_equal(
-        temporary_post_insert.execute(), from_table.execute()
+        temporary.execute(), from_table.execute()
     )
