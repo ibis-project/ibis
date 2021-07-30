4a9c92d47d2a48d38fc334a75b33223f1d9cf843
Marc Garcia <garcia.marc@gmail.com>
2021-07-30 11:53:59 -0500

    Running pytest errors step when pytest fails, and removing progress from output

--- ci/pytest_errors.sh
+++ ci/pytest_errors.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 # parse the pytest log and print the pytest errors in a concise way
 
-csplit pytest.log "/short test summary info/" '{*}' > /dev/null
-mv xx00 pytest_errors.log
-mv xx01 pytest_summary.log
+if [[ -f "pytest.log" ]]; then
+    csplit pytest.log "/= FAILURES =/" "/= short test summary info =/" '{*}' > /dev/null
+    mv xx00 pytest_progress.log
+    mv xx01 pytest_errors.log
+    mv xx02 pytest_summary.log
 
-head -n1 pytest_summary.log >> pytest_errors.log
-grep "^FAILED \|^ERROR " pytest_summary.log >> pytest_errors.log
-tail -n1 pytest_summary.log >> pytest_errors.log
+    head -n1 pytest_summary.log >> pytest_errors.log
+    grep "^FAILED \|^ERROR " pytest_summary.log >> pytest_errors.log
+    tail -n1 pytest_summary.log >> pytest_errors.log
 
-cat pytest_errors.log
+    cat pytest_errors.log
+fi
