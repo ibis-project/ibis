a3699ff7047d6a05185c7e1274ff4b2236681ac7
Marc Garcia <garcia.marc@gmail.com>
2021-08-21 14:18:46 -0500

    Adding test

--- ibis/backends/tests/test_api.py
+++ ibis/backends/tests/test_api.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_backend_name(backend):
     # backend is the TestConf for the backend
     assert backend.api.name == backend.name()
@@ -5,3 +8,19 @@ def test_backend_name(backend):
 
 def test_version(backend):
     assert isinstance(backend.api.version, str)
+
+
+@pytest.mark.xfail_unsupported
+def test_set_database(backend):
+    databases = backend.api.list_databases()
+    current_database = backend.api.current_database
+    if len(databases) < 2:
+        # If there are no more databases, we set the database
+        # again to the current database. While not a perfect
+        # test, at least this should call the `set_database` code
+        another_database = current_database
+    else:
+        another_database = databases.remove(current_database)
+
+    backend.api.set_database(another_database)
+    assert backend.api.current_database == another_database
