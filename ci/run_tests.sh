f1948dab1386813544645fbfb6abc0409b655ce0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-05 19:28:06 -0400

    refactor: replace PYTEST_EXPRESSION with inline argument

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 # Run the Ibis tests. Two environment variables are considered:
 # - PYTEST_BACKENDS: Space-separated list of backends to run
-# - PYTEST_EXPRESSION: Marker expression, for example "not udf"
 
 TESTS_DIRS="ibis/tests ibis/backends/tests"
 for BACKEND in $PYTEST_BACKENDS; do
@@ -11,13 +10,11 @@ for BACKEND in $PYTEST_BACKENDS; do
 done
 
 echo "TESTS_DIRS: $TESTS_DIRS"
-echo "PYTEST_EXPRESSION: $PYTEST_EXPRESSION"
 
 set -o pipefail
 
 pytest $TESTS_DIRS \
     -q \
-    -m "${PYTEST_EXPRESSION}" \
     -ra \
     --junitxml=junit.xml \
     --cov=ibis \
