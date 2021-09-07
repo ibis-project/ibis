8e2d1466904226ac0b27715cc5acf54783e3acb2
Marc Garcia <garcia.marc@gmail.com>
2021-09-07 12:27:34 -0500

    Remove obsolete test

--- ibis/backends/postgres/tests/test_client.py
+++ ibis/backends/postgres/tests/test_client.py
@@ -107,21 +107,6 @@ def test_metadata_is_per_table():
     assert len(con.meta.tables) == 1
 
 
-def test_schema_table():
-    con = ibis.postgres.connect(
-        host=IBIS_POSTGRES_HOST,
-        database=POSTGRES_TEST_DB,
-        user=IBIS_POSTGRES_USER,
-        password=IBIS_POSTGRES_PASS,
-    )
-
-    # ensure that we can reflect the information schema (which is guaranteed
-    # to exist)
-    schema = con.schema('information_schema')
-
-    assert isinstance(schema['tables'], ir.TableExpr)
-
-
 def test_schema_type_conversion():
     typespec = [
         # name, type, nullable
