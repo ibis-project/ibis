c8551c40fff5fe06d964c449c32250fa7a09ab26
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-07 16:56:46 -0400

    ci: refactor ci

---
+++ ci/merge_and_update_env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Merge environment files and update the corresponding conda environment
+
+set -euo pipefail
+
+if [ "$#" -eq 0 ]; then
+  >&2 echo "error: must provide at least one backend"
+fi
+
+# install conda-merge
+mamba install --name ibis conda-merge
+
+additional_env_files=()
+
+# pull all files associated with input backends
+for backend in "$@"; do
+  env_file="ci/deps/${backend}.yml"
+
+  if [ -f "${env_file}" ]; then
+    additional_env_files+=("$env_file")
+  fi
+done
+
+env_yaml="$(mktemp --suffix=.yml)"
+conda-merge environment.yml "${additional_env_files[@]}" | tee "$env_yaml"
+mamba env update --name ibis --file "$env_yaml"
