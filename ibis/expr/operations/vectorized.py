4783073a3d7b8ba283d4e3090d7054f4a8e2ca49
Krisztián Szűcs <szucs.krisztian@gmail.com>
2021-11-03 13:03:41 +0100

    Use VectorizedUDF base

--- ibis/expr/operations/vectorized.py
+++ ibis/expr/operations/vectorized.py
@@ -1,3 +1,5 @@
+from types import FunctionType, LambdaType
+
 from public import public
 
 from .. import rules as rlz
@@ -7,61 +9,39 @@ from .core import ValueOp, distinct_roots
 from .reductions import Reduction
 
 
+class VectorizedUDF(ValueOp):
+    func = Arg(rlz.instance_of((FunctionType, LambdaType)))
+    func_args = Arg(rlz.list_of(rlz.any))
+    input_type = Arg(rlz.list_of(rlz.datatype))
+    return_type = Arg(rlz.datatype)
+
+    @property
+    def inputs(self):
+        return self.func_args
+
+    def root_tables(self):
+        return distinct_roots(*self.func_args)
+
+
 @public
-class ElementWiseVectorizedUDF(ValueOp):
+class ElementWiseVectorizedUDF(VectorizedUDF):
     """Node for element wise UDF."""
-
-    func = Arg((types.FunctionType, types.LambdaType))
-    func_args = Arg(rlz.list_of(rlz.any))
-    input_type = Arg(rlz.list_of(rlz.datatype))
-    return_type = Arg(rlz.datatype)
-
-    @property
-    def inputs(self):
-        return self.func_args
-
-    def output_type(self):
-        return rlz.shape_like(self.func_args, dtype=self.return_type)
-
-    def root_tables(self):
-        return distinct_roots(*self.func_args)
-
-
-@public
-class ReductionVectorizedUDF(Reduction):
-    """Node for reduction UDF."""
-
-    func = Arg((types.FunctionType, types.LambdaType))
-    func_args = Arg(rlz.list_of(rlz.any))
-    input_type = Arg(rlz.list_of(rlz.datatype))
-    return_type = Arg(rlz.datatype)
-
-    @property
-    def inputs(self):
-        return self.func_args
-
-    def output_type(self):
-        return self.return_type.scalar_type()
-
-    def root_tables(self):
-        return distinct_roots(*self.func_args)
-
-
-@public
-class AnalyticVectorizedUDF(AnalyticOp):
-    """Node for analytics UDF."""
-
-    func = Arg((types.FunctionType, types.LambdaType))
-    func_args = Arg(rlz.list_of(rlz.any))
-    input_type = Arg(rlz.list_of(rlz.datatype))
-    return_type = Arg(rlz.datatype)
-
-    @property
-    def inputs(self):
-        return self.func_args
 
     def output_type(self):
         return self.return_type.column_type()
 
-    def root_tables(self):
-        return distinct_roots(*self.func_args)
+
+@public
+class ReductionVectorizedUDF(VectorizedUDF, Reduction):
+    """Node for reduction UDF."""
+
+    def output_type(self):
+        return self.return_type.scalar_type()
+
+
+@public
+class AnalyticVectorizedUDF(VectorizedUDF, AnalyticOp):
+    """Node for analytics UDF."""
+
+    def output_type(self):
+        return self.return_type.column_type()
