d39e92eefe57e2793579ca216b22c9ed71992682
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-15 06:25:04 -0500

    ci: only run nix builds when relevant files have changed

--- default.nix
+++ default.nix
@@ -4,8 +4,24 @@
 let
   pkgs = import ./nix;
   drv =
-    { poetry2nix, python }:
+    { poetry2nix
+    , python
+    , lib
+    }:
 
+    let
+      backends = [
+        "csv"
+        "dask"
+        "datafusion"
+        "hdf5"
+        "pandas"
+        "parquet"
+        "sqlite"
+      ];
+
+      backendsString = lib.concatStringsSep " " backends;
+    in
     poetry2nix.mkPoetryApplication {
       inherit python;
 
@@ -26,15 +42,38 @@ let
       buildInputs = with pkgs; [ graphviz-nox ];
       checkInputs = with pkgs; [ graphviz-nox ];
 
+      preCheck = ''
+        export PYTEST_BACKENDS="${backendsString}"
+      '';
+
       checkPhase = ''
         runHook preCheck
-        pytest ibis/tests --numprocesses auto
+
+        tempdir="$(mktemp -d)"
+
+        cp -r ${pkgs.ibisTestingData}/* "$tempdir"
+
+        chmod -R u+rwx "$tempdir"
+
+        ln -s "$tempdir" ci/ibis-testing-data
+
+        for backend in ${backendsString}; do
+          python ci/datamgr.py "$backend" &
+        done
+
+        wait
+
+        pytest --numprocesses auto \
+          ibis/tests \
+          ibis/backends/tests \
+          ibis/backends/{${lib.concatStringsSep "," backends}}/tests
+
         runHook postCheck
       '';
 
       inherit doCheck;
 
-      pythonImportsCheck = [ "ibis" ];
+      pythonImportsCheck = [ "ibis" ] ++ (map (backend: "ibis.backends.${backend}") backends);
     };
 in
 pkgs.callPackage drv {
