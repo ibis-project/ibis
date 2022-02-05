890c9b5a0d2915621aa1c4138e83fd6f5583881d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-02-04 19:06:46 -0500

    test: add failing test

--- ibis/backends/datafusion/tests/test_client.py
+++ ibis/backends/datafusion/tests/test_client.py
@@ -1,6 +1,15 @@
 def test_list_tables(client):
-    assert set(client.list_tables()) == {
-        'awards_players',
-        'batting',
-        'functional_alltypes',
-    }
+    tables = set(client.list_tables())
+    assert {'awards_players', 'batting', 'functional_alltypes'} <= tables
+
+
+def test_register_csv(client, data_directory):
+    path = data_directory / 'functional_alltypes.csv'
+    client.register_csv("ft_csv", path)
+    assert "ft_csv" in client.list_tables()
+
+
+def test_register_parquet(client, data_directory):
+    path = data_directory / 'functional_alltypes.parquet'
+    client.register_parquet("ft_parquet", path)
+    assert "ft_parquet" in client.list_tables()
