ibis-project/ibis

commit 1f2b3fae8f8259da64004e1efc3849c59939081a

refactor(ir): make Annotable immutable
Annotable instances are now truly immutable.

BREAKING CHANGE: Relation-based `execute_node` dispatch rules must now accept tuples of expressions.

ibis/expr/operations/relations.py
@@ -136,7 +136,7 @@ def _clean_join_predicates(left, right, predicates):
         result.extend(preds)
 
     _validate_join_predicates(left, right, result)
-    return result
+    return tuple(result)
 
 
 def _validate_join_predicates(left, right, predicates):
@@ -157,13 +157,16 @@ def _validate_join_predicates(left, right, predicates):
 class Join(TableNode):
     left = rlz.table
     right = rlz.table
-    predicates = rlz.optional(rlz.list_of(rlz.boolean), default=[])
+    # TODO(kszucs): convert to proper predicate rules
+    predicates = rlz.optional(lambda x, this: x, default=())
 
-    def __init__(self, left, right, predicates):
+    def __init__(self, left, right, predicates, **kwargs):
         left, right, predicates = _make_distinct_join_predicates(
             left, right, predicates
         )
-        super().__init__(left, right, predicates)
+        super().__init__(
+            left=left, right=right, predicates=predicates, **kwargs
+        )
 
     @property
     def schema(self):
@@ -232,36 +235,19 @@ class CrossJoin(Join):
 
 @public
 class AsOfJoin(Join):
-    left = rlz.table
-    right = rlz.table
-    predicates = rlz.list_of(rlz.boolean)
-    by = rlz.optional(
-        rlz.list_of(
-            rlz.one_of(
-                (
-                    rlz.function_of("table"),
-                    rlz.column_from("table"),
-                    rlz.any,
-                )
-            )
-        ),
-        default=[],
-    )
+    # TODO(kszucs): convert to proper predicate rules
+    by = rlz.optional(lambda x, this: x, default=())
     tolerance = rlz.optional(rlz.interval)
 
     def __init__(self, left, right, predicates, by, tolerance):
-        super().__init__(left, right, predicates)
-        self.by = _clean_join_predicates(self.left, self.right, by)
-        self.tolerance = tolerance
-
-        self._validate_args(['by', 'tolerance'])
-
-    def _validate_args(self, args: list[str]):
-        # this should be removed altogether
-        for arg in args:
-            argument = self.__signature__.parameters[arg]
-            value = argument.validate(self, getattr(self, arg))
-            setattr(self, arg, value)
+        by = _clean_join_predicates(left, right, by)
+        super().__init__(
+            left=left,
+            right=right,
+            predicates=predicates,
+            by=by,
+            tolerance=tolerance,
+        )
 
 
 @public
@@ -269,13 +255,14 @@ class SetOp(TableNode, sch.HasSchema):
     left = rlz.table
     right = rlz.table
 
-    def _validate(self):
-        if not self.left.schema().equals(self.right.schema()):
+    def __init__(self, left, right, **kwargs):
+        if not left.schema().equals(right.schema()):
             raise com.RelationError(
                 'Table schemas must be equal for set operations'
             )
+        super().__init__(left=left, right=right, **kwargs)
 
-    @cached_property
+    @property
     def schema(self):
         return self.left.schema()
 
@@ -322,7 +309,7 @@ class Limit(TableNode):
 class SelfReference(TableNode, sch.HasSchema):
     table = rlz.table
 
-    @cached_property
+    @property
     def schema(self):
         return self.table.schema()
 
@@ -351,9 +338,9 @@ class Selection(TableNode, sch.HasSchema):
                 )
             )
         ),
-        default=[],
+        default=(),
     )
-    predicates = rlz.optional(rlz.list_of(rlz.boolean), default=[])
+    predicates = rlz.optional(rlz.list_of(rlz.boolean), default=())
     sort_keys = rlz.optional(
         rlz.list_of(
             rlz.one_of(
@@ -385,21 +372,28 @@ class Selection(TableNode, sch.HasSchema):
                 )
             )
         ),
-        default=[],
+        default=(),
     )
 
-    def _validate(self):
+    def __init__(self, table, selections, predicates, sort_keys):
         from ibis.expr.analysis import FilterValidator
 
         # Need to validate that the column expressions are compatible with the
         # input table; this means they must either be scalar expressions or
         # array expressions originating from the same root table expression
-        dependent_exprs = self.selections + self.sort_keys
-        self.table._assert_valid(dependent_exprs)
+        dependent_exprs = selections + sort_keys
+        table._assert_valid(dependent_exprs)
 
         # Validate predicates
-        validator = FilterValidator([self.table])
-        validator.validate_all(self.predicates)
+        validator = FilterValidator([table])
+        validator.validate_all(predicates)
+
+        super().__init__(
+            table=table,
+            selections=selections,
+            predicates=predicates,
+            sort_keys=sort_keys,
+        )
 
         # Validate no overlapping columns in schema
         assert self.schema
@@ -581,7 +575,7 @@ class Aggregation(TableNode, sch.HasSchema):
             ),
             flatten=True,
         ),
-        default=[],
+        default=(),
     )
     by = rlz.optional(
         rlz.list_of(
@@ -593,7 +587,7 @@ class Aggregation(TableNode, sch.HasSchema):
                 )
             )
         ),
-        default=[],
+        default=(),
     )
     having = rlz.optional(
         rlz.list_of(
@@ -606,9 +600,9 @@ class Aggregation(TableNode, sch.HasSchema):
                 )
             ),
         ),
-        default=[],
+        default=(),
     )
-    predicates = rlz.optional(rlz.list_of(rlz.boolean), default=[])
+    predicates = rlz.optional(rlz.list_of(rlz.boolean), default=())
     sort_keys = rlz.optional(
         rlz.list_of(
             rlz.one_of(
@@ -640,26 +634,31 @@ class Aggregation(TableNode, sch.HasSchema):
                 )
             )
         ),
-        default=[],
+        default=(),
     )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if not self.by:
-            self.sort_keys.clear()
-
-    def _validate(self):
+    def __init__(self, table, metrics, by, having, predicates, sort_keys):
         from ibis.expr.analysis import FilterValidator
 
         # All non-scalar refs originate from the input table
-        all_exprs = self.metrics + self.by + self.having + self.sort_keys
-        self.table._assert_valid(all_exprs)
+        all_exprs = metrics + by + having + sort_keys
+        table._assert_valid(all_exprs)
 
         # Validate predicates
-        validator = FilterValidator([self.table])
-        validator.validate_all(self.predicates)
+        validator = FilterValidator([table])
+        validator.validate_all(predicates)
 
+        if not by:
+            sort_keys = tuple()
+
+        super().__init__(
+            table=table,
+            metrics=metrics,
+            by=by,
+            having=having,
+            predicates=predicates,
+            sort_keys=sort_keys,
+        )
         # Validate schema has no overlapping columns
         assert self.schema
 
@@ -723,11 +722,12 @@ class Distinct(TableNode, sch.HasSchema):
 
     table = rlz.table
 
-    def _validate(self):
+    def __init__(self, table):
         # check whether schema has overlapping columns or not
-        assert self.schema
+        assert table.schema()
+        super().__init__(table=table)
 
-    @cached_property
+    @property
     def schema(self):
         return self.table.schema()
 
@@ -766,7 +766,18 @@ class FillNa(TableNode, sch.HasSchema):
         )
     )
 
-    @cached_property
+    def __init__(self, table, replacements, **kwargs):
+        super().__init__(
+            table=table,
+            replacements=(
+                replacements
+                if not isinstance(replacements, collections.abc.Mapping)
+                else util.frozendict(replacements)
+            ),
+            **kwargs,
+        )
+
+    @property
     def schema(self):
         return self.table.schema()
 
@@ -777,9 +788,9 @@ class DropNa(TableNode, sch.HasSchema):
 
     table = rlz.table
     how = rlz.isin({'any', 'all'})
-    subset = rlz.optional(rlz.list_of(rlz.column_from("table")), default=[])
+    subset = rlz.optional(rlz.list_of(rlz.column_from("table")), default=())
 
-    @cached_property
+    @property
     def schema(self):
         return self.table.schema()
 

ibis/expr/types/relations.py
@@ -442,14 +442,13 @@ class TableExpr(Expr):
         TableExpr
             Sorted table
         """
-        return (
-            self.op()
-            .sort_by(
-                self,
-                [] if sort_exprs is None else util.promote_list(sort_exprs),
-            )
-            .to_expr()
-        )
+        if isinstance(sort_exprs, tuple):
+            sort_exprs = [sort_exprs]
+        elif sort_exprs is None:
+            sort_exprs = []
+        else:
+            sort_exprs = util.promote_list(sort_exprs)
+        return self.op().sort_by(self, sort_exprs).to_expr()
 
     def union(
         self,

.gitignore
@@ -98,3 +98,4 @@ ci/udf/build.ninja
 junit.xml
 spark-warehouse
 docs/backends/support_matrix.csv
+__pycache__

ibis/backends/base/sql/alchemy/__init__.py
@@ -354,7 +354,12 @@ class BaseAlchemyBackend(BaseSQLBackend):
 
     def _sqla_table_to_expr(self, table: sa.Table) -> ir.TableExpr:
         schema = self._schemas.get(table.name)
-        node = self.table_class(table, self, schema)
+        node = self.table_class(
+            source=self,
+            sqla_table=table,
+            name=table.name,
+            schema=schema,
+        )
         return self.table_expr_class(node)
 
     def table(

ibis/backends/base/sql/alchemy/database.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
+from typing import Hashable, MutableMapping
+
 import ibis.expr.operations as ops
+import ibis.expr.rules as rlz
 import ibis.expr.schema as sch
 from ibis.backends.base import Database
 
@@ -11,12 +16,28 @@ class AlchemyDatabase(Database):
 
 
 class AlchemyTable(ops.DatabaseTable):
-    def __init__(self, table, source, schema=None):
-        schema = sch.infer(table, schema=schema)
-        super().__init__(table.name, schema, source)
-        self.sqla_table = table
+    sqla_table = rlz.instance_of(object)
+    name = rlz.optional(rlz.instance_of(str), default=None)
+    schema = rlz.optional(rlz.instance_of(sch.Schema), default=None)
 
-    def __getstate__(self):
-        d = super().__getstate__()
-        d['sqla_table'] = self.sqla_table
-        return d
+    def __init__(self, source, sqla_table, name, schema):
+        if name is None:
+            name = sqla_table.name
+        if schema is None:
+            schema = sch.infer(sqla_table, schema=schema)
+        super().__init__(
+            name=name, schema=schema, sqla_table=sqla_table, source=source
+        )
+
+    # TODO(cpcloud): implement this as __component_eq__ after #3621
+    def equals(
+        self,
+        other: AlchemyTable,
+        cache: MutableMapping[Hashable, bool] | None = None,
+    ) -> bool:
+        return (
+            type(self) == type(other)
+            and self.name == other.name
+            and self.source == other.source
+            and self.schema.equals(other.schema, cache=cache)
+        )

ibis/backends/clickhouse/registry.py
@@ -378,11 +378,19 @@ def _literal(translator, expr):
             value = value.strftime('%Y-%m-%d')
         return f"toDate('{value!s}')"
     elif isinstance(expr, ir.ArrayValue):
-        return str(list(value))
+        return str(list(_tuple_to_list(value)))
     elif isinstance(expr, ir.SetScalar):
         return '({})'.format(', '.join(map(repr, value)))
     else:
         raise NotImplementedError(type(expr))
+
+
+def _tuple_to_list(t: tuple):
+    for element in t:
+        if util.is_iterable(element):
+            yield list(_tuple_to_list(element))
+        else:
+            yield element
 
 
 class _CaseFormatter:

ibis/backends/dask/execution/aggregations.py
@@ -27,18 +27,34 @@ from .util import coerce_to_output, safe_concat
 
 # TODO - aggregations - #2553
 # Not all code paths work cleanly here
-@execute_node.register(ops.Aggregation, dd.DataFrame)
+@execute_node.register(
+    ops.Aggregation,
+    dd.DataFrame,
+    tuple,
+    tuple,
+    tuple,
+    tuple,
+    tuple,
+)
 def execute_aggregation_dataframe(
-    op, data, scope=None, timecontext: Optional[TimeContext] = None, **kwargs
+    op,
+    data,
+    metrics,
+    by,
+    having,
+    predicates,
+    sort_keys,
+    scope=None,
+    timecontext: Optional[TimeContext] = None,
+    **kwargs
 ):
-    assert op.metrics, 'no metrics found during aggregation execution'
+    assert metrics, 'no metrics found during aggregation execution'
 
-    if op.sort_keys:
+    if sort_keys:
         raise NotImplementedError(
             'sorting on aggregations not yet implemented'
         )
 
-    predicates = op.predicates
     if predicates:
         predicate = functools.reduce(
             operator.and_,
@@ -51,9 +67,9 @@ def execute_aggregation_dataframe(
 
     columns = {}
 
-    if op.by:
+    if by:
         grouping_key_pairs = list(
-            zip(op.by, map(operator.methodcaller('op'), op.by))
+            zip(by, map(operator.methodcaller('op'), by))
         )
         grouping_keys = [
             by_op.name
@@ -75,7 +91,7 @@ def execute_aggregation_dataframe(
     scope = scope.merge_scope(Scope({op.table.op(): source}, timecontext))
 
     pieces = []
-    for metric in op.metrics:
+    for metric in metrics:
         piece = execute(metric, scope=scope, timecontext=timecontext, **kwargs)
         piece = coerce_to_output(piece, metric)
         pieces.append(piece)
@@ -86,15 +102,15 @@ def execute_aggregation_dataframe(
     result = safe_concat(pieces)
 
     # If grouping, need a reset to get the grouping key back as a column
-    if op.by:
+    if by:
         result = result.reset_index()
 
     result.columns = [columns.get(c, c) for c in result.columns]
 
-    if op.having:
+    if having:
         # .having(...) is only accessible on groupby, so this should never
         # raise
-        if not op.by:
+        if not by:
             raise ValueError(
                 'Filtering out aggregation values is not allowed without at '
                 'least one grouping key'
@@ -105,7 +121,7 @@ def execute_aggregation_dataframe(
             operator.and_,
             (
                 execute(having, scope=scope, timecontext=timecontext, **kwargs)
-                for having in op.having
+                for having in having
             ),
         )
         assert len(predicate) == len(

ibis/backends/dask/execution/generic.py
@@ -112,7 +112,7 @@ DASK_DISPATCH_TYPES: TypeRegistrationDict = {
     ops.Difference: [
         ((dd.DataFrame, dd.DataFrame), execute_difference_dataframe_dataframe)
     ],
-    ops.DropNa: [((dd.DataFrame,), execute_node_dropna_dataframe)],
+    ops.DropNa: [((dd.DataFrame, tuple), execute_node_dropna_dataframe)],
     ops.FillNa: [
         ((dd.DataFrame, simple_types), execute_node_fillna_dataframe_scalar),
         ((dd.DataFrame,), execute_node_fillna_dataframe_dict),

commit bb60af34494f8d8c9db0e39094ad8745bd92fbaa

fix: regression in sort_by with resolved_keys
Fix for minor regression introduced in #3619

ibis/expr/operations/relations.py
@@ -828,7 +828,7 @@ class Aggregation(TableNode, sch.HasSchema):
                 by=self.by,
                 having=self.having,
                 predicates=self.predicates,
-                sort_keys=self.sort_keys + resolved_keys,
+                sort_keys=self.sort_keys + tuple(resolved_keys),
             )
 
         return Selection(expr, [], sort_keys=resolved_keys)

ibis/backends/tests/test_aggregation.py
@@ -472,3 +472,10 @@ def test_binds_are_cast(alltypes):
     )
 
     expr.execute()
+
+
+@pytest.mark.notimpl(["datafusion"])
+def test_agg_sort(alltypes):
+    query = alltypes.aggregate(count=alltypes.count())
+    query = query.sort_by(alltypes.year)
+    query.execute()