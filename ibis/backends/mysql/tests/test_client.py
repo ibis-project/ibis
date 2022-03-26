efbd03043097e0165df4bcea3a5834e029c12079
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-03-26 17:04:00 -0400

    build: bump mariadb container version

---
+++ ibis/backends/mysql/tests/test_client.py
@@ -0,0 +1,66 @@
+import pytest
+from pytest import param
+
+import ibis
+import ibis.expr.datatypes as dt
+
+
+@pytest.mark.parametrize(
+    ("mysql_type", "expected_type"),
+    [
+        param(mysql_type, ibis_type, id=mysql_type.lower())
+        for mysql_type, ibis_type in [
+            ("tinyint", dt.int8),
+            ("int1", dt.int8),
+            ("boolean", dt.int8),
+            ("smallint", dt.int16),
+            ("int2", dt.int16),
+            ("mediumint", dt.int32),
+            ("int3", dt.int32),
+            ("int", dt.int32),
+            ("int4", dt.int32),
+            ("integer", dt.int32),
+            ("bigint", dt.int64),
+            ("decimal", dt.Decimal(10, 0)),
+            ("decimal(5, 2)", dt.Decimal(5, 2)),
+            ("dec", dt.Decimal(10, 0)),
+            ("numeric", dt.Decimal(10, 0)),
+            ("fixed", dt.Decimal(10, 0)),
+            ("float", dt.float32),
+            ("double", dt.float64),
+            ("timestamp", dt.Timestamp("UTC")),
+            ("date", dt.date),
+            ("time", dt.time),
+            ("datetime", dt.timestamp),
+            ("year", dt.int16),
+            ("char(32)", dt.string),
+            ("char byte", dt.binary),
+            ("varchar(42)", dt.string),
+            ("mediumtext", dt.string),
+            ("text", dt.string),
+            ("binary(42)", dt.binary),
+            ("varbinary(42)", dt.binary),
+            ("bit(1)", dt.int8),
+            ("bit(9)", dt.int16),
+            ("bit(17)", dt.int32),
+            ("bit(33)", dt.int64),
+            # mariadb doesn't have a distinct json type
+            ("json", dt.string),
+            ("enum('small', 'medium', 'large')", dt.string),
+            ("inet6", dt.string),
+            ("set('a', 'b', 'c', 'd')", dt.Set(dt.string)),
+            ("mediumblob", dt.binary),
+            ("blob", dt.binary),
+            ("uuid", dt.string),
+        ]
+    ],
+)
+def test_get_schema_from_query(con, mysql_type, expected_type):
+    raw_name = ibis.util.guid()
+    name = con.con.dialect.identifier_preparer.quote_identifier(raw_name)
+    con.raw_sql(
+        f"CREATE TEMPORARY TABLE {name} (x {mysql_type}, y {mysql_type})"
+    )
+    expected_schema = ibis.schema(dict(x=expected_type, y=expected_type))
+    result_schema = con._get_schema_using_query(f"SELECT * FROM {name}")
+    assert result_schema == expected_schema
