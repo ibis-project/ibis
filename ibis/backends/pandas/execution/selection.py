d629763ac7de18379d1dc76f7feb7ab21c855eb9
Gil Forsyth <gil@forsyth.dev>
2022-04-04 17:15:40 -0400

    fix(pandas): don't reassign the same column to silence SettingWithCopyWarning warning
    The warning is valid in many contexts, but it's spurious here and it's
    quite loud.

--- ibis/backends/pandas/execution/selection.py
+++ ibis/backends/pandas/execution/selection.py
@@ -333,7 +333,8 @@ def build_df_from_selection(
             renamed_cols[from_col] = to_cols[0]
         else:
             for new_col in to_cols:
-                result[new_col] = result.loc[:, from_col]
+                if from_col != new_col:
+                    result[new_col] = result[from_col]
 
     if renamed_cols:
         result = result.rename(columns=renamed_cols)
