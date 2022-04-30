8ae53b66d455b239a0e7323f44dec218771fe6e0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-04-30 12:55:28 -0400

    ci: verify version in built wheel during prepare phase

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p poetry nix -i bash
+#!nix-shell -p gnugrep unzip poetry nix -i bash
 # shellcheck shell=bash
 
 set -euo pipefail
 
+version="${1}"
+
 # set version
-poetry version "$1"
+poetry version "$version"
 
 ./dev/poetry2setup -o setup.py
 
 # build artifacts
 poetry build
+
+# ensure that the built wheel has the correct version number
+unzip -p "dist/ibis_framework-${version}-py3-none-any.whl" ibis/__init__.py | grep -q "__version__ = \"$version\""
