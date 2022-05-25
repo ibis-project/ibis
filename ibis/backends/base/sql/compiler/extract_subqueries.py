50cc15f441982ee4703244ea25cc57579cb1a4c8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-25 12:37:38 -0400

    refactor: traverse nodes not exprs

--- ibis/backends/base/sql/compiler/extract_subqueries.py
+++ ibis/backends/base/sql/compiler/extract_subqueries.py
@@ -1,85 +1,88 @@
+from __future__ import annotations
+
 from collections import Counter
+from typing import TYPE_CHECKING, Sequence
 
 import ibis.expr.operations as ops
 import ibis.expr.types as ir
+
+if TYPE_CHECKING:
+    from ibis.backends.base.sql.compiler import SelectBuilder
 
 
 class ExtractSubqueries:
     __slots__ = "query", "counts"
 
-    def __init__(self, query):
+    def __init__(self, query) -> None:
         self.query = query
         self.counts = Counter()
 
     @classmethod
-    def extract(cls, select_stmt):
+    def extract(cls, select_stmt: SelectBuilder):
         helper = cls(select_stmt)
         return helper.get_result()
 
-    def get_result(self):
+    def get_result(self) -> Sequence[ir.Table]:
         query = self.query
         if query.table_set is not None:
-            self.visit(query.table_set)
+            self.visit(query.table_set.op())
 
         for clause in query.filters:
-            self.visit(clause)
+            self.visit(clause.op())
 
         return [op.to_expr() for op, count in self.counts.items() if count > 1]
 
-    def observe(self, expr):
-        self.counts[expr.op()] += 1
+    def observe(self, node: ops.Node) -> None:
+        self.counts[node] += 1
 
-    def visit(self, expr):
-        node = expr.op()
-
+    def visit(self, node: ops.Node) -> None:
         if (
             method := getattr(self, f"visit_{type(node).__name__}", None)
         ) is not None:
-            method(expr)
+            method(node)
         elif isinstance(node, ops.Join):
-            self.visit_join(expr)
+            self.visit_join(node)
         elif isinstance(node, ops.PhysicalTable):
             # do nothing, because physical tables can be referred to by name
             pass
         elif isinstance(node, ops.TableNode):
-            for arg in node.flat_args():
-                if isinstance(arg, ir.Table):
+            for arg in node._flat_ops:
+                if isinstance(arg, ops.TableNode):
                     self.visit(arg)
-            self.observe(expr)
+            self.observe(node)
         elif isinstance(node, ops.Value):
-            for arg in node.flat_args():
-                if isinstance(arg, ir.Expr):
-                    self.visit(arg)
+            for arg in node._flat_ops:
+                self.visit(arg)
         else:
             raise NotImplementedError(type(node))
 
-    def visit_join(self, expr):
-        node = expr.op()
-        self.visit(node.left)
-        self.visit(node.right)
+    def visit_join(self, node: ir.Join) -> None:
+        self.visit(node.left.op())
+        self.visit(node.right.op())
 
-    def visit_Exists(self, expr):
-        node = expr.op()
-        self.visit(node.foreign_table)
+    def visit_ExistsSubquery(
+        self, node: ops.ExistsSubquery | ops.NotExistsSubquery
+    ) -> None:
+        self.visit(node.foreign_table.op())
         for pred in node.predicates:
-            self.visit(pred)
+            self.visit(pred.op())
 
-    visit_NotExistsSubquery = visit_ExistsSubquery = visit_Exists
+    visit_NotExistsSubquery = visit_ExistsSubquery
 
-    def visit_Distinct(self, expr):
-        self.observe(expr)
+    def visit_Distinct(self, node: ops.Distinct) -> None:
+        self.observe(node)
 
-    def visit_Selection(self, expr):
-        self.visit(expr.op().table)
-        self.observe(expr)
+    def visit_Selection(self, node: ops.Selection) -> None:
+        self.visit(node.table.op())
+        self.observe(node)
 
-    def visit_SQLQueryResult(self, expr):
-        self.observe(expr)
+    def visit_SQLQueryResult(self, node: ops.SQLQueryResult) -> None:
+        self.observe(node)
 
-    def visit_TableColumn(self, expr):
-        table = expr.op().table
-        if not self.counts[table.op()]:
+    def visit_TableColumn(self, node: ops.TableColumn) -> None:
+        table = node.table.op()
+        if not self.counts[table]:
             self.visit(table)
 
-    def visit_SelfReference(self, expr):
-        self.visit(expr.op().table)
+    def visit_SelfReference(self, node):
+        self.visit(node.table.op())
