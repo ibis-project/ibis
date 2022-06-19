faf94c6b565d99b3cf333024c89af2e71761aa73
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-19 16:01:41 -0500

    test: remove connection pool tests

--- ibis/backends/impala/tests/test_client.py
+++ ibis/backends/impala/tests/test_client.py
@@ -368,27 +368,3 @@ def test_tables_robust_to_set_database(con, test_data_db, temp_database):
     n = 10
     df = table.limit(n).execute()
     assert len(df) == n
-
-
-def test_connection_pool_size(hdfs, env, test_data_db):
-    client = ibis.impala.connect(
-        port=env.impala_port,
-        hdfs_client=hdfs,
-        host=env.impala_host,
-        database=test_data_db,
-    )
-
-    # the client cursor may or may not be GC'd, so the connection
-    # pool will contain either zero or one cursor
-    assert len(client.con.connection_pool) in (0, 1)
-
-
-def test_connection_pool_size_after_close(hdfs, env, test_data_db):
-    client = ibis.impala.connect(
-        port=env.impala_port,
-        hdfs_client=hdfs,
-        host=env.impala_host,
-        database=test_data_db,
-    )
-    client.close()
-    assert not client.con.connection_pool
