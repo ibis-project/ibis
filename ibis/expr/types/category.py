02d433342ba4e4fd3e9d6e5d8e35ad056216b9ff
Krisztián Szűcs <szucs.krisztian@gmail.com>
2022-08-10 13:13:38 +0200

    refactor(ir): move random() to api.py

--- ibis/expr/types/category.py
+++ ibis/expr/types/category.py
@@ -1,11 +1,33 @@
+from typing import Optional, Sequence
+
 from public import public
 
+import ibis.expr.operations as ops
 from ibis.expr.types.generic import Column, Scalar, Value
+from ibis.expr.types.strings import StringValue
 
 
 @public
 class CategoryValue(Value):
-    pass  # noqa: E701,E302
+    def label(
+        self, labels: Sequence[str], nulls: Optional[str] = None
+    ) -> StringValue:
+        """Format a known number of categories as strings.
+
+        Parameters
+        ----------
+        labels
+            Labels to use for formatting categories
+        nulls
+            How to label any null values among the categories
+
+        Returns
+        -------
+        StringValue
+            Labeled categories
+        """
+        op = ops.CategoryLabel(self, labels, nulls)
+        return op.to_expr()
 
 
 @public
