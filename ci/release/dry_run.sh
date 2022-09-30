a7472952788c873f6cd17be50fa3795fbc3764ee
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-09-30 07:18:45 -0500

    ci: move to `nixos-unstable`
    `nixos-unstable-small` seems to have broken a few core python packages

--- ci/release/dry_run.sh
+++ ci/release/dry_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -I nixpkgs=channel:nixos-unstable-small --pure -p git jq nodejs nix -i bash
+#!nix-shell -I nixpkgs=channel:nixos-unstable --pure -p git jq nodejs nix -i bash
 # shellcheck shell=bash
 
 set -euo pipefail
