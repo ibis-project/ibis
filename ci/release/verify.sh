eb82a2608339d0fd54fbecb907e13580dea73b17
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-09-30 07:18:45 -0500

    ci: move to `nixos-unstable`
    `nixos-unstable-small` seems to have broken a few core python packages

--- ci/release/verify.sh
+++ ci/release/verify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -I nixpkgs=channel:nixos-unstable-small --pure --keep POETRY_PYPI_TOKEN_PYPI -p dyff git poetry yj -i bash
+#!nix-shell -I nixpkgs=channel:nixos-unstable --pure --keep POETRY_PYPI_TOKEN_PYPI -p dyff git poetry yj -i bash
 # shellcheck shell=bash
 
 set -euo pipefail
