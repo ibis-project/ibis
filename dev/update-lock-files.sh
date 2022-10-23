9cb7614f066512365dd193536c138abe47d04565
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-10-23 08:45:29 -0700

    build: relock files with `--no-cache` option

--- dev/update-lock-files.sh
+++ dev/update-lock-files.sh
@@ -8,6 +8,6 @@ export PYTHONHASHSEED=0
 TOP="${1:-$(dirname "$(dirname "$(readlink -f "$0")")")}"
 
 pushd "${TOP}" > /dev/null || exit 1
-poetry lock --no-update
+poetry lock --no-update --no-cache
 poetry export --with dev --with test --with docs --without-hashes --no-ansi > "${TOP}/requirements.txt"
 popd > /dev/null || exit 1
