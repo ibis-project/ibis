bdb51eab12d103ab143edf29303ec45f56c2dcf8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-12-19 09:56:24 -0500

    test(perf): remove unnecessary and slow tests

--- ibis/backends/dask/tests/test_core.py
+++ ibis/backends/dask/tests/test_core.py
@@ -1,11 +1,7 @@
-from typing import Any
-
 import pytest
-from multipledispatch.conflict import ambiguities
 
 import ibis
 import ibis.common.exceptions as com
-import ibis.expr.datatypes as dt
 import ibis.expr.operations as ops
 from ibis.backends.pandas.dispatch import execute_node as pandas_execute_node
 from ibis.expr.scope import Scope
@@ -15,17 +11,12 @@ dd = pytest.importorskip("dask.dataframe")
 from dask.dataframe.utils import tm  # noqa: E402
 
 from ibis.backends.dask import Backend  # noqa: E402
-from ibis.backends.dask.core import execute, is_computable_input  # noqa: E402
+from ibis.backends.dask.core import execute  # noqa: E402
 from ibis.backends.dask.dispatch import (  # noqa: E402
     execute_node,
     post_execute,
     pre_execute,
 )
-
-
-@pytest.mark.parametrize('func', [execute_node, pre_execute, post_execute])
-def test_no_execute_ambiguities(func):
-    assert not ambiguities(func.funcs)
 
 
 def test_from_dataframe(dataframe, ibis_table, core_client):
@@ -106,58 +97,6 @@ def test_post_execute_called_on_joins(dataframe, core_client, ibis_table):
     assert result is not None
     assert len(result.index) > 0
     assert count[0] == 1
-
-
-def test_is_computable_input():
-    class MyObject:
-        def __init__(self, value: float) -> None:
-            self.value = value
-
-        def __getattr__(self, name: str) -> Any:
-            return getattr(self.value, name)
-
-        def __hash__(self) -> int:
-            return hash((type(self), self.value))
-
-        def __eq__(self, other):
-            return (
-                isinstance(other, type(self))
-                and isinstance(self, type(other))
-                and self.value == other.value
-            )
-
-        def __float__(self) -> float:
-            return self.value
-
-    @execute_node.register(ops.Add, int, MyObject)
-    def add_int_my_object(op, left, right, **kwargs):
-        return left + right.value
-
-    # This multimethod must be implemented to play nicely with other value
-    # types like columns and literals. In other words, for a custom
-    # non-expression object to play nicely it must somehow map to one of the
-    # types in ibis/expr/datatypes.py
-    @dt.infer.register(MyObject)
-    def infer_my_object(_, **kwargs):
-        return dt.float64
-
-    @is_computable_input.register(MyObject)
-    def is_computable_input_my_object(_):
-        return True
-
-    one = ibis.literal(1)
-    two = MyObject(2.0)
-    assert is_computable_input(two)
-
-    three = one + two
-    four = three + 1
-    result = execute(four.op())
-    assert result == 4.0
-
-    del execute_node[ops.Add, int, MyObject]
-
-    execute_node.reorder()
-    execute_node._cache.clear()
 
 
 def test_scope_look_up():
