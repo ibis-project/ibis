9d60c1fe0e1fd9f9b372a1cb66e029fb27a94495
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-12-24 10:19:39 -0500

    ci: disable hypothesis too-slow healthcheck
    This seems to make tests flaky in some cases, in particular the macos
    nix builds. We only test macos on nix, so that's likely why we don't see
    it in the non-nix builds.

--- ibis/tests/conftest.py
+++ ibis/tests/conftest.py
@@ -3,9 +3,18 @@ import os
 import hypothesis as h
 
 # setup hypothesis profiles
-h.settings.register_profile('ci', max_examples=1000)
-h.settings.register_profile('dev', max_examples=50)
-h.settings.register_profile('debug', max_examples=10, verbosity=h.Verbosity.verbose)
+h.settings.register_profile(
+    'ci', max_examples=1000, suppress_health_check=[h.HealthCheck.too_slow]
+)
+h.settings.register_profile(
+    'dev', max_examples=50, suppress_health_check=[h.HealthCheck.too_slow]
+)
+h.settings.register_profile(
+    'debug',
+    max_examples=10,
+    verbosity=h.Verbosity.verbose,
+    suppress_health_check=[h.HealthCheck.too_slow],
+)
 
 # load default hypothesis profile, either set HYPOTHESIS_PROFILE environment
 # variable or pass --hypothesis-profile option to pytest, to see the generated
