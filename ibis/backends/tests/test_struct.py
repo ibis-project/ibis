2bbf73cb7ab462af7be46e399ab29e55b6d0a7d0
Krzysztof <krzysztofwitt93@wp.pl>
2023-01-06 16:53:09 +0100

    feat(bigquery): implement ops.StructColumn, ops.ArrayColumn

--- ibis/backends/tests/test_struct.py
+++ ibis/backends/tests/test_struct.py
@@ -87,7 +87,7 @@ def test_null_literal(con, field):
     tm.assert_series_equal(result, expected)
 
 
-@pytest.mark.notimpl(["bigquery", "dask", "pandas", "postgres", "snowflake"])
+@pytest.mark.notimpl(["dask", "pandas", "postgres", "snowflake"])
 def test_struct_column(alltypes, df):
     t = alltypes
     expr = ibis.struct(dict(a=t.string_col, b=1, c=t.bigint_col)).name("s")
