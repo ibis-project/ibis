deb80f2bdb6e00ad653744060a76cb892fbe49de
Krzysztof <krzysztofwitt93@wp.pl>
2023-01-13 18:52:48 +0100

    docs: clean up gen_matrix.py after adding streamlit app

--- gen_matrix.py
+++ gen_matrix.py
@@ -33,65 +33,27 @@ INTERNAL_OPS = {
     ops.ScalarParameter,
 }
 
-GEOSPATIAL_OPS = frozenset(get_leaf_classes(ops.GeoSpatialUnOp)) | frozenset(
-    get_leaf_classes(ops.GeoSpatialBinOp)
-)
-
-CORE_OPS = (frozenset(get_leaf_classes(ops.Value))) - INTERNAL_OPS - GEOSPATIAL_OPS
-
-ICONS = {
-    True: ":material-check-decagram:{ .verified }",
-    False: ":material-cancel:{ .cancel }",
-}
+PUBLIC_OPS = (frozenset(get_leaf_classes(ops.Value))) - INTERNAL_OPS
 
 
-def gen_matrix(basename, possible_ops, raw_data_format=False):
-    if raw_data_format:
-        support = {
-            "operation": [f"{op.__module__}.{op.__name__}" for op in possible_ops]
-        }
-    else:
-        support = {"operation": [f"`{op.__name__}`" for op in possible_ops]}
-
+def main():
+    support = {"operation": [f"{op.__module__}.{op.__name__}" for op in PUBLIC_OPS]}
     support.update(
-        (name, list(map(backend.has_operation, possible_ops)))
+        (name, list(map(backend.has_operation, PUBLIC_OPS)))
         for name, backend in get_backends()
     )
 
     df = pd.DataFrame(support).set_index("operation").sort_index()
-    if not raw_data_format:
-        counts = df.sum().sort_values(ascending=False)
-        counts = counts[counts > 0]
-        num_ops = len(possible_ops)
-        coverage = (
-            counts.map(lambda n: f"_{n} ({round(100 * n / num_ops)}%)_")
-            .to_frame(name="**API Coverage**")
-            .T
-        )
 
-        ops_table = df.loc[:, counts.index].replace(ICONS)
-        table = pd.concat([coverage, ops_table])
-    else:
-        table = df
+    file_path = Path("backends", "raw_support_matrix.csv")
+    local_path = Path(__file__).parent / "docs" / file_path
 
-    file_path = Path("backends", f"{basename}_support_matrix.csv")
-    local_pth = Path(__file__).parent / "docs" / file_path
     buf = io.StringIO()
-    table.to_csv(buf, index_label="Backends")
-    local_pth.write_text(buf.getvalue())
+    df.to_csv(buf, index_label="FullOperation")
+
+    local_path.write_text(buf.getvalue())
     with mkdocs_gen_files.open(file_path, "w") as f:
         f.write(buf.getvalue())
-
-
-def main():
-    gen_matrix(basename="core", possible_ops=CORE_OPS)
-    gen_matrix(
-        basename="geospatial",
-        possible_ops=GEOSPATIAL_OPS,
-    )
-    gen_matrix(
-        basename="raw", possible_ops=CORE_OPS | GEOSPATIAL_OPS, raw_data_format=True
-    )
 
 
 main()
