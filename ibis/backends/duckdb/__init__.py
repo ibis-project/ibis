c77bde7353de003eeb5a15570b8f833a69555e74
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-01-14 05:48:15 -0500

    fix(duckdb): only try to load extensions when necessary for csv

--- ibis/backends/duckdb/__init__.py
+++ ibis/backends/duckdb/__init__.py
@@ -250,7 +250,9 @@ class Backend(BaseAlchemyBackend):
         sql = f"""CREATE OR REPLACE VIEW {quoted_table_name} AS
 SELECT * FROM read_csv({', '.join(args)})"""
 
-        self._load_extensions(["httpfs"])
+        if any(source.startswith(("http://", "https://")) for source in source_list):
+            self._load_extensions(["httpfs"])
+
         with self.begin() as bind:
             bind.execute(sql)
         return self._read(table_name)
