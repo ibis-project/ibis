26ca127686a04d635f5f89a5756014ae6a94447a
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-01-31 10:06:28 -0500

    chore(nix): clean up disabled wheels

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,17 +1,23 @@
-self: super: {
+self: super:
+let
+  disabledMacOSWheels = [
+    "grpcio"
+    "debugpy"
+    "sqlalchemy"
+    "greenlet"
+  ];
+in
+{
   # `wheel` cannot be used as a wheel to unpack itself, since that would
   # require itself (infinite recursion)
   wheel = super.wheel.override { preferWheel = false; };
 
   ipython-genutils = self.ipython_genutils;
-
-  # TODO: Build grpcio from src on darwin because macOS 12_0 wheels won't install
-  # TODO: Remove when the nix version of `pip` is upgraded?
-  grpcio = super.grpcio.override { preferWheel = !self.pkgs.stdenv.isDarwin; };
-
-  # No macOS 12 wheel available as of 2023-01-09
-  debugpy = super.debugpy.override { preferWheel = !self.pkgs.stdenv.isDarwin; };
-
-  # No macOS 12 wheel available as of 2023-01-24 for 1.4.46
-  sqlalchemy = super.sqlalchemy.override { preferWheel = !self.pkgs.stdenv.isDarwin; };
-}
+} // super.lib.listToAttrs (
+  map
+    (name: {
+      inherit name;
+      value = super.${name}.override { preferWheel = !self.pkgs.stdenv.isDarwin; };
+    })
+    disabledMacOSWheels
+)
