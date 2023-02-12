d70b9c0b4305d9e42d55a913c1c3cc985ab514b1
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-02-12 08:53:28 -0500

    feat(trino): implement `ops.Between`

--- ibis/backends/trino/registry.py
+++ ibis/backends/trino/registry.py
@@ -336,8 +336,6 @@ _invalid_operations = {
     # ibis.expr.operations.analytic
     ops.CumulativeOp,
     ops.NTile,
-    # ibis.expr.operations.logical
-    ops.Between,
     # ibis.expr.operations.maps
     ops.MapLength,
     # ibis.expr.operations.reductions
