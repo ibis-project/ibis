28aefd9149d3e87bd08a3f8918795ccea3d67853
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-03-20 07:47:04 -0400

    feat(postgres): implement `ops.Arbitrary`

--- ibis/backends/postgres/__init__.py
+++ ibis/backends/postgres/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import contextlib
 from typing import TYPE_CHECKING, Iterable, Literal
 
 import sqlalchemy as sa
@@ -114,6 +115,37 @@ class Backend(BaseAlchemyBackend):
         engine = sa.create_engine(
             alchemy_url, connect_args=connect_args, poolclass=sa.pool.StaticPool
         )
+
+        # define first/last aggs for ops.Arbitrary
+        #
+        # ignore exceptions so the rest of ibis still works: a user may not
+        # have permissions to define funtions and/or aggregates
+        with engine.begin() as con, contextlib.suppress(Exception):
+            # adapted from https://wiki.postgresql.org/wiki/First/last_%28aggregate%29
+            con.exec_driver_sql(
+                """\
+CREATE OR REPLACE FUNCTION public._ibis_first_agg (anyelement, anyelement)
+RETURNS anyelement
+LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+'SELECT $1';
+
+CREATE OR REPLACE AGGREGATE public._ibis_first (anyelement) (
+  SFUNC = public._ibis_first_agg,
+  STYPE = anyelement,
+  PARALLEL = safe
+);
+
+CREATE OR REPLACE FUNCTION public._ibis_last_agg (anyelement, anyelement)
+RETURNS anyelement
+LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+'SELECT $2';
+
+CREATE OR REPLACE AGGREGATE public._ibis_last (anyelement) (
+  SFUNC = public._ibis_last_agg,
+  STYPE = anyelement,
+  PARALLEL = safe
+);"""
+            )
 
         @sa.event.listens_for(engine, "connect")
         def connect(dbapi_connection, connection_record):
