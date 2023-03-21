51be4592473e99b20dcdb56d67c4fece40a0a1b0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-03-21 17:48:04 -0400

    docs: patch docs build to fix anchor links

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -17,6 +17,21 @@ in
   });
 
   ipython-genutils = self.ipython_genutils;
+
+  mkdocs-jupyter =
+    let
+      linksPatch = self.pkgs.fetchpatch {
+        name = "fix-mkdocs-jupyter-heading-links.patch";
+        url = "https://github.com/danielfrg/mkdocs-jupyter/commit/f3b517580132fc743a34e5d9947731bc4f3c2143.patch";
+        sha256 = "sha256-qcNobdcIziX3pFfnm6vxnhTqow/2VGI/+jbBs9jXkUo=";
+      };
+    in
+    super.mkdocs-jupyter.overridePythonAttrs (_: {
+      postFixup = ''
+        cd $out/${self.python.sitePackages}
+        patch -p1 < "${linksPatch}"
+      '';
+    });
 } // super.lib.listToAttrs (
   map
     (name: {
