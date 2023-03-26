f895e12e26897ffd5e74170b62d65c44163fb044
tokoko <togurg14@freeuni.edu.ge>
2023-03-26 04:03:05 +0400

    style(pyspark): fix ruff errors in tests

--- ibis/backends/pyspark/tests/conftest.py
+++ ibis/backends/pyspark/tests/conftest.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 
 import numpy as np
 import pandas as pd
