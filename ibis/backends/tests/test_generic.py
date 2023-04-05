42d11915275e166f4eb8529256cb2a597c8a5f3c
anjakefala <anja@voltrondata.com>
2023-04-05 16:43:43 -0700

    feat(pandas): support memtable in pandas backend
    Closes #5467

--- ibis/backends/tests/test_generic.py
+++ ibis/backends/tests/test_generic.py
@@ -916,7 +916,7 @@ def test_literal_na(con, dtype):
 
 
 @pytest.mark.notimpl(["datafusion"], raises=com.OperationNotDefinedError)
-@pytest.mark.notimpl(["dask", "pandas"], raises=com.UnboundExpressionError)
+@pytest.mark.notimpl(["dask"], raises=com.UnboundExpressionError)
 def test_memtable_bool_column(backend, con, monkeypatch):
     monkeypatch.setattr(ibis.options, "default_backend", con)
 
@@ -925,7 +925,7 @@ def test_memtable_bool_column(backend, con, monkeypatch):
 
 
 @pytest.mark.notimpl(["datafusion"], raises=com.OperationNotDefinedError)
-@pytest.mark.notimpl(["dask", "pandas"], raises=com.UnboundExpressionError)
+@pytest.mark.notimpl(["dask"], raises=com.UnboundExpressionError)
 @pytest.mark.broken(
     ["druid"],
     raises=AssertionError,
