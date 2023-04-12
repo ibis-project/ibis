86de2a2bb419bcffe54d44d2ea704a0934ee0b3e
anjakefala <anja@voltrondata.com>
2023-04-12 16:09:35 -0700

    docs: improve docstrings for BooleanValue and BoleanColumn

--- ibis/expr/types/logical.py
+++ ibis/expr/types/logical.py
@@ -48,21 +48,184 @@ class BooleanValue(NumericValue):
         return ops.Where(self, true_expr, false_expr).to_expr()
 
     def __and__(self, other: BooleanValue) -> BooleanValue:
+        """Construct a binary AND conditional expression with `self` and `other`.
+
+
+        Parameters
+        ----------
+        self
+            Left operand
+        other
+            Right operand
+
+        Returns
+        -------
+        BooleanValue
+            A Boolean expression
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [[1], [], [42, 42], None]})
+        >>> t.arr.contains(42) & (t.arr.contains(1))
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ And(ArrayContains(arr, 42), ArrayContains(arr, 1)) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                            │
+        ├────────────────────────────────────────────────────┤
+        │ False                                              │
+        │ False                                              │
+        │ False                                              │
+        │ NULL                                               │
+        └────────────────────────────────────────────────────┘
+
+        >>> t.arr.contains(42) & (t.arr.contains(42))
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ And(ArrayContains(arr, 42), ArrayContains(arr, 42)) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                             │
+        ├─────────────────────────────────────────────────────┤
+        │ False                                               │
+        │ False                                               │
+        │ True                                                │
+        │ NULL                                                │
+        └─────────────────────────────────────────────────────┘
+        """
         return _binop(ops.And, self, other)
 
     __rand__ = __and__
 
     def __or__(self, other: BooleanValue) -> BooleanValue:
+        """Construct a binary OR conditional expression with `self` and `other`.
+
+
+        Parameters
+        ----------
+        self
+            Left operand
+        other
+            Right operand
+
+        Returns
+        -------
+        BooleanValue
+            A Boolean expression
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, None]})
+        >>> (t.arr > 1) | (t.arr > 2)
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ Or(Greater(arr, 1), Greater(arr, 2)) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                              │
+        ├──────────────────────────────────────┤
+        │ False                                │
+        │ True                                 │
+        │ True                                 │
+        │ NULL                                 │
+        └──────────────────────────────────────┘
+        """
         return _binop(ops.Or, self, other)
 
     __ror__ = __or__
 
     def __xor__(self, other: BooleanValue) -> BooleanValue:
+        """Construct a binary XOR conditional expression with `self` and `other`.
+
+
+        Parameters
+        ----------
+        self
+            Left operand
+        other
+            Right operand
+
+        Returns
+        -------
+        BooleanValue
+            A Boolean expression
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, None]})
+        >>> t.arr == 2
+        ┏━━━━━━━━━━━━━━━━┓
+        ┃ Equals(arr, 2) ┃
+        ┡━━━━━━━━━━━━━━━━┩
+        │ boolean        │
+        ├────────────────┤
+        │ False          │
+        │ True           │
+        │ False          │
+        │ NULL           │
+        └────────────────┘
+
+        >>> (t.arr > 2)
+        ┏━━━━━━━━━━━━━━━━━┓
+        ┃ Greater(arr, 2) ┃
+        ┡━━━━━━━━━━━━━━━━━┩
+        │ boolean         │
+        ├─────────────────┤
+        │ False           │
+        │ False           │
+        │ True            │
+        │ NULL            │
+        └─────────────────┘
+
+        >>> (t.arr == 2) ^ (t.arr > 2)
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ Xor(Equals(arr, 2), Greater(arr, 2)) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                              │
+        ├──────────────────────────────────────┤
+        │ False                                │
+        │ True                                 │
+        │ True                                 │
+        │ NULL                                 │
+        └──────────────────────────────────────┘
+        """
+
         return _binop(ops.Xor, self, other)
 
     __rxor__ = __xor__
 
     def __invert__(self) -> BooleanValue:
+        """Construct a unary NOT conditional expression with `self`.
+
+
+        Parameters
+        ----------
+        self
+            Operand
+
+        Returns
+        -------
+        BooleanValue
+            A Boolean expression
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [True, False, False, None]})
+        >>> ~t.arr
+        ┏━━━━━━━━━━┓
+        ┃ Not(arr) ┃
+        ┡━━━━━━━━━━┩
+        │ boolean  │
+        ├──────────┤
+        │ False    │
+        │ True     │
+        │ True     │
+        │ NULL     │
+        └──────────┘
+        """
         return self.negate()
 
     @staticmethod
@@ -84,29 +247,178 @@ class BooleanColumn(NumericColumn, BooleanValue):
         ----------
         where
             Optional filter for the aggregation
+
+        Returns
+        -------
+        Boolean Value
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, None]})
+        >>> (t.arr > 2).any()
+        True
+        >>> (t.arr > 4).any()
+        False
+        >>> m = ibis.memtable({"arr": [True, True, True, False]})
+        >>> (t.arr == None).any(where=t.arr != None)
+        False
         """
         import ibis.expr.analysis as an
 
         return an._make_any(self, ops.Any, where=where)
 
     def notany(self, where: BooleanValue | None = None) -> BooleanValue:
-        """Return whether no elements are `True`."""
+        """Return whether no elements are `True`.
+
+        Parameters
+        ----------
+        where
+            Optional filter for the aggregation
+
+        Returns
+        -------
+        Boolean Value
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, 4]})
+        >>> (t.arr > 1).notany()
+        False
+        >>> (t.arr > 4).notany()
+        True
+        """
         import ibis.expr.analysis as an
 
         return an._make_any(self, ops.NotAny, where=where)
 
     def all(self, where: BooleanValue | None = None) -> BooleanScalar:
-        """Return whether all elements are `True`."""
+        """Return whether all elements are `True`.
+
+        Parameters
+        ----------
+        where
+            Optional filter for the aggregation
+
+        Returns
+            -------
+            Boolean Value
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, 4]})
+        >>> (t.arr >= 1).all()
+        True
+        >>> (t.arr > 2).all()
+        False
+        >>> (t.arr == 2).all(where=t.arr == 2)
+        True
+        >>> (t.arr == 2).all(where=t.arr >= 2)
+        False
+
+        """
         return ops.All(self, where=where).to_expr()
 
     def notall(self, where: BooleanValue | None = None) -> BooleanScalar:
-        """Return whether not all elements are `True`."""
+        """Return whether not all elements are `True`.
+
+        Parameters
+        ----------
+        where
+            Optional filter for the aggregation
+
+        Returns
+        -------
+        Boolean Value
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, 4]})
+        >>> (t.arr >= 1).notall()
+        False
+        >>> (t.arr > 2).notall()
+        True
+        """
         return ops.NotAll(self, where=where).to_expr()
 
     def cumany(self) -> BooleanColumn:
-        """Accumulate the `any` aggregate."""
+        """Accumulate the `any` aggregate.
+
+        Returns
+        -------
+        BooleanColumns
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, 4]})
+        >>> ((t.arr > 1) | (t.arr >= 1)).cumany()
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ CumulativeAny(Or(Greater(arr, 1), GreaterEqual(arr, 1))) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                                  │
+        ├──────────────────────────────────────────────────────────┤
+        │ True                                                     │
+        │ True                                                     │
+        │ True                                                     │
+        │ True                                                     │
+        └──────────────────────────────────────────────────────────┘
+
+        >>> ((t.arr > 1) & (t.arr >= 1)).cumany()
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ CumulativeAny(And(Greater(arr, 1), GreaterEqual(arr, 1))) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                                   │
+        ├───────────────────────────────────────────────────────────┤
+        │ False                                                     │
+        │ True                                                      │
+        │ True                                                      │
+        │ True                                                      │
+        └───────────────────────────────────────────────────────────┘
+        """
         return ops.CumulativeAny(self).to_expr()
 
     def cumall(self) -> BooleanColumn:
-        """Accumulate the `all` aggregate."""
+        """Accumulate the `all` aggregate.
+
+        Returns
+        -------
+        BooleanColumns
+
+        Examples
+        --------
+        >>> import ibis
+        >>> ibis.options.interactive = True
+        >>> t = ibis.memtable({"arr": [1, 2, 3, 4]})
+        >>> ((t.arr > 1) & (t.arr >= 1)).cumall()
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ CumulativeAll(And(Greater(arr, 1), GreaterEqual(arr, 1))) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                                   │
+        ├───────────────────────────────────────────────────────────┤
+        │ False                                                     │
+        │ False                                                     │
+        │ False                                                     │
+        │ False                                                     │
+        └───────────────────────────────────────────────────────────┘
+
+        >>> ((t.arr > 0) & (t.arr >= 1)).cumall()
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ CumulativeAll(And(Greater(arr, 0), GreaterEqual(arr, 1))) ┃
+        ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+        │ boolean                                                   │
+        ├───────────────────────────────────────────────────────────┤
+        │ True                                                      │
+        │ True                                                      │
+        │ True                                                      │
+        │ True                                                      │
+        └───────────────────────────────────────────────────────────┘
+        """
         return ops.CumulativeAll(self).to_expr()
