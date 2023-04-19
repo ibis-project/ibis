f85fdf92f3caab7b1c3536269009a02d14b2a532
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-04-19 07:36:45 -0400

    chore: remove unnecessary pydruid override

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -11,10 +11,6 @@ in
   # `wheel` cannot be used as a wheel to unpack itself, since that would
   # require itself (infinite recursion)
   wheel = super.wheel.override { preferWheel = false; };
-
-  pydruid = super.pydruid.overridePythonAttrs (attrs: {
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
-  });
 } // super.lib.listToAttrs (
   map
     (name: {
