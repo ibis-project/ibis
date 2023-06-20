aab3baeca5e5d88c0b2c5ea6081757180d275e55
alexander-beedie <alexander.m.beedie@icloud.com>
2023-06-20 20:15:41 +0400

    feat(polars): add polars support for `identical_to`

--- ibis/backends/tests/test_generic.py
+++ ibis/backends/tests/test_generic.py
@@ -205,9 +205,7 @@ def test_coalesce(con, expr, expected):
 
 
 # TODO(dask) - identicalTo - #2553
-@pytest.mark.notimpl(
-    ["clickhouse", "datafusion", "polars", "dask", "pyspark", "mssql", "druid"]
-)
+@pytest.mark.notimpl(["clickhouse", "datafusion", "dask", "pyspark", "mssql", "druid"])
 def test_identical_to(backend, alltypes, sorted_df):
     sorted_alltypes = alltypes.order_by('id')
     df = sorted_df
@@ -1075,12 +1073,12 @@ def test_pivot_wider(backend):
     reason="arbitrary not implemented in the backend",
 )
 @pytest.mark.notimpl(
-    ["dask", "datafusion", "polars"],
+    ["dask", "datafusion"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement window functions",
 )
 @pytest.mark.notimpl(
-    ["pandas"],
+    ["pandas", "polars"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement ops.WindowFunction",
 )
@@ -1135,12 +1133,12 @@ def test_distinct_on_keep(backend, on, keep):
     reason="arbitrary not implemented in the backend",
 )
 @pytest.mark.notimpl(
-    ["dask", "datafusion", "polars"],
+    ["dask", "datafusion"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement window functions",
 )
 @pytest.mark.notimpl(
-    ["pandas"],
+    ["pandas", "polars"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement ops.WindowFunction",
 )
