be52c2086e86a8b981f297630516452a63488806
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-06-23 07:35:47 -0400

    test(flink): test different hows for mutating join

--- ibis/backends/flink/tests/test_join.py
+++ ibis/backends/flink/tests/test_join.py
@@ -1,12 +1,15 @@
+import pytest
+
 from ibis.backends.flink.compiler.core import translate
 
 
-def test_mutating_join(batting, awards_players, snapshot):
+@pytest.mark.parametrize("how", ["inner", "left", "right", "outer"])
+def test_mutating_join(batting, awards_players, how, snapshot):
     left = batting[batting.yearID == 2015]
     right = awards_players[awards_players.lgID == 'NL'].drop('yearID', 'lgID')
 
     predicate = ['playerID']
 
-    expr = left.join(right, predicate)
+    expr = left.join(right, predicate, how=how)
     result = translate(expr.as_table().op())
     snapshot.assert_match(str(result), "out.sql")
