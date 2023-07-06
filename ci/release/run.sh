c05805fbc344b5d559c57e548d5003fecc95d20e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-07-06 05:18:54 -0400

    fix(release): use @google/semantic-release-replace-plugin@1.2.0 to avoid module loading bug
    Upstream fix is already PR'd here: https://github.com/jpoehnelt/semantic-release-replace-plugin/pull/222

--- ci/release/run.sh
+++ ci/release/run.sh
@@ -10,6 +10,6 @@ nix develop '.#release' -c npx --yes \
   -p "@semantic-release/github" \
   -p "@semantic-release/exec" \
   -p "@semantic-release/git" \
-  -p "@google/semantic-release-replace-plugin" \
+  -p "@google/semantic-release-replace-plugin@1.2.0" \
   -p "conventional-changelog-conventionalcommits" \
   semantic-release --ci
