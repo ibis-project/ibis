d01e0e816ecc1d302782d9ab493ba824919a2180
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-07-07 05:32:27 -0400

    chore(dev-deps): remove upstreamed cairocffi poetry2nix overrides

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -11,16 +11,6 @@ in
   # `wheel` cannot be used as a wheel to unpack itself, since that would
   # require itself (infinite recursion)
   wheel = super.wheel.override { preferWheel = false; };
-  cairocffi = (super.cairocffi.override {
-    # FIXME(cpcloud): fix upstream in poetry2nix by ignoring patches when
-    # preferWheel = true wheel can't be patched with upstream nixpkgs patch, so
-    # build from source
-    preferWheel = false;
-  }).overridePythonAttrs (attrs: {
-    # FIXME(cpcloud): this can be fixed upstream in poetry2nix by adding
-    # flit-core to build-systems.json
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.flit-core ];
-  });
 } // super.lib.listToAttrs (
   map
     (name: {
