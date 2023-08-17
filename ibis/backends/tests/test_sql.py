7f0ce4ca7fa824a6b842efd6e7656ba41b47b857
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-08-17 14:10:10 -0500

    test(flink): mark unnest operation not implemented

--- ibis/backends/tests/test_sql.py
+++ ibis/backends/tests/test_sql.py
@@ -152,7 +152,7 @@ def test_isin_bug(con, snapshot):
     ["sqlite", "mysql", "druid", "impala", "mssql"], reason="no unnest support upstream"
 )
 @pytest.mark.notimpl(
-    ["bigquery", "oracle"],
+    ["bigquery", "oracle", "flink"],
     reason="unnest not yet implemented",
     raises=exc.OperationNotDefinedError,
 )
