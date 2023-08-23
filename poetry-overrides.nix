af186a873e9119f6bbbd40060a7820ea224a5bd8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-08-23 10:27:17 -0400

    chore(deps): add readtime and paginate build systems

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -11,6 +11,12 @@ in
   # `wheel` cannot be used as a wheel to unpack itself, since that would
   # require itself (infinite recursion)
   wheel = super.wheel.override { preferWheel = false; };
+  paginate = super.paginate.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  });
+  readtime = super.readtime.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  });
 } // super.lib.listToAttrs (
   map
     (name: {
