e6494180e18e0d69665dcb02d402ed5b69999f93
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-08-24 05:21:53 -0400

    chore(deps): bump sqlglot version and regen clickhouse snapshot

--- ibis/backends/tests/snapshots/test_sql/test_union_aliasing/clickhouse/out.sql
+++ ibis/backends/tests/snapshots/test_sql/test_union_aliasing/clickhouse/out.sql
@@ -23,30 +23,30 @@ FROM (
           FROM (
             SELECT
               t1.field_of_study,
-              CAST(t1.__pivoted__.1 AS Nullable(TEXT)) AS years,
+              CAST(t1.__pivoted__.1 AS Nullable(String)) AS years,
               CAST(t1.__pivoted__.2 AS Nullable(Int64)) AS degrees
             FROM (
               SELECT
                 t0.field_of_study,
                 arrayJoin(
-                  [  CAST(('1970-71', t0."1970-71") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('1975-76', t0."1975-76") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('1980-81', t0."1980-81") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('1985-86', t0."1985-86") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('1990-91', t0."1990-91") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('1995-96', t0."1995-96") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2000-01', t0."2000-01") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2005-06', t0."2005-06") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2010-11', t0."2010-11") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2011-12', t0."2011-12") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2012-13', t0."2012-13") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2013-14', t0."2013-14") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2014-15', t0."2014-15") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2015-16', t0."2015-16") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2016-17', t0."2016-17") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2017-18', t0."2017-18") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2018-19', t0."2018-19") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                    CAST(('2019-20', t0."2019-20") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64)))]
+                  [  CAST(('1970-71', t0."1970-71") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('1975-76', t0."1975-76") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('1980-81', t0."1980-81") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('1985-86', t0."1985-86") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('1990-91', t0."1990-91") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('1995-96', t0."1995-96") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2000-01', t0."2000-01") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2005-06', t0."2005-06") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2010-11', t0."2010-11") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2011-12', t0."2011-12") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2012-13', t0."2012-13") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2013-14', t0."2013-14") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2014-15', t0."2014-15") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2015-16', t0."2015-16") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2016-17', t0."2016-17") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2017-18', t0."2017-18") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2018-19', t0."2018-19") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                    CAST(('2019-20', t0."2019-20") AS Tuple(years Nullable(String), degrees Nullable(Int64)))]
                 ) AS __pivoted__
               FROM humanities AS t0
             ) AS t1
@@ -85,30 +85,30 @@ FROM (
             FROM (
               SELECT
                 t1.field_of_study,
-                CAST(t1.__pivoted__.1 AS Nullable(TEXT)) AS years,
+                CAST(t1.__pivoted__.1 AS Nullable(String)) AS years,
                 CAST(t1.__pivoted__.2 AS Nullable(Int64)) AS degrees
               FROM (
                 SELECT
                   t0.field_of_study,
                   arrayJoin(
-                    [  CAST(('1970-71', t0."1970-71") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('1975-76', t0."1975-76") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('1980-81', t0."1980-81") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('1985-86', t0."1985-86") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('1990-91', t0."1990-91") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('1995-96', t0."1995-96") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2000-01', t0."2000-01") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2005-06', t0."2005-06") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2010-11', t0."2010-11") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2011-12', t0."2011-12") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2012-13', t0."2012-13") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2013-14', t0."2013-14") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2014-15', t0."2014-15") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2015-16', t0."2015-16") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2016-17', t0."2016-17") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2017-18', t0."2017-18") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2018-19', t0."2018-19") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64))),
-                      CAST(('2019-20', t0."2019-20") AS Tuple(years Nullable(TEXT), degrees Nullable(Int64)))]
+                    [  CAST(('1970-71', t0."1970-71") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('1975-76', t0."1975-76") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('1980-81', t0."1980-81") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('1985-86', t0."1985-86") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('1990-91', t0."1990-91") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('1995-96', t0."1995-96") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2000-01', t0."2000-01") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2005-06', t0."2005-06") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2010-11', t0."2010-11") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2011-12', t0."2011-12") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2012-13', t0."2012-13") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2013-14', t0."2013-14") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2014-15', t0."2014-15") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2015-16', t0."2015-16") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2016-17', t0."2016-17") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2017-18', t0."2017-18") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2018-19', t0."2018-19") AS Tuple(years Nullable(String), degrees Nullable(Int64))),
+                      CAST(('2019-20', t0."2019-20") AS Tuple(years Nullable(String), degrees Nullable(Int64)))]
                   ) AS __pivoted__
                 FROM humanities AS t0
               ) AS t1
