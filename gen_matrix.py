c0cbe09b07b954373acc05977b7d607749e967c7
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-08-28 09:36:57 -0400

    chore: clean up gen_matrix.py usage

--- gen_matrix.py
+++ gen_matrix.py
@@ -1,21 +1,14 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING
 
-import mkdocs_gen_files
 import pandas as pd
 
 import ibis
 import ibis.expr.operations as ops
 
-if TYPE_CHECKING:
-    from collections.abc import Container, Sequence
 
-    from ibis.backends.base import BaseBackend
-
-
-def get_backends(exclude: Container[str] = ()) -> Sequence[tuple[str, BaseBackend]]:
+def get_backends(exclude=()):
     entry_points = sorted(ep.name for ep in ibis.util.backend_entry_points())
     return [
         (backend, getattr(ibis, backend))
@@ -32,20 +25,18 @@ def get_leaf_classes(op):
             yield from get_leaf_classes(child_class)
 
 
-INTERNAL_OPS = {
-    # Never translates into anything
-    ops.UnresolvedExistsSubquery,
-    ops.UnresolvedNotExistsSubquery,
-    ops.ScalarParameter,
-}
-
-PUBLIC_OPS = frozenset(get_leaf_classes(ops.Value)) - INTERNAL_OPS
-
-
 def main():
-    support = {"operation": [f"{op.__module__}.{op.__name__}" for op in PUBLIC_OPS]}
+    internal_ops = {
+        # Never translates into anything
+        ops.UnresolvedExistsSubquery,
+        ops.UnresolvedNotExistsSubquery,
+        ops.ScalarParameter,
+    }
+
+    public_ops = frozenset(get_leaf_classes(ops.Value)) - internal_ops
+    support = {"operation": [f"{op.__module__}.{op.__name__}" for op in public_ops]}
     support.update(
-        (name, list(map(backend.has_operation, PUBLIC_OPS)))
+        (name, list(map(backend.has_operation, public_ops)))
         # exclude flink until https://github.com/apache/flink/pull/23141 is
         # merged and released we also need to roll it into poetry
         for name, backend in get_backends(exclude=("flink",))
@@ -53,8 +44,11 @@ def main():
 
     df = pd.DataFrame(support).set_index("operation").sort_index()
 
-    with mkdocs_gen_files.open(Path("backends", "raw_support_matrix.csv"), "w") as f:
+    with Path(ibis.__file__).parents[1].joinpath(
+        "backends", "raw_support_matrix.csv"
+    ).open(mode="w") as f:
         df.to_csv(f, index_label="FullOperation")
 
 
-main()
+if __name__ == "__main__":
+    main()
