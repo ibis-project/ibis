df981d5595e7643391b378bcfbfe789173621be5
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-09-08 06:34:36 -0400

    docs: bring back backend API documentation

--- docs/backends/_utils.py
+++ docs/backends/_utils.py
@@ -26,9 +26,39 @@ def get_callable(obj, name):
         return obj.functions[name]
 
 
+def find_member_with_docstring(member):
+    """Find the first inherited member with a docstring."""
+    if member.docstring is not None:
+        return member
+
+    cls = member.parent
+    for base in cls.resolved_bases:
+        try:
+            parent_member = get_callable(base, member.name)
+        except KeyError:
+            continue
+        else:
+            if parent_member.docstring is not None:
+                return parent_member
+    return member
+
+
 def render_method(*, member, renderer: MdRenderer) -> Iterator[str]:
-    yield f"{'#' * renderer.crnt_header_level} {member.name} {{ #{member.path} }}"
-    yield renderer.render(member)
+    header_level = renderer.crnt_header_level
+    header = "#" * header_level
+    name = member.name
+    try:
+        params = renderer.render(member.parameters)
+    except AttributeError:
+        params = None
+    yield "\n"
+    yield f"{header} {name} {{ #{member.path} }}"
+    yield "\n"
+    if params is not None:
+        yield f"`{name}({params})`"
+    yield "\n"
+
+    yield renderer.render(find_member_with_docstring(member))
 
 
 def render_methods(obj, *methods: str, level: int) -> None:
