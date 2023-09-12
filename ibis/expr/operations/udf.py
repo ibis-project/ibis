8ee12bf73aee8446e67f8005e62e5f51450c6787
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-09-12 08:16:56 -0400

    feat(udf): add support for builtin aggregate UDFs

--- ibis/expr/operations/udf.py
+++ ibis/expr/operations/udf.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import abc
 import enum
 import functools
 import inspect
 import typing
-from typing import TYPE_CHECKING, Any, Callable, TypeVar
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypeVar
 
 from public import public
 
@@ -36,6 +37,11 @@ class ScalarUDF(ops.Value):
     shape = rlz.shape_like("args")
 
 
+@public
+class AggUDF(ops.Reduction):
+    where: Optional[ops.Value[dt.Boolean]] = None
+
+
 def _wrap(
     wrapper,
     input_type: InputType,
@@ -51,11 +57,117 @@ def _wrap(
     return functools.update_wrapper(wrapper(input_type, fn, *args, **kwargs), fn)
 
 
-S = TypeVar("S", bound=ScalarUDF)
+S = TypeVar("S", bound=ops.Value)
+B = TypeVar("B", bound=ops.Value)
+
+
+class _UDF(abc.ABC):
+    __slots__ = ()
+
+    @property
+    @abc.abstractmethod
+    def _base(self) -> type[B]:
+        """Base class of the UDF."""
+
+    @util.experimental
+    @classmethod
+    def builtin(
+        cls,
+        fn: Callable | None = None,
+        *args: Any,
+        name: str | None = None,
+        schema: str | None = None,
+        **kwargs: Any,
+    ) -> Callable:
+        """Construct a scalar user-defined function that is built-in to the backend.
+
+        Parameters
+        ----------
+        fn
+            The The function to wrap.
+        args
+            Configuration arguments for the UDF.
+        name
+            The name of the UDF in the backend if different from the function name.
+        schema
+            The schema in which the builtin function resides.
+        kwargs
+            Additional configuration arguments for the UDF.
+
+        Examples
+        --------
+        >>> import ibis
+        >>> @ibis.udf.scalar.builtin
+        ... def hamming(a: str, b: str) -> int:
+        ...     '''Compute the Hamming distance between two strings.'''
+        >>> expr = hamming("duck", "luck")
+        >>> con = ibis.connect("duckdb://")
+        >>> con.execute(expr)
+        1
+        """
+        return _wrap(
+            cls._make_wrapper,
+            InputType.BUILTIN,
+            fn,
+            *args,
+            name=name,
+            schema=schema,
+            **kwargs,
+        )
+
+    @classmethod
+    def _make_node(
+        cls,
+        fn: Callable,
+        input_type: InputType,
+        *args,
+        name: str | None = None,
+        schema: str | None = None,
+        **kwargs,
+    ) -> type[S]:
+        """Construct a scalar user-defined function that is built-in to the backend."""
+
+        annotations = typing.get_type_hints(fn)
+        if (return_annotation := annotations.pop("return", None)) is None:
+            raise exc.MissingReturnAnnotationError(fn)
+
+        func_name = name if name is not None else fn.__name__
+
+        fields = {
+            arg_name: Argument(
+                pattern=rlz.ValueOf(annotations.get(arg_name)), default=param.default
+            )
+            for arg_name, param in inspect.signature(fn).parameters.items()
+        } | {
+            "dtype": dt.dtype(return_annotation),
+            "__input_type__": input_type,
+            # must wrap `fn` in a `property` otherwise `fn` is assumed to be a
+            # method
+            "__func__": property(fget=lambda _, fn=fn: fn),
+            "__config__": FrozenDict(args=args, kwargs=FrozenDict(**kwargs)),
+            "__udf_namespace__": schema,
+            "__module__": fn.__module__,
+            "__func_name__": func_name,
+            "__full_name__": ".".join(filter(None, (schema, func_name))),
+        }
+
+        return type(fn.__name__, (cls._base,), fields)
+
+    @classmethod
+    def _make_wrapper(
+        cls, input_type: InputType, fn: Callable, *args: Any, **kwargs: Any
+    ) -> Callable:
+        node = cls._make_node(fn, input_type, *args, **kwargs)
+
+        @functools.wraps(fn)
+        def construct(*args: Any, **kwargs: Any) -> ir.Value:
+            return node(*args, **kwargs).to_expr()
+
+        return construct
 
 
 @public
-class scalar:
+class scalar(_UDF):
     """Scalar user-defined functions.
 
     ::: {.callout-note}
@@ -63,9 +175,12 @@ class scalar:
     :::
     """
 
+    _base = ScalarUDF
+
     @util.experimental
-    @staticmethod
+    @classmethod
     def python(
+        cls,
         fn: Callable | None = None,
         *args: Any,
         name: str | None = None,
@@ -117,7 +232,7 @@ class scalar:
         - [`pyarrow`](./scalar-udfs.qmd#ibis.expr.operations.scalar.pyarrow)
         """
         return _wrap(
-            scalar._make_wrapper,
+            cls._make_wrapper,
             InputType.PYTHON,
             fn,
             *args,
@@ -127,8 +242,9 @@ class scalar:
         )
 
     @util.experimental
-    @staticmethod
+    @classmethod
     def pandas(
+        cls,
         fn: Callable | None = None,
         *args: Any,
         name: str | None = None,
@@ -169,7 +285,7 @@ class scalar:
         - [`pyarrow`](./scalar-udfs.qmd#ibis.expr.operations.scalar.pyarrow)
         """
         return _wrap(
-            scalar._make_wrapper,
+            cls._make_wrapper,
             InputType.PANDAS,
             fn,
             *args,
@@ -179,8 +295,9 @@ class scalar:
         )
 
     @util.experimental
-    @staticmethod
+    @classmethod
     def pyarrow(
+        cls,
         fn: Callable | None = None,
         *args: Any,
         name: str | None = None,
@@ -220,7 +337,7 @@ class scalar:
         - [`pandas`](./scalar-udfs.qmd#ibis.expr.operations.scalar.pandas)
         """
         return _wrap(
-            scalar._make_wrapper,
+            cls._make_wrapper,
             InputType.PYARROW,
             fn,
             *args,
@@ -229,16 +346,16 @@ class scalar:
             **kwargs,
         )
 
+
+class agg(_UDF):
+    __slots__ = ()
+
+    _base = AggUDF
+
     @util.experimental
-    @staticmethod
-    def builtin(
-        fn: Callable | None = None,
-        *args: Any,
-        name: str | None = None,
-        schema: str | None = None,
-        **kwargs: Any,
-    ) -> Callable:
-        """Construct a scalar user-defined function that is built-in to the backend.
+    @classmethod
+    def builtin(cls, *args: Any, **kwargs: Any) -> Callable:
+        """Construct an aggregate user-defined function that is built-in to the backend.
 
         Parameters
         ----------
@@ -256,68 +373,13 @@ class scalar:
         Examples
         --------
         >>> import ibis
-        >>> @ibis.udf.scalar.builtin
-        ... def hamming(a: str, b: str) -> int:
-        ...     '''Compute the Hamming distance between two strings.'''
-        >>> expr = hamming("duck", "luck")
-        >>> con = ibis.connect("duckdb://")
-        >>> con.execute(expr)
-        1
+        >>> ibis.options.interactive = True
+        >>> @ibis.udf.agg.builtin
+        ... def favg(a: float) -> float:
+        ...     '''Compute the average of a column using Kahan summation.'''
+        >>> t = ibis.examples.penguins.fetch()
+        >>> expr = favg(t.bill_length_mm)
+        >>> expr
+        43.9219298245614
         """
-        return _wrap(
-            scalar._make_wrapper,
-            InputType.BUILTIN,
-            fn,
-            *args,
-            name=name,
-            schema=schema,
-            **kwargs,
-        )
-
-    @staticmethod
-    def _make_node(
-        fn: Callable,
-        input_type: InputType,
-        *args,
-        name: str | None = None,
-        schema: str | None = None,
-        **kwargs,
-    ) -> type[S]:
-        """Construct a scalar user-defined function that is built-in to the backend."""
-
-        annotations = typing.get_type_hints(fn)
-        if (return_annotation := annotations.pop("return", None)) is None:
-            raise exc.MissingReturnAnnotationError(fn)
-
-        func_name = name if name is not None else fn.__name__
-
-        fields = {
-            arg_name: Argument(
-                pattern=rlz.ValueOf(annotations.get(arg_name)), default=param.default
-            )
-            for arg_name, param in inspect.signature(fn).parameters.items()
-        } | {
-            "dtype": dt.dtype(return_annotation),
-            "__input_type__": input_type,
-            # must wrap `fn` in a `property` otherwise `fn` is assumed to be a
-            # method
-            "__func__": property(fget=lambda _, fn=fn: fn),
-            "__config__": FrozenDict(args=args, kwargs=FrozenDict(**kwargs)),
-            "__udf_namespace__": schema,
-            "__module__": fn.__module__,
-            "__func_name__": func_name,
-        }
-
-        return type(func_name, (ScalarUDF,), fields)
-
-    @staticmethod
-    def _make_wrapper(
-        input_type: InputType, fn: Callable, *args: Any, **kwargs: Any
-    ) -> Callable:
-        node = scalar._make_node(fn, input_type, *args, **kwargs)
-
-        @functools.wraps(fn)
-        def construct(*args: Any, **kwargs: Any) -> ir.Value:
-            return node(*args, **kwargs).to_expr()
-
-        return construct
+        return super().builtin(*args, **kwargs)
