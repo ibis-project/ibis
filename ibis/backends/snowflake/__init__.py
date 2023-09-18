3dc9ccf13be5e3bf3aa640206630464ca064b3cb
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-09-18 09:45:54 -0400

    chore(snowflake): force column names to match the expression schema

--- ibis/backends/snowflake/__init__.py
+++ ibis/backends/snowflake/__init__.py
@@ -372,6 +372,7 @@ $$""".format(
         if (table := cursor.cursor.fetch_arrow_all()) is None:
             table = schema.to_pyarrow().empty_table()
         df = table.to_pandas(timestamp_as_object=True)
+        df.columns = list(schema.names)
         return SnowflakePandasData.convert_table(df, schema)
 
     def to_pyarrow_batches(
