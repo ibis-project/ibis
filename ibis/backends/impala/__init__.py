8bfe95bccd1477d822e9c88cfca560b9e138175c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-09-19 06:33:43 -0400

    chore: impala fix naming

--- ibis/backends/impala/__init__.py
+++ ibis/backends/impala/__init__.py
@@ -348,10 +348,11 @@ class Backend(BaseSQLBackend):
             tables = [row[0] for row in cursor.fetchall()]
         return self._filter_with_like(tables)
 
-    def fetch_from_cursor(self, cursor, schema):
+    def fetch_from_cursor(self, cursor, schema: sch.Schema) -> pd.DataFrame:
         batches = cursor.fetchall(columnar=True)
         names = [name for name, *_ in cursor.description]
         df = _column_batches_to_dataframe(names, batches)
+        df.columns = list(schema.names)
         if schema:
             return PandasData.convert_table(df, schema)
         return df
