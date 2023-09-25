7619e2f71e81ca9431ac220c8b4222ef5a48788d
mfatihaktas <mfatihaktas@gmail.com>
2023-09-25 11:31:57 -0400

    [skip ci] Cleared test_first_last

--- ibis/backends/tests/test_window.py
+++ ibis/backends/tests/test_window.py
@@ -1083,7 +1083,7 @@ def test_mutate_window_filter(backend, alltypes, df):
 
 @pytest.mark.notimpl(["datafusion", "polars"], raises=com.OperationNotDefinedError)
 @pytest.mark.notimpl(
-    ["datafusion"],
+    ["datafusion", "flink"],
     raises=Exception,
     reason="KeyError: \"Table with name win doesn't exist.",
 )
