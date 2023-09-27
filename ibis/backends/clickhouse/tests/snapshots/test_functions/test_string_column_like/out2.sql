5fc1135c10be50b35c917e84d8f80385c1f3bd7c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-09-27 05:35:56 -0400

    chore: avoid threading `None` where possible

--- ibis/backends/clickhouse/tests/snapshots/test_functions/test_string_column_like/out2.sql
+++ ibis/backends/clickhouse/tests/snapshots/test_functions/test_string_column_like/out2.sql
@@ -1,3 +1,7 @@
 SELECT
-  t0.string_col LIKE 'foo%' OR t0.string_col LIKE '%bar' AS "Or(StringSQLLike(string_col, 'foo%'), StringSQLLike(string_col, '%bar'))"
+  (
+    t0.string_col LIKE 'foo%'
+  ) OR (
+    t0.string_col LIKE '%bar'
+  ) AS "Or(StringSQLLike(string_col, 'foo%'), StringSQLLike(string_col, '%bar'))"
 FROM functional_alltypes AS t0
