72e002dae3ed82371c736acfc01e435754f37664
dlovell <dlovell@gmail.com>
2023-10-04 07:34:38 -0400

    style: fix lints

--- ibis/backends/datafusion/tests/test_connect.py
+++ ibis/backends/datafusion/tests/test_connect.py
@@ -24,10 +24,7 @@ def test_none_config():
 
 
 def test_str_config(name_to_path):
-    config = {
-        name: str(path)
-        for name, path in name_to_path.items()
-    }
+    config = {name: str(path) for name, path in name_to_path.items()}
     conn = ibis.datafusion.connect(config)
     assert sorted(conn.list_tables()) == sorted(name_to_path)
 
@@ -40,7 +37,7 @@ def test_path_config(name_to_path):
 
 def test_context_config(name_to_path):
     ctx = SessionContext()
-    for (name, path) in name_to_path.items():
+    for name, path in name_to_path.items():
         ctx.register_parquet(name, str(path))
     conn = ibis.datafusion.connect(ctx)
     assert sorted(conn.list_tables()) == sorted(name_to_path)
