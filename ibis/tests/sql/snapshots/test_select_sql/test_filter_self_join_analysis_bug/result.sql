933c68a4609f0b2702c196dc0000ef2bd4e86730
Krisztián Szűcs <szucs.krisztian@gmail.com>
2023-10-16 23:13:39 +0200

    refactor(analysis): remove `pushdown_aggregation_filters()`

--- ibis/tests/sql/snapshots/test_select_sql/test_filter_self_join_analysis_bug/result.sql
+++ ibis/tests/sql/snapshots/test_select_sql/test_filter_self_join_analysis_bug/result.sql
@@ -1,19 +1,16 @@
 WITH t0 AS (
-  SELECT t3.`region`, t3.`kind`, sum(t3.`amount`) AS `total`
-  FROM purchases t3
+  SELECT t2.`region`, t2.`kind`, sum(t2.`amount`) AS `total`
+  FROM purchases t2
+  WHERE t2.`kind` = 'bar'
   GROUP BY 1, 2
 ),
 t1 AS (
-  SELECT t0.*
-  FROM t0
-  WHERE t0.`kind` = 'bar'
-),
-t2 AS (
-  SELECT t0.*
-  FROM t0
-  WHERE t0.`kind` = 'foo'
+  SELECT t2.`region`, t2.`kind`, sum(t2.`amount`) AS `total`
+  FROM purchases t2
+  WHERE t2.`kind` = 'foo'
+  GROUP BY 1, 2
 )
-SELECT t2.`region`, t2.`total` - t1.`total` AS `diff`
-FROM t2
-  INNER JOIN t1
-    ON t2.`region` = t1.`region`
+SELECT t1.`region`, t1.`total` - t0.`total` AS `diff`
+FROM t1
+  INNER JOIN t0
+    ON t1.`region` = t0.`region`
