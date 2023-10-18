10a59164877fedcce76cc929985ca8bc01dd7533
Jim Crist-Harif <jcristharif@gmail.com>
2023-10-18 17:33:33 -0500

    feat(clickhouse): add support for timestamp `bucket`

--- ibis/backends/tests/test_temporal.py
+++ ibis/backends/tests/test_temporal.py
@@ -2518,7 +2518,6 @@ def test_delta(con, start, end, unit, expected):
 @pytest.mark.notimpl(
     [
         "bigquery",
-        "clickhouse",
         "dask",
         "datafusion",
         "flink",
@@ -2543,7 +2542,15 @@ def test_delta(con, start, end, unit, expected):
 @pytest.mark.parametrize(
     "kws, pd_freq",
     [
-        ({"milliseconds": 50}, "50ms"),
+        param(
+            {"milliseconds": 50},
+            "50ms",
+            marks=pytest.mark.notimpl(
+                ["clickhouse"],
+                raises=com.UnsupportedOperationError,
+                reason="clickhouse doesn't support sub-second interval precision",
+            ),
+        ),
         ({"seconds": 2}, "2s"),
         ({"minutes": 5}, "300s"),
         ({"hours": 2}, "2h"),
@@ -2560,7 +2567,6 @@ def test_timestamp_bucket(backend, kws, pd_freq):
 @pytest.mark.notimpl(
     [
         "bigquery",
-        "clickhouse",
         "dask",
         "datafusion",
         "flink",
