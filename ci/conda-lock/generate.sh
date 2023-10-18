910f2d73e2232e690ea11c279b2e5c2e0320edd6
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-10-18 04:43:36 -0400

    build: move to unified lockfile

--- ci/conda-lock/generate.sh
+++ ci/conda-lock/generate.sh
@@ -2,23 +2,14 @@
 
 set -euo pipefail
 
-python_version="${1}"
-python_version_file="$(mktemp --suffix=.yml)"
-
-{
-  echo 'name: conda-lock'
-  echo 'dependencies:'
-  echo "  - python=${python_version}"
-} > "${python_version_file}"
-
 extras=(
   -e bigquery
   -e clickhouse
   -e dask
+  -e datafusion
   -e druid
   -e duckdb
-  # this doesn't work on any platform yet (issues with resolving some google deps)
-  # -e geospatial
+  -e geospatial
   -e impala
   -e mssql
   -e mysql
@@ -32,24 +23,25 @@ extras=(
   -e trino
   -e visualization
   -e decompiler
+  -e deltalake
 )
-template="conda-lock/{platform}-${python_version}.lock"
 
-function conda_lock() {
-  local platforms
-  platforms=(--platform "$1" --platform "$2")
-  shift 2
-  conda lock \
-    --file pyproject.toml \
-    --file "${python_version_file}" \
-    --kind explicit \
-    "${platforms[@]}" \
-    --filename-template "${template}" \
-    --filter-extras \
-    --conda="$(which conda)" \
-    --category dev --category test --category docs \
-    "${@}"
-}
+# directory of this script
+top="$(dirname "$(readlink -f -- "$0")")"
 
-conda_lock linux-64 osx-64 "${extras[@]}" -e datafusion
-conda_lock osx-arm64 win-64 "${extras[@]}"
+python_version="${1}"
+shift 1
+
+conda lock \
+  --file pyproject.toml \
+  --file "${top}/versions/python-${python_version}.yml" \
+  --channel conda-forge \
+  --platform linux-64 \
+  --platform osx-64 \
+  --platform osx-arm64 \
+  --platform win-64 \
+  --filter-extras \
+  --mamba \
+  --category dev --category test --category docs \
+  "${extras[@]}" \
+  "${@}"
