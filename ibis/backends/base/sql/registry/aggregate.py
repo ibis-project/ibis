eac200b8c17e0c51636c0c3e54e3b39b50669e47
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-11-01 22:57:15 -0600

    fix(flink): cast argument to integer for reduction

--- ibis/backends/base/sql/registry/aggregate.py
+++ ibis/backends/base/sql/registry/aggregate.py
@@ -1,10 +1,26 @@
 from __future__ import annotations
 
 import ibis
+import ibis.expr.datatypes as dt
 import ibis.expr.operations as ops
 
 
-def _reduction_format(translator, func_name, where, *args):
+def _maybe_cast_bool(translator, op, arg):
+    if (
+        translator._bool_aggs_need_cast_to_int32
+        and isinstance(op, (ops.Sum, ops.Mean, ops.Min, ops.Max))
+        and (dtype := arg.dtype).is_boolean()
+    ):
+        return ops.Cast(arg, dt.Int32(nullable=dtype.nullable))
+    return arg
+
+
+def _reduction_format(translator, op, func_name, where, *args):
+    args = (
+        _maybe_cast_bool(translator, op, arg)
+        for arg in args
+        if isinstance(arg, ops.Node)
+    )
     if where is not None:
         args = (ops.IfElse(where, arg, ibis.NA) for arg in args)
 
@@ -17,7 +33,7 @@ def _reduction_format(translator, func_name, where, *args):
 def reduction(func_name):
     def formatter(translator, op):
         *args, where = op.args
-        return _reduction_format(translator, func_name, where, *args)
+        return _reduction_format(translator, op, func_name, where, *args)
 
     return formatter
 
@@ -29,7 +45,7 @@ def variance_like(func_name):
     }
 
     def formatter(translator, op):
-        return _reduction_format(translator, func_names[op.how], op.where, op.arg)
+        return _reduction_format(translator, op, func_names[op.how], op.where, op.arg)
 
     return formatter
 
