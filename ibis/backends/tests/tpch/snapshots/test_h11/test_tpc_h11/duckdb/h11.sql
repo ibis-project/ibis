30e03c3c10559d61f70c434eaaebd0a997798b0b
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-11-23 12:16:41 -0500

    chore: regen snapshots

--- ibis/backends/tests/tpch/snapshots/test_h11/test_tpc_h11/duckdb/h11.sql
+++ ibis/backends/tests/tpch/snapshots/test_h11/test_tpc_h11/duckdb/h11.sql
@@ -53,49 +53,45 @@ FROM (
       t7.value > (
         (
           SELECT
-            t8.total
+            SUM((
+              t6.ps_supplycost * t6.ps_availqty
+            )) AS "Sum(Multiply(ps_supplycost, ps_availqty))"
           FROM (
             SELECT
-              SUM((
-                t6.ps_supplycost * t6.ps_availqty
-              )) AS total
+              *
             FROM (
               SELECT
-                *
-              FROM (
-                SELECT
-                  t0.ps_partkey AS ps_partkey,
-                  t0.ps_suppkey AS ps_suppkey,
-                  t0.ps_availqty AS ps_availqty,
-                  t0.ps_supplycost AS ps_supplycost,
-                  t0.ps_comment AS ps_comment,
-                  t1.s_suppkey AS s_suppkey,
-                  t1.s_name AS s_name,
-                  t1.s_address AS s_address,
-                  t1.s_nationkey AS s_nationkey,
-                  t1.s_phone AS s_phone,
-                  t1.s_acctbal AS s_acctbal,
-                  t1.s_comment AS s_comment,
-                  t2.n_nationkey AS n_nationkey,
-                  t2.n_name AS n_name,
-                  t2.n_regionkey AS n_regionkey,
-                  t2.n_comment AS n_comment
-                FROM "partsupp" AS t0
-                INNER JOIN "supplier" AS t1
-                  ON (
-                    t0.ps_suppkey = t1.s_suppkey
-                  )
-                INNER JOIN "nation" AS t2
-                  ON (
-                    t2.n_nationkey = t1.s_nationkey
-                  )
-              ) AS t5
-              WHERE
-                (
-                  t5.n_name = 'GERMANY'
+                t0.ps_partkey AS ps_partkey,
+                t0.ps_suppkey AS ps_suppkey,
+                t0.ps_availqty AS ps_availqty,
+                t0.ps_supplycost AS ps_supplycost,
+                t0.ps_comment AS ps_comment,
+                t1.s_suppkey AS s_suppkey,
+                t1.s_name AS s_name,
+                t1.s_address AS s_address,
+                t1.s_nationkey AS s_nationkey,
+                t1.s_phone AS s_phone,
+                t1.s_acctbal AS s_acctbal,
+                t1.s_comment AS s_comment,
+                t2.n_nationkey AS n_nationkey,
+                t2.n_name AS n_name,
+                t2.n_regionkey AS n_regionkey,
+                t2.n_comment AS n_comment
+              FROM "partsupp" AS t0
+              INNER JOIN "supplier" AS t1
+                ON (
+                  t0.ps_suppkey = t1.s_suppkey
                 )
-            ) AS t6
-          ) AS t8
+              INNER JOIN "nation" AS t2
+                ON (
+                  t2.n_nationkey = t1.s_nationkey
+                )
+            ) AS t5
+            WHERE
+              (
+                t5.n_name = 'GERMANY'
+              )
+          ) AS t6
         ) * CAST(0.0001 AS DOUBLE)
       )
     )
--- ibis/backends/tests/tpch/snapshots/test_h22/test_tpc_h22/duckdb/h22.sql
+++ ibis/backends/tests/tpch/snapshots/test_h22/test_tpc_h22/duckdb/h22.sql
@@ -1,62 +1,67 @@
-WITH t0 AS (
-  SELECT
-    CASE
-      WHEN (
-        CAST(0 AS TINYINT) + 1 >= 1
-      )
-      THEN SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
-      ELSE SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1 + LENGTH(t2.c_phone), CAST(2 AS TINYINT))
-    END AS cntrycode,
-    t2.c_acctbal AS c_acctbal
-  FROM main.customer AS t2
-  WHERE
-    CASE
-      WHEN (
-        CAST(0 AS TINYINT) + 1 >= 1
-      )
-      THEN SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
-      ELSE SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1 + LENGTH(t2.c_phone), CAST(2 AS TINYINT))
-    END IN ('13', '31', '23', '29', '30', '18', '17')
-    AND t2.c_acctbal > (
-      SELECT
-        anon_1.avg_bal
-      FROM (
-        SELECT
-          AVG(t2.c_acctbal) AS avg_bal
-        FROM main.customer AS t2
-        WHERE
-          t2.c_acctbal > CAST(0.0 AS REAL(53))
-          AND CASE
-            WHEN (
-              CAST(0 AS TINYINT) + 1 >= 1
-            )
-            THEN SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
-            ELSE SUBSTR(t2.c_phone, CAST(0 AS TINYINT) + 1 + LENGTH(t2.c_phone), CAST(2 AS TINYINT))
-          END IN ('13', '31', '23', '29', '30', '18', '17')
-      ) AS anon_1
-    )
-    AND NOT (
-      EXISTS(
-        SELECT
-          CAST(1 AS TINYINT) AS anon_2
-        FROM main.orders AS t3
-        WHERE
-          t3.o_custkey = t2.c_custkey
-      )
-    )
-)
 SELECT
-  t1.cntrycode,
-  t1.numcust,
-  t1.totacctbal
+  *
 FROM (
   SELECT
-    t0.cntrycode AS cntrycode,
+    t7.cntrycode AS cntrycode,
     COUNT(*) AS numcust,
-    SUM(t0.c_acctbal) AS totacctbal
-  FROM t0
+    SUM(t7.c_acctbal) AS totacctbal
+  FROM (
+    SELECT
+      CASE
+        WHEN CAST(0 AS TINYINT) >= 0
+        THEN SUBSTRING(t6.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
+        ELSE SUBSTRING(t6.c_phone, CAST(0 AS TINYINT), CAST(2 AS TINYINT))
+      END AS cntrycode,
+      t6.c_acctbal AS c_acctbal
+    FROM (
+      SELECT
+        *
+      FROM "customer" AS t0
+      WHERE
+        CASE
+          WHEN CAST(0 AS TINYINT) >= 0
+          THEN SUBSTRING(t0.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
+          ELSE SUBSTRING(t0.c_phone, CAST(0 AS TINYINT), CAST(2 AS TINYINT))
+        END IN ('13', '31', '23', '29', '30', '18', '17')
+        AND (
+          t0.c_acctbal > (
+            SELECT
+              AVG(t3.c_acctbal) AS "Mean(c_acctbal)"
+            FROM (
+              SELECT
+                *
+              FROM "customer" AS t0
+              WHERE
+                (
+                  t0.c_acctbal > CAST(0.0 AS DOUBLE)
+                )
+                AND CASE
+                  WHEN CAST(0 AS TINYINT) >= 0
+                  THEN SUBSTRING(t0.c_phone, CAST(0 AS TINYINT) + 1, CAST(2 AS TINYINT))
+                  ELSE SUBSTRING(t0.c_phone, CAST(0 AS TINYINT), CAST(2 AS TINYINT))
+                END IN ('13', '31', '23', '29', '30', '18', '17')
+            ) AS t3
+          )
+        )
+        AND NOT EXISTS(
+          (
+            SELECT
+              CAST(1 AS TINYINT) AS "1"
+            FROM (
+              SELECT
+                *
+              FROM "orders" AS t1
+              WHERE
+                (
+                  t1.o_custkey = t0.c_custkey
+                )
+            ) AS t2
+          )
+        )
+    ) AS t6
+  ) AS t7
   GROUP BY
     1
-) AS t1
+) AS t8
 ORDER BY
-  t1.cntrycode ASC
+  t8.cntrycode ASC
