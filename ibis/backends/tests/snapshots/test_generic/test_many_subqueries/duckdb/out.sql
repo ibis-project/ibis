e2b7e0a2252dfaff71ea5bf2ec9cf0616a354f49
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-11-27 14:55:42 -0500

    chore: sqlgen

--- ibis/backends/tests/snapshots/test_generic/test_many_subqueries/duckdb/out.sql
+++ ibis/backends/tests/snapshots/test_generic/test_many_subqueries/duckdb/out.sql
@@ -1,68 +1,55 @@
 SELECT
-  t5.street,
-  t5.key
+  t5.street AS street,
+  t5.key AS key,
+  t5.key_right AS key_right
 FROM (
   SELECT
-    t4.street,
-    ROW_NUMBER() OVER (ORDER BY t4.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
+    t1.street AS street,
+    ROW_NUMBER() OVER (ORDER BY t1.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key,
+    t2.key AS key_right
   FROM (
     SELECT
-      t1.street,
-      t1.key
+      t0.street AS street,
+      ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
+    FROM "data" AS t0
+  ) AS t1
+  INNER JOIN (
+    SELECT
+      t1.key AS key
     FROM (
       SELECT
-        t0.*,
-        ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
+        t0.street AS street,
+        ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
+      FROM "data" AS t0
+    ) AS t1
+  ) AS t2
+    ON t1.key = t2.key
+) AS t5
+INNER JOIN (
+  SELECT
+    t5.key AS key
+  FROM (
+    SELECT
+      t1.street AS street,
+      ROW_NUMBER() OVER (ORDER BY t1.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key,
+      t2.key AS key_right
+    FROM (
+      SELECT
+        t0.street AS street,
+        ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
       FROM "data" AS t0
     ) AS t1
     INNER JOIN (
       SELECT
-        t1.key
+        t1.key AS key
       FROM (
         SELECT
-          t0.*,
-          ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
+          t0.street AS street,
+          ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
         FROM "data" AS t0
       ) AS t1
     ) AS t2
-      ON (
-        t1.key = t2.key
-      )
-  ) AS t4
-) AS t5
-INNER JOIN (
-  SELECT
-    t5.key
-  FROM (
-    SELECT
-      t4.street,
-      ROW_NUMBER() OVER (ORDER BY t4.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
-    FROM (
-      SELECT
-        t1.street,
-        t1.key
-      FROM (
-        SELECT
-          t0.*,
-          ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
-        FROM "data" AS t0
-      ) AS t1
-      INNER JOIN (
-        SELECT
-          t1.key
-        FROM (
-          SELECT
-            t0.*,
-            ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - 1 AS key
-          FROM "data" AS t0
-        ) AS t1
-      ) AS t2
-        ON (
-          t1.key = t2.key
-        )
-    ) AS t4
+      ON t1.key = t2.key
   ) AS t5
 ) AS t6
-  ON (
-    t5.key = t6.key
-  )
+  ON t5.key = t6.key
