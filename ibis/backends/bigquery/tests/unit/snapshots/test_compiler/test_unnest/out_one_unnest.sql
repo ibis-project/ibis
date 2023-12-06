56b6a9173e2f9ce2e36e5efec086ad55fb42f028
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-12-06 13:52:21 -0500

    ci(bigquery): fix quoting

--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_unnest/out_one_unnest.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_unnest/out_one_unnest.sql
@@ -1,8 +1,8 @@
 SELECT
   t0.`rowindex`,
-  IF(pos = pos_2, repeated_struct_col, NULL) AS repeated_struct_col
+  IF(pos = pos_2, `repeated_struct_col`, NULL) AS `repeated_struct_col`
 FROM array_test AS t0, UNNEST(GENERATE_ARRAY(0, GREATEST(ARRAY_LENGTH(t0.`repeated_struct_col`)) - 1)) AS pos
-CROSS JOIN UNNEST(t0.`repeated_struct_col`) AS repeated_struct_col WITH OFFSET AS pos_2
+CROSS JOIN UNNEST(t0.`repeated_struct_col`) AS `repeated_struct_col` WITH OFFSET AS pos_2
 WHERE
   pos = pos_2
   OR (
