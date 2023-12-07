38fa081accd2d0b4979dcccb70c5e47b627a701f
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-12-07 07:22:34 -0500

    chore: generate bogus sql

--- ibis/backends/tests/sql/snapshots/test_select_sql/test_join_with_conditional_aggregate/result.sql
+++ ibis/backends/tests/sql/snapshots/test_select_sql/test_join_with_conditional_aggregate/result.sql
@@ -1,23 +1,31 @@
 SELECT
-  t0.on AS on,
-  t0.by AS by,
-  t1.on AS on_right,
-  t1.by AS by_right,
-  t1.val AS val
-FROM left AS t0
-LEFT OUTER JOIN right AS t1
-  ON t0.by = t1.by
+  *
+FROM (
+  SELECT
+    t0.on AS on,
+    t0.by AS by,
+    t1.on AS on_right,
+    t1.by AS by_right,
+    t1.val AS val
+  FROM left AS t0
+  LEFT OUTER JOIN right AS t1
+    ON t0.by = t1.by
+) AS t4
 WHERE
   (
-    t1.on_right = (
+    t4.on_right = (
       SELECT
-        MAX(t1.on) AS "Max(on)"
-      FROM right AS t1
-      WHERE
-        (
-          t1.by = t0.by
-        ) AND (
-          t1.on <= t0.on
-        )
+        MAX(t3.on) AS "Max(on)"
+      FROM (
+        SELECT
+          *
+        FROM right AS t1
+        WHERE
+          (
+            t1.by = t0.by
+          ) AND (
+            t1.on <= t0.on
+          )
+      ) AS t3
     )
   )
