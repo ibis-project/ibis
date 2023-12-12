4253783dc636df2e43b4e4640dc68163adee370d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-12-12 05:17:11 -0500

    chore(duckdb): regenerate sql

--- ibis/backends/tests/snapshots/test_generic/test_many_subqueries/duckdb/out.sql
+++ ibis/backends/tests/snapshots/test_generic/test_many_subqueries/duckdb/out.sql
@@ -6,7 +6,7 @@ FROM (
   SELECT
     t1.street AS street,
     ROW_NUMBER() OVER (ORDER BY t1.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key,
-    t2.key AS key_right
+    t3.key AS key_right
   FROM (
     SELECT
       t0.street AS street,
@@ -15,15 +15,10 @@ FROM (
   ) AS t1
   INNER JOIN (
     SELECT
-      t1.key AS key
-    FROM (
-      SELECT
-        t0.street AS street,
-        ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
-      FROM data AS t0
-    ) AS t1
-  ) AS t2
-    ON t1.key = t2.key
+      ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
+    FROM data AS t0
+  ) AS t3
+    ON t1.key = t3.key
 ) AS t5
 INNER JOIN (
   SELECT
@@ -32,7 +27,7 @@ INNER JOIN (
     SELECT
       t1.street AS street,
       ROW_NUMBER() OVER (ORDER BY t1.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key,
-      t2.key AS key_right
+      t3.key AS key_right
     FROM (
       SELECT
         t0.street AS street,
@@ -41,15 +36,10 @@ INNER JOIN (
     ) AS t1
     INNER JOIN (
       SELECT
-        t1.key AS key
-      FROM (
-        SELECT
-          t0.street AS street,
-          ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
-        FROM data AS t0
-      ) AS t1
-    ) AS t2
-      ON t1.key = t2.key
+        ROW_NUMBER() OVER (ORDER BY t0.street ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - CAST(1 AS TINYINT) AS key
+      FROM data AS t0
+    ) AS t3
+      ON t1.key = t3.key
   ) AS t5
-) AS t6
-  ON t5.key = t6.key
+) AS t7
+  ON t5.key = t7.key
--- ibis/backends/tests/snapshots/test_sql/test_union_aliasing/duckdb/out.sql
+++ ibis/backends/tests/snapshots/test_sql/test_union_aliasing/duckdb/out.sql
@@ -1,100 +1,74 @@
 SELECT
-  t11.field_of_study AS field_of_study,
-  t11.diff AS diff
+  t8.field_of_study AS field_of_study,
+  t8.diff AS diff
 FROM (
   SELECT
-    *
+    t3.field_of_study AS field_of_study,
+    t3.diff AS diff
   FROM (
     SELECT
-      t4.field_of_study AS field_of_study,
-      FIRST(t4.diff) AS diff
+      t2.field_of_study AS field_of_study,
+      FIRST(t2.diff) AS diff
     FROM (
       SELECT
-        t3.field_of_study AS field_of_study,
-        t3.years AS years,
-        t3.degrees AS degrees,
-        t3.earliest_degrees AS earliest_degrees,
-        t3.latest_degrees AS latest_degrees,
-        t3.latest_degrees - t3.earliest_degrees AS diff
+        t1.field_of_study AS field_of_study,
+        t1.years AS years,
+        t1.degrees AS degrees,
+        FIRST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS earliest_degrees,
+        LAST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_degrees,
+        LAST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - FIRST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS diff
       FROM (
         SELECT
-          t2.field_of_study AS field_of_study,
-          t2.years AS years,
-          t2.degrees AS degrees,
-          FIRST(t2.degrees) OVER (PARTITION BY t2.field_of_study ORDER BY t2.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS earliest_degrees,
-          LAST(t2.degrees) OVER (PARTITION BY t2.field_of_study ORDER BY t2.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_degrees
-        FROM (
-          SELECT
-            t1.field_of_study AS field_of_study,
-            t1.__pivoted__['years'] AS years,
-            t1.__pivoted__['degrees'] AS degrees
-          FROM (
-            SELECT
-              t0.field_of_study AS field_of_study,
-              UNNEST(
-                [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
-              ) AS __pivoted__
-            FROM humanities AS t0
-          ) AS t1
-        ) AS t2
-      ) AS t3
-    ) AS t4
+          t0.field_of_study AS field_of_study,
+          UNNEST(
+            [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
+          )['years'] AS years,
+          UNNEST(
+            [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
+          )['degrees'] AS degrees
+        FROM humanities AS t0
+      ) AS t1
+    ) AS t2
     GROUP BY
       1
-  ) AS t5
+  ) AS t3
   ORDER BY
-    t5.diff DESC
+    t3.diff DESC
   LIMIT 10
   UNION ALL
   SELECT
-    *
+    t3.field_of_study AS field_of_study,
+    t3.diff AS diff
   FROM (
     SELECT
-      *
+      t2.field_of_study AS field_of_study,
+      FIRST(t2.diff) AS diff
     FROM (
       SELECT
-        t4.field_of_study AS field_of_study,
-        FIRST(t4.diff) AS diff
+        t1.field_of_study AS field_of_study,
+        t1.years AS years,
+        t1.degrees AS degrees,
+        FIRST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS earliest_degrees,
+        LAST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_degrees,
+        LAST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - FIRST(t1.degrees) OVER (PARTITION BY t1.field_of_study ORDER BY t1.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS diff
       FROM (
         SELECT
-          t3.field_of_study AS field_of_study,
-          t3.years AS years,
-          t3.degrees AS degrees,
-          t3.earliest_degrees AS earliest_degrees,
-          t3.latest_degrees AS latest_degrees,
-          t3.latest_degrees - t3.earliest_degrees AS diff
-        FROM (
-          SELECT
-            t2.field_of_study AS field_of_study,
-            t2.years AS years,
-            t2.degrees AS degrees,
-            FIRST(t2.degrees) OVER (PARTITION BY t2.field_of_study ORDER BY t2.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS earliest_degrees,
-            LAST(t2.degrees) OVER (PARTITION BY t2.field_of_study ORDER BY t2.years ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_degrees
-          FROM (
-            SELECT
-              t1.field_of_study AS field_of_study,
-              t1.__pivoted__['years'] AS years,
-              t1.__pivoted__['degrees'] AS degrees
-            FROM (
-              SELECT
-                t0.field_of_study AS field_of_study,
-                UNNEST(
-                  [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
-                ) AS __pivoted__
-              FROM humanities AS t0
-            ) AS t1
-          ) AS t2
-        ) AS t3
-      ) AS t4
-      GROUP BY
-        1
-    ) AS t5
-    WHERE
-      (
-        t5.diff < CAST(0 AS TINYINT)
-      )
-  ) AS t7
+          t0.field_of_study AS field_of_study,
+          UNNEST(
+            [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
+          )['years'] AS years,
+          UNNEST(
+            [{'years': '1970-71', 'degrees': t0."1970-71"}, {'years': '1975-76', 'degrees': t0."1975-76"}, {'years': '1980-81', 'degrees': t0."1980-81"}, {'years': '1985-86', 'degrees': t0."1985-86"}, {'years': '1990-91', 'degrees': t0."1990-91"}, {'years': '1995-96', 'degrees': t0."1995-96"}, {'years': '2000-01', 'degrees': t0."2000-01"}, {'years': '2005-06', 'degrees': t0."2005-06"}, {'years': '2010-11', 'degrees': t0."2010-11"}, {'years': '2011-12', 'degrees': t0."2011-12"}, {'years': '2012-13', 'degrees': t0."2012-13"}, {'years': '2013-14', 'degrees': t0."2013-14"}, {'years': '2014-15', 'degrees': t0."2014-15"}, {'years': '2015-16', 'degrees': t0."2015-16"}, {'years': '2016-17', 'degrees': t0."2016-17"}, {'years': '2017-18', 'degrees': t0."2017-18"}, {'years': '2018-19', 'degrees': t0."2018-19"}, {'years': '2019-20', 'degrees': t0."2019-20"}]
+          )['degrees'] AS degrees
+        FROM humanities AS t0
+      ) AS t1
+    ) AS t2
+    GROUP BY
+      1
+  ) AS t3
+  WHERE
+    t3.diff < CAST(0 AS TINYINT)
   ORDER BY
-    t7.diff ASC
+    t3.diff ASC
   LIMIT 10
-) AS t11
+) AS t8
