5de074916e107eb58efe8a03a6c827c25edf66d7
Nicola Coretti <nico.coretti@gmail.com>
2023-12-14 08:05:10 +0100

    chore(exasol): rename sqlglot dialect mapping
    Co-authored-by: Gil Forsyth <gforsyth@users.noreply.github.com>

--- ibis/backends/base/__init__.py
+++ ibis/backends/base/__init__.py
@@ -40,7 +40,7 @@ _IBIS_TO_SQLGLOT_DIALECT = {
     "polars": "postgres",
     "datafusion": "postgres",
     # closest match see https://github.com/ibis-project/ibis/pull/7303#discussion_r1350223901
-    "exa.websocket": "oracle",
+    "exasol": "oracle",
 }
 
 _SQLALCHEMY_TO_SQLGLOT_DIALECT = {
