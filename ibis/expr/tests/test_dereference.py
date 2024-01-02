14f702457a0d3b32b6f62d1dc575d77c32736a2e
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-02 19:44:42 +0100

    fir(ir): `asof` join `tolerance` parameter should post-filter and post-join instead of adding a predicate

--- ibis/expr/tests/test_dereference.py
+++ ibis/expr/tests/test_dereference.py
@@ -17,9 +17,30 @@ def dereference_expect(expected):
     return {k.op(): v.op() for k, v in expected.items()}
 
 
+def test_dereference_project():
+    p = t.projection([t.int_col, t.double_col])
+
+    mapping = dereference_mapping([p.op()])
+    expected = dereference_expect(
+        {
+            p.int_col: p.int_col,
+            p.double_col: p.double_col,
+            t.int_col: p.int_col,
+            t.double_col: p.double_col,
+        }
+    )
+    assert mapping == expected
+
+
 def test_dereference_mapping_self_reference():
     v = t.view()
 
     mapping = dereference_mapping([v.op()])
-    expected = dereference_expect({})
+    expected = dereference_expect(
+        {
+            v.int_col: v.int_col,
+            v.double_col: v.double_col,
+            v.string_col: v.string_col,
+        }
+    )
     assert mapping == expected
