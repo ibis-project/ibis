02c4c386264841a49573f08b84746ce138bddd10
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-04 17:18:02 +0100

    refactor(polars): update the polars backend to use the new relational abstractions (#7868)
    Bare minimum changes required for the polars backend to work with the
    new relational operations. Since polars' join API follows the same
    semantics as pandas, I'm using the pandas specific rewrites here.
    
    In the future we may want to rewrite the compiler similarly to the
    pandas one: using `node.map()` and `Dispatched`.

---
+++ ibis/backends/polars/tests/test_join.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import pandas as pd
+import pandas.testing as tm
+
+import ibis
+
+
+def test_memtable_join(con):
+    t1 = ibis.memtable({"x": [1, 2, 3], "y": [4, 5, 6], "z": ["a", "b", "c"]})
+    t2 = ibis.memtable({"x": [3, 2, 1], "y": [7, 8, 9], "z": ["d", "e", "f"]})
+    expr = t1.join(t2, "x")
+
+    result = con.execute(expr)
+    expected = pd.DataFrame(
+        {
+            "x": [1, 2, 3],
+            "y": [4, 5, 6],
+            "z": ["a", "b", "c"],
+            "x_right": [1, 2, 3],
+            "y_right": [9, 8, 7],
+            "z_right": ["f", "e", "d"],
+        }
+    )
+
+    left = result.sort_values("x").reset_index(drop=True)
+    right = expected.sort_values("x").reset_index(drop=True)
+    tm.assert_frame_equal(left, right)
