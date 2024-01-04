67ede0a0f207ce05d537626f5b2f01b0a126baa0
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-04 12:46:03 +0100

    refactor(pandas): port the pandas backend with an improved execution model (#7797)
    ## Old Implementation
    
    Since we need to reimplement/port all of the backends for #7752, I took
    an
    attempt at reimplementing the pandas backend using a new execution
    engine.
    Previously the pandas backend was implemented using a top-down execution
    model
    and each operation was executing using a multidispatched function. While
    it
    served us well for a long time, it had a few drawbacks:
    - it was often hard to understand what was going on due to the complex 
      preparation steps and various execution hooks
    - the multidispatched functions were hard to debug, additionally they
    supported
      a wide variety of inputs making the implementation rather bulky
    - due to the previous reaon, several inputs combinations were not
    supported,
      e.g. value operations with multiple columnar inputs
    - the `Scope` object was used to pass around the execution context which
    was
    created for each operation separately and the results were not reusable
    even
      though the same operation was executed multiple times 
    
    ## New Implementation
    
    The new execution model has changed in several ways:
    - there is a rewrite layer before execution which lowers the input
    expression
    to a form closer to the pandas execution model, this makes it much
    easier to
      implement the operations and also makes the input "plan" inspectable
    - the execution is now topologically sorted and executed in a bottom-up
    manner;
    the intermediate results are reused, making the execution more efficient
    while
    also aggressively cleaned up as soon as they are not needed anymore to
    reduce
      the memory usage
    - the execute function is now single-dispatched making the
    implementation
      easier to locate and debug
    - the inputs now broadcasted to columnar shape so that the same
    implementation
    can be used for multiple input shape combinations, this removes several
    special cases from the implementation in exchange of a negligible
    performance
      overhead
    - there are helper utilities making it easier to implement compute
    kernels for
      the various value operations: `rowwise`, `columnwise`, `elementwise`, 
    `serieswise`; if there are multiple implementations available for a
    given
    operation, the most efficient one is selected based on the input shapes
    
    The new backend implementation has a higher feature coverage while the 
    implementation is one third of the size of the previous one. 
    
    BREAKING CHANGE: the `timecontext` feature is not supported anymore

--- ibis/backends/tests/test_generic.py
+++ ibis/backends/tests/test_generic.py
@@ -307,12 +307,14 @@ def test_filter(backend, alltypes, sorted_df, predicate_fn, expected_fn):
         "druid",
         "oracle",
         "exasol",
+        "pandas",
     ]
 )
 @pytest.mark.never(
     ["flink"],
     reason="Flink engine does not support generic window clause with no order by",
 )
+# TODO(kszucs): this is not supported at the expression level
 def test_filter_with_window_op(backend, alltypes, sorted_df):
     sorted_alltypes = alltypes.order_by("id")
     table = sorted_alltypes
@@ -1119,7 +1121,7 @@ def test_pivot_wider(backend):
     reason="backend doesn't implement window functions",
 )
 @pytest.mark.notimpl(
-    ["pandas", "polars"],
+    ["polars"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement ops.WindowFunction",
 )
@@ -1192,7 +1194,7 @@ def test_distinct_on_keep(backend, on, keep):
     reason="backend doesn't implement window functions",
 )
 @pytest.mark.notimpl(
-    ["pandas", "polars"],
+    ["polars"],
     raises=com.OperationNotDefinedError,
     reason="backend doesn't implement ops.WindowFunction",
 )
--- ibis/backends/tests/test_vectorized_udf.py
+++ ibis/backends/tests/test_vectorized_udf.py
@@ -570,7 +570,8 @@ def test_elementwise_udf_named_destruct(udf_alltypes):
     add_one_struct_udf = create_add_one_struct_udf(
         result_formatter=lambda v1, v2: (v1, v2)
     )
-    with pytest.raises(com.IbisTypeError, match=r"Unable to infer"):
+    msg = "Duplicate column name 'new_struct' in result set"
+    with pytest.raises(com.IntegrityError, match=msg):
         udf_alltypes.mutate(
             new_struct=add_one_struct_udf(udf_alltypes["double_col"]).destructure()
         )
