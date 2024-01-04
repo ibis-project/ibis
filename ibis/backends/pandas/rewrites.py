ab6dd5268817c4f6b48c9b472c270901178db651
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-04 12:46:03 +0100

    refactor(pandas): port the pandas backend with an improved execution model (#7797)
    ## Old Implementation
    
    Since we need to reimplement/port all of the backends for #7752, I took
    an
    attempt at reimplementing the pandas backend using a new execution
    engine.
    Previously the pandas backend was implemented using a top-down execution
    model
    and each operation was executing using a multidispatched function. While
    it
    served us well for a long time, it had a few drawbacks:
    - it was often hard to understand what was going on due to the complex 
      preparation steps and various execution hooks
    - the multidispatched functions were hard to debug, additionally they
    supported
      a wide variety of inputs making the implementation rather bulky
    - due to the previous reaon, several inputs combinations were not
    supported,
      e.g. value operations with multiple columnar inputs
    - the `Scope` object was used to pass around the execution context which
    was
    created for each operation separately and the results were not reusable
    even
      though the same operation was executed multiple times 
    
    ## New Implementation
    
    The new execution model has changed in several ways:
    - there is a rewrite layer before execution which lowers the input
    expression
    to a form closer to the pandas execution model, this makes it much
    easier to
      implement the operations and also makes the input "plan" inspectable
    - the execution is now topologically sorted and executed in a bottom-up
    manner;
    the intermediate results are reused, making the execution more efficient
    while
    also aggressively cleaned up as soon as they are not needed anymore to
    reduce
      the memory usage
    - the execute function is now single-dispatched making the
    implementation
      easier to locate and debug
    - the inputs now broadcasted to columnar shape so that the same
    implementation
    can be used for multiple input shape combinations, this removes several
    special cases from the implementation in exchange of a negligible
    performance
      overhead
    - there are helper utilities making it easier to implement compute
    kernels for
      the various value operations: `rowwise`, `columnwise`, `elementwise`, 
    `serieswise`; if there are multiple implementations available for a
    given
    operation, the most efficient one is selected based on the input shapes
    
    The new backend implementation has a higher feature coverage while the 
    implementation is one third of the size of the previous one. 
    
    BREAKING CHANGE: the `timecontext` feature is not supported anymore

---
+++ ibis/backends/pandas/rewrites.py
@@ -0,0 +1,322 @@
+from __future__ import annotations
+
+from public import public
+
+import ibis
+import ibis.expr.datashape as ds
+import ibis.expr.datatypes as dt
+import ibis.expr.operations as ops
+from ibis.common.annotations import attribute
+from ibis.common.collections import FrozenDict
+from ibis.common.patterns import replace
+from ibis.common.typing import VarTuple  # noqa: TCH001
+from ibis.expr.schema import Schema
+from ibis.util import gen_name
+
+
+class PandasRelation(ops.Relation):
+    pass
+
+
+class PandasValue(ops.Value):
+    pass
+
+
+@public
+class PandasRename(PandasRelation):
+    parent: ops.Relation
+    mapping: FrozenDict[str, str]
+
+    @classmethod
+    def from_prefix(cls, parent, prefix):
+        mapping = {k: f"{prefix}_{k}" for k in parent.schema}
+        return cls(parent, mapping)
+
+    @attribute
+    def values(self):
+        return FrozenDict(
+            {to: ops.Field(self.parent, from_) for from_, to in self.mapping.items()}
+        )
+
+    @attribute
+    def schema(self):
+        return Schema(
+            {self.mapping[name]: dtype for name, dtype in self.parent.schema.items()}
+        )
+
+
+@public
+class PandasJoin(PandasRelation):
+    left: ops.Relation
+    right: ops.Relation
+    left_on: VarTuple[ops.Value]
+    right_on: VarTuple[ops.Value]
+    how: str
+
+    @attribute
+    def values(self):
+        return FrozenDict({**self.left.values, **self.right.values})
+
+    @attribute
+    def schema(self):
+        return self.left.schema | self.right.schema
+
+
+@public
+class PandasAsofJoin(PandasJoin):
+    left_by: VarTuple[ops.Value]
+    right_by: VarTuple[ops.Value]
+    operator: type
+
+
+@public
+class PandasAggregate(PandasRelation):
+    parent: ops.Relation
+    groups: FrozenDict[str, ops.Field]
+    metrics: FrozenDict[str, ops.Reduction]
+
+    @attribute
+    def values(self):
+        return FrozenDict({**self.groups, **self.metrics})
+
+    @attribute
+    def schema(self):
+        return Schema({k: v.dtype for k, v in self.values.items()})
+
+
+@public
+class PandasLimit(PandasRelation):
+    parent: ops.Relation
+    n: ops.Relation
+    offset: ops.Relation
+
+    @attribute
+    def values(self):
+        return self.parent.values
+
+    @attribute
+    def schema(self):
+        return self.parent.schema
+
+
+@public
+class PandasScalarSubquery(PandasValue):
+    # variant with no integrity checks
+    rel: ops.Relation
+
+    shape = ds.scalar
+
+    @attribute
+    def dtype(self):
+        return self.rel.schema.types[0]
+
+
+def is_columnar(node):
+    return isinstance(node, ops.Value) and node.shape.is_columnar()
+
+
+@replace(ops.Project)
+def rewrite_project(_, **kwargs):
+    winfuncs = []
+    for v in _.values.values():
+        winfuncs.extend(v.find(ops.WindowFunction, ops.Value))
+
+    if not winfuncs:
+        return _
+
+    selects = {ops.Field(_.parent, k): k for k in _.parent.schema}
+    for node in winfuncs:
+        # add computed values from the window function
+        values = list(node.func.__args__)
+        # add computed values from the window frame
+        values += node.frame.group_by
+        values += [key.expr for key in node.frame.order_by]
+        if node.frame.start is not None:
+            values.append(node.frame.start.value)
+        if node.frame.end is not None:
+            values.append(node.frame.end.value)
+
+        for v in values:
+            if is_columnar(v) and v not in selects:
+                selects[v] = gen_name("value")
+
+    # STEP 1: construct the pre-projection
+    proj = ops.Project(_.parent, {v: k for k, v in selects.items()})
+    subs = {node: ops.Field(proj, name) for name, node in proj.values.items()}
+
+    # STEP 2: construct new window function nodes
+    metrics = {}
+    for node in winfuncs:
+        frame = node.frame
+        start = None if frame.start is None else frame.start.replace(subs)
+        end = None if frame.end is None else frame.end.replace(subs)
+        order_by = [key.replace(subs) for key in frame.order_by]
+        group_by = [key.replace(subs) for key in frame.group_by]
+        frame = frame.__class__(
+            proj, start=start, end=end, group_by=group_by, order_by=order_by
+        )
+        metrics[node] = ops.WindowFunction(node.func.replace(subs), frame)
+
+    # STEP 3: reconstruct the current projection with the window functions
+    subs.update(metrics)
+    values = {k: v.replace(subs, filter=ops.Value) for k, v in _.values.items()}
+    return ops.Project(proj, values)
+
+
+@replace(ops.Aggregate)
+def rewrite_aggregate(_, **kwargs):
+    selects = {ops.Field(_.parent, k): k for k in _.parent.schema}
+    for v in _.groups.values():
+        if v not in selects:
+            selects[v] = gen_name("group")
+
+    reductions = {}
+    for v in _.metrics.values():
+        for reduction in v.find_topmost(ops.Reduction):
+            for arg in reduction.__args__:
+                if is_columnar(arg) and arg not in selects:
+                    selects[arg] = gen_name("value")
+            if reduction not in reductions:
+                reductions[reduction] = gen_name("reduction")
+
+    # STEP 1: construct the pre-projection
+    proj = ops.Project(_.parent, {v: k for k, v in selects.items()})
+
+    # STEP 2: construct the pandas aggregation
+    subs = {node: ops.Field(proj, name) for name, node in proj.values.items()}
+    groups = {name: ops.Field(proj, selects[node]) for name, node in _.groups.items()}
+    metrics = {name: node.replace(subs) for node, name in reductions.items()}
+    agg = PandasAggregate(proj, groups, metrics)
+
+    # STEP 3: construct the post-projection
+    subs = {node: ops.Field(agg, name) for node, name in reductions.items()}
+    values = {name: ops.Field(agg, name) for name, node in _.groups.items()}
+    values.update({name: node.replace(subs) for name, node in _.metrics.items()})
+    return ops.Project(agg, values)
+
+
+def split_join_predicates(left, right, predicates, only_equality=True):
+    left_on = []
+    right_on = []
+    for pred in predicates:
+        if left not in pred.relations or right not in pred.relations:
+            # not a usual join predicate, so apply a trick by placing the
+            # predicate to the left side and adding a literal True to the right
+            # which the left side must be equal to
+            left_on.append(pred)
+            right_on.append(ops.Literal(True, dtype=dt.boolean))
+        elif isinstance(pred, ops.Binary):
+            if only_equality and not isinstance(pred, ops.Equals):
+                raise TypeError("Only equality join predicates supported with pandas")
+            if left in pred.left.relations and right in pred.right.relations:
+                left_on.append(pred.left)
+                right_on.append(pred.right)
+            elif left in pred.right.relations and right in pred.left.relations:
+                left_on.append(pred.right)
+                right_on.append(pred.left)
+            else:
+                raise ValueError("Join predicate does not reference both tables")
+        else:
+            raise TypeError(f"Unsupported join predicate {pred}")
+
+    return left_on, right_on
+
+
+@replace(ops.JoinChain)
+def rewrite_join(_, **kwargs):
+    prefixes = {}
+    prefixes[_.first] = prefix = str(len(prefixes))
+    left = PandasRename.from_prefix(_.first, prefix)
+
+    for link in _.rest:
+        prefixes[link.table] = prefix = str(len(prefixes))
+        right = PandasRename.from_prefix(link.table, prefix)
+
+        subs = {v: ops.Field(left, k) for k, v in left.values.items()}
+        subs.update({v: ops.Field(right, k) for k, v in right.values.items()})
+        preds = [pred.replace(subs, filter=ops.Value) for pred in link.predicates]
+
+        # separate ASOF from the rest of the joins
+        if link.how == "asof":
+            on, *by = preds
+            left_on, right_on = split_join_predicates(
+                left, right, [on], only_equality=False
+            )
+            left_by, right_by = split_join_predicates(left, right, by)
+            left = PandasAsofJoin(
+                how="asof",
+                left=left,
+                right=right,
+                left_on=left_on,
+                right_on=right_on,
+                left_by=left_by,
+                right_by=right_by,
+                operator=type(on),
+            )
+        else:
+            # need to replace the fields in the predicates
+            left_on, right_on = split_join_predicates(left, right, preds)
+            left = PandasJoin(
+                how=link.how,
+                left=left,
+                right=right,
+                left_on=left_on,
+                right_on=right_on,
+            )
+
+    subs = {v: ops.Field(left, k) for k, v in left.values.items()}
+    fields = {k: v.replace(subs, filter=ops.Value) for k, v in _.values.items()}
+    return ops.Project(left, fields)
+
+
+@replace(ops.Limit)
+def rewrite_limit(_, **kwargs):
+    if isinstance(_.n, ops.Value):
+        n = _.n.to_expr()
+    else:
+        n = ibis.literal(_.n)
+
+    if isinstance(_.offset, ops.Value):
+        offset = _.offset.to_expr()
+    else:
+        offset = ibis.literal(_.offset)
+
+    n = n.as_table().op()
+    if isinstance(n, ops.Aggregate):
+        n = rewrite_aggregate.match(n, context={})
+
+    offset = offset.as_table().op()
+    if isinstance(offset, ops.Aggregate):
+        offset = rewrite_aggregate.match(offset, context={})
+
+    return PandasLimit(_.parent, n, offset)
+
+
+@replace(ops.ScalarSubquery)
+def rewrite_scalar_subquery(_, **kwargs):
+    return PandasScalarSubquery(_.rel)
+
+
+@replace(ops.ScalarParameter)
+def replace_parameter(_, params, **kwargs):
+    return ops.Literal(value=params[_], dtype=_.dtype)
+
+
+@replace(ops.UnboundTable)
+def bind_unbound_table(_, backend, **kwargs):
+    return ops.DatabaseTable(name=_.name, schema=_.schema, source=backend)
+
+
+def plan(node, backend, params):
+    ctx = {"params": params, "backend": backend}
+    node = node.replace(rewrite_scalar_subquery)
+    node = node.replace(
+        rewrite_project
+        | rewrite_aggregate
+        | rewrite_join
+        | rewrite_limit
+        | replace_parameter
+        | bind_unbound_table,
+        context=ctx,
+    )
+    return node
--- ibis/backends/pandas/tests/test_core.py
+++ ibis/backends/pandas/tests/test_core.py
@@ -6,11 +6,7 @@ import pytest
 
 import ibis
 import ibis.common.exceptions as com
-import ibis.expr.operations as ops
-from ibis.backends.base.df.scope import Scope
 from ibis.backends.pandas import Backend
-from ibis.backends.pandas.dispatch import post_execute, pre_execute
-from ibis.backends.pandas.execution import execute
 
 
 @pytest.fixture
@@ -50,59 +46,24 @@ def test_from_dataframe(dataframe, ibis_table, core_client):
     tm.assert_frame_equal(result, expected)
 
 
-def test_pre_execute_basic():
-    """Test that pre_execute has intercepted execution and provided its own
-    scope dict."""
-
-    @pre_execute.register(ops.Add)
-    def pre_execute_test(op, *clients, scope=None, **kwargs):
-        return Scope({op: 4}, None)
-
-    one = ibis.literal(1)
-    expr = one + one
-    result = execute(expr.op())
-    assert result == 4
-
-    del pre_execute.funcs[(ops.Add,)]
-    pre_execute.reorder()
-    pre_execute._cache.clear()
-
-
 def test_execute_parameter_only():
     param = ibis.param("int64")
-    result = execute(param.op(), params={param.op(): 42})
+    con = ibis.pandas.connect()
+    result = con.execute(param, params={param.op(): 42})
     assert result == 42
 
 
 def test_missing_data_sources():
-    t = ibis.table([("a", "string")])
+    t = ibis.table([("a", "string")], name="t")
     expr = t.a.length()
+    con = ibis.pandas.connect()
     with pytest.raises(com.UnboundExpressionError):
-        execute(expr.op())
+        con.execute(expr)
 
 
-def test_post_execute_called_on_joins(dataframe, core_client, ibis_table):
-    count = [0]
-
-    @post_execute.register(ops.InnerJoin, pd.DataFrame)
-    def tmp_left_join_exe(op, lhs, **kwargs):
-        count[0] += 1
-        return lhs
-
-    left = ibis_table
-    right = left.view()
-    join = left.join(right, "plain_strings")[left.plain_int64]
-    result = join.execute()
-    assert result is not None
-    assert not result.empty
-    assert count[0] == 1
-
-
-def test_scope_look_up():
-    # test if scope could lookup items properly
-    scope = Scope()
-    one_day = ibis.interval(days=1).op()
-    one_hour = ibis.interval(hours=1).op()
-    scope = scope.merge_scope(Scope({one_day: 1}, None))
-    assert scope.get_value(one_hour) is None
-    assert scope.get_value(one_day) is not None
+def test_unbound_table_execution():
+    t = ibis.table([("a", "string")], name="t")
+    expr = t.a.length()
+    con = ibis.pandas.connect({"t": pd.DataFrame({"a": ["a", "ab", "abc"]})})
+    result = con.execute(expr)
+    assert result.tolist() == [1, 2, 3]
