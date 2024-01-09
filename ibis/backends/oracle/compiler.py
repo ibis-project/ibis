affcfbd8834a8f070a2af9edaa38fe19830892f8
Gil Forsyth <gil@forsyth.dev>
2024-01-09 13:40:45 -0500

    feat(oracle): port to sqlglot (numeric)

--- ibis/backends/oracle/compiler.py
+++ ibis/backends/oracle/compiler.py
@@ -6,11 +6,53 @@ import sqlglot as sg
 import sqlglot.expressions as sge
 from public import public
 
+import ibis
+import ibis.common.exceptions as com
 import ibis.expr.operations as ops
 from ibis.backends.base.sqlglot.compiler import SQLGlotCompiler
 from ibis.backends.base.sqlglot.datatypes import OracleType
 from ibis.backends.base.sqlglot.rewrites import replace_log2, replace_log10
+from ibis.common.patterns import replace
+from ibis.expr.analysis import p, x, y
 from ibis.expr.rewrites import rewrite_sample
+
+
+@replace(p.WindowFunction(p.First(x, y)))
+def rewrite_first(_, x, y):
+    if y is not None:
+        raise com.UnsupportedOperationError(
+            "`first` aggregate over window does not support `where`"
+        )
+    return _.copy(func=ops.FirstValue(x))
+
+
+@replace(p.WindowFunction(p.Last(x, y)))
+def rewrite_last(_, x, y):
+    if y is not None:
+        raise com.UnsupportedOperationError(
+            "`last` aggregate over window does not support `where`"
+        )
+    return _.copy(func=ops.LastValue(x))
+
+
+@replace(p.WindowFunction(frame=x @ p.WindowFrame(order_by=())))
+def rewrite_empty_order_by_window(_, x):
+    return _.copy(frame=x.copy(order_by=(ibis.NA,)))
+
+
+@replace(p.WindowFunction(p.RowNumber | p.NTile, x))
+def exclude_unsupported_window_frame_from_row_number(_, x):
+    return ops.Subtract(_.copy(frame=x.copy(start=None, end=None)), 1)
+
+
+@replace(
+    p.WindowFunction(
+        p.Lag | p.Lead | p.PercentRank | p.CumeDist | p.Any | p.All,
+        x @ p.WindowFrame(start=None),
+    )
+)
+def exclude_unsupported_window_frame_from_ops(_, x):
+    return _.copy(frame=x.copy(start=None, end=None))
 
 
 @public
@@ -21,6 +63,11 @@ class OracleCompiler(SQLGlotCompiler):
     quoted = True
     type_mapper = OracleType
     rewrites = (
+        exclude_unsupported_window_frame_from_row_number,
+        exclude_unsupported_window_frame_from_ops,
+        rewrite_first,
+        rewrite_last,
+        rewrite_empty_order_by_window,
         rewrite_sample,
         replace_log2,
         replace_log10,
@@ -78,3 +125,7 @@ class OracleCompiler(SQLGlotCompiler):
     @visit_node.register(ops.Radians)
     def visit_Radians(self, op, *, arg):
         return self.visit_node(ops.Pi()) * arg / 180
+
+    @visit_node.register(ops.Modulus)
+    def visit_Modulus(self, op, *, left, right):
+        return self.f.mod(left, right)
