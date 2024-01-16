9d4c2be90f7016fe77471dcdb01a9139f10fb329
Gil Forsyth <gil@forsyth.dev>
2024-01-16 15:17:52 -0500

    fix(oracle): remove oracle from alchemycon

--- ibis/backends/conftest.py
+++ ibis/backends/conftest.py
@@ -550,7 +550,7 @@ def ddl_con(ddl_backend):
 
 
 @pytest.fixture(
-    params=_get_backends_to_test(keep=("exasol", "mssql", "oracle", "sqlite")),
+    params=_get_backends_to_test(keep=("exasol", "mssql", "sqlite")),
     scope="session",
 )
 def alchemy_backend(request, data_dir, tmp_path_factory, worker_id):
