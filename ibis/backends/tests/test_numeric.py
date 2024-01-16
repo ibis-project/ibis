5daedaa5248d3f5495cc801eb2e680270dceff88
Gil Forsyth <gil@forsyth.dev>
2024-01-16 13:02:11 -0500

    fix(oracle): test fixups in asof join and numerics

--- ibis/backends/tests/test_numeric.py
+++ ibis/backends/tests/test_numeric.py
@@ -1492,7 +1492,14 @@ flink_no_bitwise = pytest.mark.notyet(
 )
 
 
-@pytest.mark.parametrize("op", [and_, or_, xor])
+@pytest.mark.parametrize(
+    "op",
+    [
+        and_,
+        param(or_, marks=[pytest.mark.notimpl(["oracle"], raises=OracleDatabaseError)]),
+        param(xor, marks=[pytest.mark.notimpl(["oracle"], raises=OracleDatabaseError)]),
+    ],
+)
 @pytest.mark.parametrize(
     ("left_fn", "right_fn"),
     [
@@ -1501,7 +1508,6 @@ flink_no_bitwise = pytest.mark.notyet(
         param(lambda t: t.int_col, lambda _: 3, id="col_scalar"),
     ],
 )
-@pytest.mark.notimpl(["oracle"], raises=OracleDatabaseError)
 @pytest.mark.notimpl(["exasol"], raises=(sa.exc.DBAPIError, ExaQueryError))
 @flink_no_bitwise
 def test_bitwise_columns(backend, con, alltypes, df, op, left_fn, right_fn):
@@ -1563,7 +1569,6 @@ def test_bitwise_shift(backend, alltypes, df, op, left_fn, right_fn):
     ("left", "right"),
     [param(4, L(2), id="int_col"), param(L(4), 2, id="col_int")],
 )
-@pytest.mark.notimpl(["oracle"], raises=OracleDatabaseError)
 @pytest.mark.notimpl(["exasol"], raises=ExaQueryError)
 @flink_no_bitwise
 def test_bitwise_scalars(con, op, left, right):
