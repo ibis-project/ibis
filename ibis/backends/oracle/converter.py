7a48f77be17268e6693497b94a303f1841063dfc
Gil Forsyth <gil@forsyth.dev>
2024-01-18 17:15:06 -0500

    chore(oracle): clean up converter

--- ibis/backends/oracle/converter.py
+++ ibis/backends/oracle/converter.py
@@ -1,13 +1,8 @@
 from __future__ import annotations
 
 import datetime
-from typing import TYPE_CHECKING
 
 from ibis.formats.pandas import PandasData
-
-if TYPE_CHECKING:
-    import ibis.expr.datatypes as dt
-    from ibis.expr.schema import Schema
 
 
 class OraclePandasData(PandasData):
