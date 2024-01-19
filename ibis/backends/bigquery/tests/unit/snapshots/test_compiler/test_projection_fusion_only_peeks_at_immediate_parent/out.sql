f291539091d56728941c44f2855381e02162ae75
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-19 22:54:22 +0100

    chore(sql): regenerate snapshots for bigquery

--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_projection_fusion_only_peeks_at_immediate_parent/out.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_projection_fusion_only_peeks_at_immediate_parent/out.sql
@@ -1,26 +1,18 @@
+WITH t1 AS (
+  SELECT
+    CAST(t0.file_date AS DATE) AS file_date,
+    t0.PARTITIONTIME,
+    t0.val,
+    t0.val * 2 AS XYZ
+  FROM unbound_table AS t0
+  WHERE
+    t0.PARTITIONTIME < DATE(2017, 1, 1) AND CAST(t0.file_date AS DATE) < DATE(2017, 1, 1)
+)
 SELECT
-  t2.file_date,
-  t2.PARTITIONTIME,
-  t2.val,
-  t2.XYZ
-FROM (
-  SELECT
-    CAST(t0.file_date AS DATE) AS file_date,
-    t0.PARTITIONTIME,
-    t0.val,
-    t0.val * 2 AS XYZ
-  FROM unbound_table AS t0
-  WHERE
-    t0.PARTITIONTIME < DATE(2017, 1, 1) AND CAST(t0.file_date AS DATE) < DATE(2017, 1, 1)
-) AS t2
-INNER JOIN (
-  SELECT
-    CAST(t0.file_date AS DATE) AS file_date,
-    t0.PARTITIONTIME,
-    t0.val,
-    t0.val * 2 AS XYZ
-  FROM unbound_table AS t0
-  WHERE
-    t0.PARTITIONTIME < DATE(2017, 1, 1) AND CAST(t0.file_date AS DATE) < DATE(2017, 1, 1)
-) AS t4
+  t3.file_date,
+  t3.PARTITIONTIME,
+  t3.val,
+  t3.XYZ
+FROM t1 AS t3
+INNER JOIN t1 AS t5
   ON TRUE
