e32863c1ffa4c8663dbce7818bb812471ab26220
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-01-19 22:16:19 +0100

    chore(sql): regenerate snapshots for clickhouse, duckdb and postgres

--- ibis/backends/tests/sql/snapshots/test_select_sql/test_subquery_in_union/out.sql
+++ ibis/backends/tests/sql/snapshots/test_select_sql/test_subquery_in_union/out.sql
@@ -1,57 +1,49 @@
+WITH t6 AS (
+  SELECT
+    t2.a,
+    t2.g,
+    t2.metric
+  FROM (
+    SELECT
+      t0.a,
+      t0.g,
+      SUM(t0.f) AS metric
+    FROM alltypes AS t0
+    GROUP BY
+      1,
+      2
+  ) AS t2
+  INNER JOIN (
+    SELECT
+      t0.a,
+      t0.g,
+      SUM(t0.f) AS metric
+    FROM alltypes AS t0
+    GROUP BY
+      1,
+      2
+  ) AS t4
+    ON t2.g = t4.g
+), t1 AS (
+  SELECT
+    t0.a,
+    t0.g,
+    SUM(t0.f) AS metric
+  FROM alltypes AS t0
+  GROUP BY
+    1,
+    2
+)
 SELECT
-  t8.a,
-  t8.g,
-  t8.metric
+  t9.a,
+  t9.g,
+  t9.metric
 FROM (
   SELECT
-    t2.a,
-    t2.g,
-    t2.metric
-  FROM (
-    SELECT
-      t0.a,
-      t0.g,
-      SUM(t0.f) AS metric
-    FROM alltypes AS t0
-    GROUP BY
-      1,
-      2
-  ) AS t2
-  INNER JOIN (
-    SELECT
-      t0.a,
-      t0.g,
-      SUM(t0.f) AS metric
-    FROM alltypes AS t0
-    GROUP BY
-      1,
-      2
-  ) AS t4
-    ON t2.g = t4.g
+    *
+  FROM t6 AS t7
   UNION ALL
   SELECT
-    t2.a,
-    t2.g,
-    t2.metric
-  FROM (
-    SELECT
-      t0.a,
-      t0.g,
-      SUM(t0.f) AS metric
-    FROM alltypes AS t0
-    GROUP BY
-      1,
-      2
-  ) AS t2
-  INNER JOIN (
-    SELECT
-      t0.a,
-      t0.g,
-      SUM(t0.f) AS metric
-    FROM alltypes AS t0
-    GROUP BY
-      1,
-      2
-  ) AS t4
-    ON t2.g = t4.g
-) AS t8
+    *
+  FROM t6 AS t8
+) AS t9
