d1797862c930f45621efb4d3f563c14cee467751
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-01-20 05:31:57 -0500

    chore(impala): regen snapshots

--- ibis/backends/impala/tests/snapshots/test_sql/test_nested_join_multiple_ctes/out.sql
+++ ibis/backends/impala/tests/snapshots/test_sql/test_nested_join_multiple_ctes/out.sql
@@ -1,61 +1,45 @@
-SELECT
-  `t7`.`userid`,
-  `t7`.`movieid`,
-  `t7`.`rating`,
-  `t7`.`datetime`,
-  `t7`.`title`
-FROM (
+WITH `t6` AS (
   SELECT
-    `t6`.`userid`,
-    `t6`.`movieid`,
-    `t6`.`rating`,
-    `t6`.`datetime`,
-    `t6`.`title`
+    `t4`.`userid`,
+    `t4`.`movieid`,
+    `t4`.`rating`,
+    `t4`.`datetime`,
+    `t2`.`title`
   FROM (
     SELECT
-      `t4`.`userid`,
-      `t4`.`movieid`,
-      `t4`.`rating`,
-      `t4`.`datetime`,
-      `t2`.`title`
-    FROM (
-      SELECT
-        `t0`.`userid`,
-        `t0`.`movieid`,
-        `t0`.`rating`,
-        CAST(`t0`.`timestamp` AS TIMESTAMP) AS `datetime`
-      FROM `ratings` AS `t0`
-    ) AS `t4`
-    INNER JOIN `movies` AS `t2`
-      ON `t4`.`movieid` = `t2`.`movieid`
-  ) AS `t6`
+      `t0`.`userid`,
+      `t0`.`movieid`,
+      `t0`.`rating`,
+      CAST(`t0`.`timestamp` AS TIMESTAMP) AS `datetime`
+    FROM `ratings` AS `t0`
+  ) AS `t4`
+  INNER JOIN `movies` AS `t2`
+    ON `t4`.`movieid` = `t2`.`movieid`
+)
+SELECT
+  `t8`.`userid`,
+  `t8`.`movieid`,
+  `t8`.`rating`,
+  `t8`.`datetime`,
+  `t8`.`title`
+FROM (
+  SELECT
+    `t7`.`userid`,
+    `t7`.`movieid`,
+    `t7`.`rating`,
+    `t7`.`datetime`,
+    `t7`.`title`
+  FROM `t6` AS `t7`
   WHERE
-    `t6`.`userid` = 118205 AND EXTRACT(year FROM `t6`.`datetime`) > 2001
-) AS `t7`
+    `t7`.`userid` = 118205 AND EXTRACT(year FROM `t7`.`datetime`) > 2001
+) AS `t8`
 WHERE
-  `t7`.`movieid` IN (
+  `t8`.`movieid` IN (
     SELECT
-      `t6`.`movieid`
-    FROM (
-      SELECT
-        `t4`.`userid`,
-        `t4`.`movieid`,
-        `t4`.`rating`,
-        `t4`.`datetime`,
-        `t2`.`title`
-      FROM (
-        SELECT
-          `t0`.`userid`,
-          `t0`.`movieid`,
-          `t0`.`rating`,
-          CAST(`t0`.`timestamp` AS TIMESTAMP) AS `datetime`
-        FROM `ratings` AS `t0`
-      ) AS `t4`
-      INNER JOIN `movies` AS `t2`
-        ON `t4`.`movieid` = `t2`.`movieid`
-    ) AS `t6`
+      `t7`.`movieid`
+    FROM `t6` AS `t7`
     WHERE
-      `t6`.`userid` = 118205
-      AND EXTRACT(year FROM `t6`.`datetime`) > 2001
-      AND EXTRACT(year FROM `t6`.`datetime`) < 2009
+      `t7`.`userid` = 118205
+      AND EXTRACT(year FROM `t7`.`datetime`) > 2001
+      AND EXTRACT(year FROM `t7`.`datetime`) < 2009
   )
