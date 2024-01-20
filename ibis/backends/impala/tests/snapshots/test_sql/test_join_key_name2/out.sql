197264551522dd4f8c3e62962d14d2bf45f7c6b6
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-01-20 05:31:57 -0500

    chore(impala): regen snapshots

--- ibis/backends/impala/tests/snapshots/test_sql/test_join_key_name2/out.sql
+++ ibis/backends/impala/tests/snapshots/test_sql/test_join_key_name2/out.sql
@@ -1,61 +1,35 @@
+WITH `t12` AS (
+  SELECT
+    EXTRACT(year FROM `t11`.`odate`) AS `year`,
+    COUNT(*) AS `CountStar()`
+  FROM (
+    SELECT
+      `t6`.`c_custkey`,
+      `t6`.`c_name`,
+      `t6`.`c_address`,
+      `t6`.`c_nationkey`,
+      `t6`.`c_phone`,
+      `t6`.`c_acctbal`,
+      `t6`.`c_mktsegment`,
+      `t6`.`c_comment`,
+      `t4`.`r_name` AS `region`,
+      `t7`.`o_totalprice`,
+      CAST(`t7`.`o_orderdate` AS TIMESTAMP) AS `odate`
+    FROM `tpch_region` AS `t4`
+    INNER JOIN `tpch_nation` AS `t5`
+      ON `t4`.`r_regionkey` = `t5`.`n_regionkey`
+    INNER JOIN `tpch_customer` AS `t6`
+      ON `t6`.`c_nationkey` = `t5`.`n_nationkey`
+    INNER JOIN `tpch_orders` AS `t7`
+      ON `t7`.`o_custkey` = `t6`.`c_custkey`
+  ) AS `t11`
+  GROUP BY
+    1
+)
 SELECT
-  `t13`.`year`,
-  `t13`.`CountStar()` AS `pre_count`,
-  `t15`.`CountStar()` AS `post_count`
-FROM (
-  SELECT
-    EXTRACT(year FROM `t11`.`odate`) AS `year`,
-    COUNT(*) AS `CountStar()`
-  FROM (
-    SELECT
-      `t6`.`c_custkey`,
-      `t6`.`c_name`,
-      `t6`.`c_address`,
-      `t6`.`c_nationkey`,
-      `t6`.`c_phone`,
-      `t6`.`c_acctbal`,
-      `t6`.`c_mktsegment`,
-      `t6`.`c_comment`,
-      `t4`.`r_name` AS `region`,
-      `t7`.`o_totalprice`,
-      CAST(`t7`.`o_orderdate` AS TIMESTAMP) AS `odate`
-    FROM `tpch_region` AS `t4`
-    INNER JOIN `tpch_nation` AS `t5`
-      ON `t4`.`r_regionkey` = `t5`.`n_regionkey`
-    INNER JOIN `tpch_customer` AS `t6`
-      ON `t6`.`c_nationkey` = `t5`.`n_nationkey`
-    INNER JOIN `tpch_orders` AS `t7`
-      ON `t7`.`o_custkey` = `t6`.`c_custkey`
-  ) AS `t11`
-  GROUP BY
-    1
-) AS `t13`
-INNER JOIN (
-  SELECT
-    EXTRACT(year FROM `t11`.`odate`) AS `year`,
-    COUNT(*) AS `CountStar()`
-  FROM (
-    SELECT
-      `t6`.`c_custkey`,
-      `t6`.`c_name`,
-      `t6`.`c_address`,
-      `t6`.`c_nationkey`,
-      `t6`.`c_phone`,
-      `t6`.`c_acctbal`,
-      `t6`.`c_mktsegment`,
-      `t6`.`c_comment`,
-      `t4`.`r_name` AS `region`,
-      `t7`.`o_totalprice`,
-      CAST(`t7`.`o_orderdate` AS TIMESTAMP) AS `odate`
-    FROM `tpch_region` AS `t4`
-    INNER JOIN `tpch_nation` AS `t5`
-      ON `t4`.`r_regionkey` = `t5`.`n_regionkey`
-    INNER JOIN `tpch_customer` AS `t6`
-      ON `t6`.`c_nationkey` = `t5`.`n_nationkey`
-    INNER JOIN `tpch_orders` AS `t7`
-      ON `t7`.`o_custkey` = `t6`.`c_custkey`
-  ) AS `t11`
-  GROUP BY
-    1
-) AS `t15`
-  ON `t13`.`year` = `t15`.`year`
+  `t14`.`year`,
+  `t14`.`CountStar()` AS `pre_count`,
+  `t16`.`CountStar()` AS `post_count`
+FROM `t12` AS `t14`
+INNER JOIN `t12` AS `t16`
+  ON `t14`.`year` = `t16`.`year`
--- ibis/backends/impala/tests/snapshots/test_sql/test_nested_joins_single_cte/out.sql
+++ ibis/backends/impala/tests/snapshots/test_sql/test_nested_joins_single_cte/out.sql
@@ -1,31 +1,25 @@
-SELECT
-  `t6`.`uuid`,
-  `t2`.`CountStar()`,
-  `t4`.`last_visit`
-FROM (
-  SELECT
-    `t1`.`uuid`,
-    MAX(`t1`.`CountStar()`) AS `max_count`
-  FROM (
-    SELECT
-      `t0`.`uuid`,
-      COUNT(*) AS `CountStar()`
-    FROM `t` AS `t0`
-    GROUP BY
-      1
-  ) AS `t1`
-  GROUP BY
-    1
-) AS `t6`
-LEFT OUTER JOIN (
+WITH `t1` AS (
   SELECT
     `t0`.`uuid`,
     COUNT(*) AS `CountStar()`
   FROM `t` AS `t0`
   GROUP BY
     1
-) AS `t2`
-  ON `t6`.`uuid` = `t2`.`uuid` AND `t6`.`max_count` = `t2`.`CountStar()`
+)
+SELECT
+  `t7`.`uuid`,
+  `t4`.`CountStar()`,
+  `t5`.`last_visit`
+FROM (
+  SELECT
+    `t2`.`uuid`,
+    MAX(`t2`.`CountStar()`) AS `max_count`
+  FROM `t1` AS `t2`
+  GROUP BY
+    1
+) AS `t7`
+LEFT OUTER JOIN `t1` AS `t4`
+  ON `t7`.`uuid` = `t4`.`uuid` AND `t7`.`max_count` = `t4`.`CountStar()`
 LEFT OUTER JOIN (
   SELECT
     `t0`.`uuid`,
@@ -33,5 +27,5 @@ LEFT OUTER JOIN (
   FROM `t` AS `t0`
   GROUP BY
     1
-) AS `t4`
-  ON `t6`.`uuid` = `t4`.`uuid`
+) AS `t5`
+  ON `t7`.`uuid` = `t5`.`uuid`
