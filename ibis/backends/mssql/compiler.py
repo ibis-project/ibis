6ad90f8a70ca86fdf04ebd3e024b5a3ac6d02284
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-02-01 11:17:32 +0100

    chore(mssql): rename ops.ArrayColumn to ops.Array

--- ibis/backends/mssql/compiler.py
+++ ibis/backends/mssql/compiler.py
@@ -383,7 +383,7 @@ class MSSQLCompiler(SQLGlotCompiler):
     @visit_node.register(ops.ArgMax)
     @visit_node.register(ops.ArgMin)
     @visit_node.register(ops.ArrayCollect)
-    @visit_node.register(ops.ArrayColumn)
+    @visit_node.register(ops.Array)
     @visit_node.register(ops.ArrayDistinct)
     @visit_node.register(ops.ArrayFlatten)
     @visit_node.register(ops.ArrayMap)
