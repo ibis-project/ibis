b2cff6af20bd025c1c90e94b55199a31150b4a33
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-02-06 14:41:24 +0100

    chore(duckdb): update snapshots for `ops.Count` to include the table's name

--- ibis/backends/tests/sql/snapshots/test_sql/test_aggregate/having_count/out.sql
+++ ibis/backends/tests/sql/snapshots/test_sql/test_aggregate/having_count/out.sql
@@ -5,10 +5,10 @@ FROM (
   SELECT
     "t0"."foo_id",
     SUM("t0"."f") AS "total",
-    COUNT(*) AS "CountStar()"
+    COUNT(*) AS "CountStar(star1)"
   FROM "star1" AS "t0"
   GROUP BY
     1
 ) AS "t1"
 WHERE
-  "t1"."CountStar()" > CAST(100 AS TINYINT)
+  "t1"."CountStar(star1)" > CAST(100 AS TINYINT)
