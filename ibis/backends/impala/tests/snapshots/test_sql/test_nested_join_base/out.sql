04f3e5ef7783b9df324639dc769bbaf5e79ce568
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-02-06 17:20:00 +0100

    refactor(ir): remove now obsolete `ops.Named` mixin (#8244)
    Co-authored-by: Phillip Cloud <417981+cpcloud@users.noreply.github.com>

--- ibis/backends/impala/tests/snapshots/test_sql/test_nested_join_base/out.sql
+++ ibis/backends/impala/tests/snapshots/test_sql/test_nested_join_base/out.sql
@@ -1,18 +1,18 @@
 WITH `t1` AS (
   SELECT
     `t0`.`uuid`,
-    COUNT(*) AS `CountStar()`
+    COUNT(*) AS `CountStar(t)`
   FROM `t` AS `t0`
   GROUP BY
     1
 )
 SELECT
   `t5`.`uuid`,
-  `t3`.`CountStar()`
+  `t3`.`CountStar(t)`
 FROM (
   SELECT
     `t2`.`uuid`,
-    MAX(`t2`.`CountStar()`) AS `max_count`
+    MAX(`t2`.`CountStar(t)`) AS `max_count`
   FROM `t1` AS `t2`
   GROUP BY
     1
