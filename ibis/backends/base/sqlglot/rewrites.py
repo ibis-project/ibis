56ac90f147aff2405a23020d637432d01379deb9
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-02-15 07:24:45 -0500

    chore: simplify rewrites

--- ibis/backends/base/sqlglot/rewrites.py
+++ ibis/backends/base/sqlglot/rewrites.py
@@ -249,16 +249,11 @@ def add_one_to_nth_value_input(_, **kwargs):
 def rewrite_capitalize(_, **kwargs):
     """Rewrite Capitalize in terms of substring, concat, upper, and lower."""
     first = ops.Uppercase(ops.Substring(_.arg, start=0, length=1))
-    length_minus_one = ops.Subtract(ops.StringLength(_.arg), 1)
-    rest = ops.Lowercase(
-        ops.Substring(
-            _.arg,
-            start=1,
-            # we can't use greatest here because datafusion doesn't support it
-            # in its sql dialect
-            length=ops.IfElse(ops.Less(length_minus_one, 0), 0, length_minus_one),
-        )
-    )
+    # use length instead of length - 1 to avoid backends complaining about
+    # asking for negative length
+    #
+    # there are at most length - 1 characters, so asking for length is fine
+    rest = ops.Lowercase(ops.Substring(_.arg, start=1, length=ops.StringLength(_.arg)))
     return ops.StringConcat((first, rest))
 
 
