08e9d1e6915083b1f349af6322c7d77b105479e9
Naty Clementi <natyclementi@gmail.com>
2024-02-16 06:03:42 -0500

    chore: add env for arm64 and xfail geospatial tests (#8371)
    Co-authored-by: Phillip Cloud <417981+cpcloud@users.noreply.github.com>

--- ibis/backends/duckdb/tests/snapshots/test_geospatial/test_literal_geospatial_inferred/shp8-0_0_1_1_2_2/out.sql
+++ ibis/backends/duckdb/tests/snapshots/test_geospatial/test_literal_geospatial_inferred/shp8-0_0_1_1_2_2/out.sql
@@ -2,5 +2,5 @@ SELECT
   ST_ASWKB("result") AS result
 FROM (
   SELECT
-    ST_GEOMFROMTEXT('MULTIPOINT (0 0, 1 1, 2 2)') AS "result"
+    ST_GEOMFROMTEXT('MULTIPOINT ((0 0), (1 1), (2 2))') AS "result"
 )
--- ibis/backends/postgres/tests/test_geospatial.py
+++ ibis/backends/postgres/tests/test_geospatial.py
@@ -81,7 +81,13 @@ def test_literal_geospatial_explicit(con, expr, snapshot):
         shp_polygon_0,
         shp_multipolygon_0,
         shp_multilinestring_0,
-        shp_multipoint_0,
+        param(
+            shp_multipoint_0,
+            marks=pytest.mark.xfail(
+                raises=AssertionError,
+                reason="Bug-fix change in GEOS 3.12 see shapely issue #1992",
+            ),
+        ),
     ],
 )
 def test_literal_geospatial_inferred(con, shp, snapshot):
@@ -387,7 +393,15 @@ def test_geo_dataframe(geotable):
             id="polygon_single",
         ),
         # Multipart geometries (2D)
-        param("multipoint", ((10, 40), (40, 30), (20, 20), (30, 10)), id="multipoint"),
+        param(
+            "multipoint",
+            ((10, 40), (40, 30), (20, 20), (30, 10)),
+            id="multipoint",
+            marks=pytest.mark.xfail(
+                raises=AssertionError,
+                reason="Bug-fix change in GEOS 3.12 see shapely issue #1992",
+            ),
+        ),
         param(
             "multilinestring",
             (((10, 10), (20, 20), (10, 40)), ((40, 40), (30, 30), (40, 20), (30, 10))),
@@ -409,7 +423,6 @@ def test_geo_dataframe(geotable):
 def test_geo_literals_smoke(con, shape, value, modifier, snapshot):
     """Smoke tests for geo spatial literals."""
     expr = ibis.literal(value, type=getattr(dt, shape).copy(**modifier))
-
     snapshot.assert_match(con.compile(expr), "out.sql")
 
 
