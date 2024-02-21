5c0c6aa490f5c36b17f0e9fe5711154c53ae1d23
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-02-21 14:14:32 -0500

    test(snowflake): allow connecting with config files for more convenient test running (#8407)
    ## Description of changes
    
    This PR enables use of the [Snowflake `connections.toml`
    file](https://docs.snowflake.com/en/developer-guide/python-connector/python-connector-connect#connecting-using-the-connections-toml-file)
    in our test setup, which allows a dev to add connection information to
    a `$SNOWFLAKE_HOME/connections.toml` file, which will be used if
    `$SNOWFLAKE_URL` is not and `$SNOWFLAKE_HOME` is set.
    
    The main benefit is that credentials can persist somewhere, which allows
    devs
    to run `pytest -m snowflake` without setting an environment variable
    each time
    a new shell session is launched.
    
    Existing test setups should continue to work.

--- ibis/backends/snowflake/tests/conftest.py
+++ ibis/backends/snowflake/tests/conftest.py
@@ -28,6 +28,26 @@ if TYPE_CHECKING:
 def _get_url():
     if (url := os.environ.get("SNOWFLAKE_URL")) is not None:
         return url
+    elif os.environ.get("SNOWFLAKE_HOME"):
+        import tomli
+
+        # requires a connection named ibis_testing or one explicitly set with
+        # export SNOWFLAKE_DEFAULT_CONNECTION_NAME
+        connection_name = os.environ.get(
+            "SNOWFLAKE_DEFAULT_CONNECTION_NAME", "ibis_testing"
+        )
+        config_file = Path(os.environ["SNOWFLAKE_HOME"], "connections.toml")
+        config_text = config_file.read_text()
+        params = tomli.loads(config_text)[connection_name]
+        user, password, account, database, schema, warehouse = (
+            params["user"],
+            params["password"],
+            params["account"],
+            params["database"],
+            params["schema"],
+            params["warehouse"],
+        )
+        return f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}"
     else:
         try:
             user, password, account, database, schema, warehouse = tuple(
