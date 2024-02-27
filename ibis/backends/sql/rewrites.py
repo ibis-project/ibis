da3f525deb7fd1d4be6f9e1628055e2a31922dd0
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-02-27 23:04:10 +0100

    fix(sql): don't fuse subsequent order by operations

--- ibis/backends/sql/rewrites.py
+++ ibis/backends/sql/rewrites.py
@@ -81,6 +81,12 @@ class Window(ops.Value):
         return self.func.dtype
 
 
+# TODO(kszucs): there is a better strategy to rewrite the relational operations
+# to Select nodes by wrapping the leaf nodes in a Select node and then merging
+# Project, Filter, Sort, etc. incrementally into the Select node. This way we
+# can have tighter control over simplification logic.
+
+
 @replace(p.Project)
 def project_to_select(_, **kwargs):
     """Convert a Project node to a Select node."""
@@ -130,20 +136,20 @@ def merge_select_select(_, **kwargs):
     for v in _.predicates:
         if v.find((ops.ExistsSubquery, ops.InSubquery), filter=ops.Value):
             return _
+    # cannot merge if the inner select has a different order keys
+    if _.sort_keys and _.parent.sort_keys:
+        return _
 
     subs = {ops.Field(_.parent, k): v for k, v in _.parent.values.items()}
     selections = {k: v.replace(subs, filter=ops.Value) for k, v in _.selections.items()}
     predicates = tuple(p.replace(subs, filter=ops.Value) for p in _.predicates)
     sort_keys = tuple(s.replace(subs, filter=ops.Value) for s in _.sort_keys)
 
-    unique_predicates = toolz.unique(_.parent.predicates + predicates)
-    unique_sort_keys = {s.expr: s for s in _.parent.sort_keys + sort_keys}
-
     return Select(
         _.parent.parent,
         selections=selections,
-        predicates=unique_predicates,
-        sort_keys=unique_sort_keys.values(),
+        predicates=toolz.unique(_.parent.predicates + predicates),
+        sort_keys=_.parent.sort_keys + sort_keys,
     )
 
 
