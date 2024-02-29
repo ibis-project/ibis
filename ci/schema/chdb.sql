4701983c04a781c18c33924d53161f29ca8c8b9b
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-02-29 14:41:27 +0100

    chore: convert date and timestamp columns to their appropiate arrow type

--- ci/schema/chdb.sql
+++ ci/schema/chdb.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE TABLE ibis_testing.tzone (
     ts Nullable(DateTime),
     key Nullable(String),
     value Nullable(Float64)
-) ENGINE = Memory;
+) ENGINE = Log;
 
 CREATE OR REPLACE TABLE ibis_testing.array_types (
     x Array(Nullable(Int64)),
@@ -28,39 +28,38 @@ CREATE OR REPLACE TABLE ibis_testing.array_types (
     grouper Nullable(String),
     scalar_column Nullable(Float64),
     multi_dim Array(Array(Nullable(Int64)))
-) ENGINE = Memory;
-
--- INSERT INTO ibis_testing.array_types VALUES
---     ([1, 2, 3], ['a', 'b', 'c'], [1.0, 2.0, 3.0], 'a', 1.0, [[], [1, 2, 3], []]),
---     ([4, 5], ['d', 'e'], [4.0, 5.0], 'a', 2.0, []),
---     ([6, NULL], ['f', NULL], [6.0, NULL], 'a', 3.0, [[], [], []]),
---     ([NULL, 1, NULL], [NULL, 'a', NULL], [], 'b', 4.0, [[1], [2], [], [3, 4, 5]]),
---     ([2, NULL, 3], ['b', NULL, 'c'], NULL, 'b', 5.0, []),
---     ([4, NULL, NULL, 5], ['d', NULL, NULL, 'e'], [4.0, NULL, NULL, 5.0], 'c', 6.0, [[1, 2, 3]]);
+) ENGINE = Log;
+INSERT INTO ibis_testing.array_types VALUES
+    ([1, 2, 3], ['a', 'b', 'c'], [1.0, 2.0, 3.0], 'a', 1.0, [[], [1, 2, 3], []]),
+    ([4, 5], ['d', 'e'], [4.0, 5.0], 'a', 2.0, []),
+    ([6, NULL], ['f', NULL], [6.0, NULL], 'a', 3.0, [[], [], []]),
+    ([NULL, 1, NULL], [NULL, 'a', NULL], [], 'b', 4.0, [[1], [2], [], [3, 4, 5]]),
+    ([2, NULL, 3], ['b', NULL, 'c'], NULL, 'b', 5.0, []),
+    ([4, NULL, NULL, 5], ['d', NULL, NULL, 'e'], [4.0, NULL, NULL, 5.0], 'c', 6.0, [[1, 2, 3]]);
 
 CREATE OR REPLACE TABLE ibis_testing.time_df1 (
     time Int64,
     value Nullable(Float64),
     key Nullable(String)
-) ENGINE = Memory;
--- INSERT INTO ibis_testing.time_df1 VALUES
---     (1, 1.0, 'x'),
---     (20, 20.0, 'x'),
---     (30, 30.0, 'x'),
---     (40, 40.0, 'x'),
---     (50, 50.0, 'x');
+) ENGINE = Log;
+INSERT INTO ibis_testing.time_df1 VALUES
+    (1, 1.0, 'x'),
+    (20, 20.0, 'x'),
+    (30, 30.0, 'x'),
+    (40, 40.0, 'x'),
+    (50, 50.0, 'x');
 
 CREATE OR REPLACE TABLE ibis_testing.time_df2 (
     time Int64,
     value Nullable(Float64),
     key Nullable(String)
-) ENGINE = Memory;
--- INSERT INTO ibis_testing.time_df2 VALUES
---     (19, 19.0, 'x'),
---     (21, 21.0, 'x'),
---     (39, 39.0, 'x'),
---     (49, 49.0, 'x'),
---     (1000, 1000.0, 'x');
+) ENGINE = Log;
+INSERT INTO ibis_testing.time_df2 VALUES
+    (19, 19.0, 'x'),
+    (21, 21.0, 'x'),
+    (39, 39.0, 'x'),
+    (49, 49.0, 'x'),
+    (1000, 1000.0, 'x');
 
 CREATE OR REPLACE TABLE ibis_testing.struct (
     abc Tuple(
@@ -68,29 +67,28 @@ CREATE OR REPLACE TABLE ibis_testing.struct (
         b Nullable(String),
         c Nullable(Int64)
     )
-) ENGINE = Memory;
+) ENGINE = Log;
 
 -- NULL is the same as tuple(NULL, NULL, NULL) because clickhouse doesn't
 -- support Nullable(Tuple(...))
--- INSERT INTO ibis_testing.struct VALUES
---     (tuple(1.0, 'banana', 2)),
---     (tuple(2.0, 'apple', 3)),
---     (tuple(3.0, 'orange', 4)),
---     (tuple(NULL, 'banana', 2)),
---     (tuple(2.0, NULL, 3)),
---     (tuple(NULL, NULL, NULL)),
---     (tuple(3.0, 'orange', NULL));
+INSERT INTO ibis_testing.struct VALUES
+    (tuple(1.0, 'banana', 2)),
+    (tuple(2.0, 'apple', 3)),
+    (tuple(3.0, 'orange', 4)),
+    (tuple(NULL, 'banana', 2)),
+    (tuple(2.0, NULL, 3)),
+    (tuple(NULL, NULL, NULL)),
+    (tuple(3.0, 'orange', NULL));
 
 CREATE OR REPLACE TABLE ibis_testing.map (kv Map(String, Nullable(Int64))) ENGINE = Memory;
-
--- INSERT INTO ibis_testing.map VALUES
---     (map('a', 1, 'b', 2, 'c', 3)),
---     (map('d', 4, 'e', 5, 'c', 6));
+INSERT INTO ibis_testing.map VALUES
+    (map('a', 1, 'b', 2, 'c', 3)),
+    (map('d', 4, 'e', 5, 'c', 6));
 
 CREATE OR REPLACE TABLE ibis_testing.win (g Nullable(String), x Int64, y Nullable(Int64)) ENGINE = Memory;
--- INSERT INTO ibis_testing.win VALUES
---     ('a', 0, 3),
---     ('a', 1, 2),
---     ('a', 2, 0),
---     ('a', 3, 1),
---     ('a', 4, 1);
+INSERT INTO ibis_testing.win VALUES
+    ('a', 0, 3),
+    ('a', 1, 2),
+    ('a', 2, 0),
+    ('a', 3, 1),
+    ('a', 4, 1);
