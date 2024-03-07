aab0c139a3fa787aadcd23be213659dd50a5ab5e
Jim Crist-Harif <jcristharif@gmail.com>
2024-03-06 19:12:29 -0600

    fix(sql): support set operations wrapping subqueries (#8414)

--- ibis/backends/tests/sql/snapshots/test_compiler/test_union/out.sql
+++ ibis/backends/tests/sql/snapshots/test_compiler/test_union/out.sql
@@ -3,16 +3,24 @@ SELECT
   "t3"."value"
 FROM (
   SELECT
-    "t0"."string_col" AS "key",
-    CAST("t0"."float_col" AS DOUBLE) AS "value"
-  FROM "functional_alltypes" AS "t0"
-  WHERE
-    "t0"."int_col" > CAST(0 AS TINYINT)
+    *
+  FROM (
+    SELECT
+      "t0"."string_col" AS "key",
+      CAST("t0"."float_col" AS DOUBLE) AS "value"
+    FROM "functional_alltypes" AS "t0"
+    WHERE
+      "t0"."int_col" > CAST(0 AS TINYINT)
+  ) AS "t1"
   UNION
   SELECT
-    "t0"."string_col" AS "key",
-    "t0"."double_col" AS "value"
-  FROM "functional_alltypes" AS "t0"
-  WHERE
-    "t0"."int_col" <= CAST(0 AS TINYINT)
+    *
+  FROM (
+    SELECT
+      "t0"."string_col" AS "key",
+      "t0"."double_col" AS "value"
+    FROM "functional_alltypes" AS "t0"
+    WHERE
+      "t0"."int_col" <= CAST(0 AS TINYINT)
+  ) AS "t2"
 ) AS "t3"
