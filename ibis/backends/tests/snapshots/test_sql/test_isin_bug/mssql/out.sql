f488a1d1340154842473044abaec331093f6cf0f
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-18 13:55:45 -0400

    chore: revert snapshots

--- ibis/backends/tests/snapshots/test_sql/test_isin_bug/mssql/out.sql
+++ ibis/backends/tests/snapshots/test_sql/test_isin_bug/mssql/out.sql
@@ -1,7 +1,13 @@
 SELECT
-  IIF([t0].[x] IN (SELECT
-    [t0].[x]
-  FROM [t] AS [t0]
-  WHERE
-    [t0].[x] > 2), 1, 0) AS [InSubquery(x)]
+  IIF(
+    [t0].[x] IN (
+      SELECT
+        [t0].[x]
+      FROM [t] AS [t0]
+      WHERE
+        [t0].[x] > 2
+    ),
+    1,
+    0
+  ) AS [InSubquery(x)]
 FROM [t] AS [t0]
