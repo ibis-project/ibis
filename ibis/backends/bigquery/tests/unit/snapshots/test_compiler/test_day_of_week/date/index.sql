680c336f5fa63aaa3a50c5b692438b9f4bf9949c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-20 07:33:21 -0400

    chore(bigquery): regen sql snapshots for mod tests

--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_day_of_week/date/index.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_day_of_week/date/index.sql
@@ -1,2 +1,2 @@
 SELECT
-  mod(EXTRACT(dayofweek FROM DATE(2017, 1, 1)) + 5, 7) AS `DayOfWeekIndex_datetime_date_2017_ 1_ 1`
+  MOD(EXTRACT(dayofweek FROM DATE(2017, 1, 1)) + 5, 7) AS `DayOfWeekIndex_datetime_date_2017_ 1_ 1`
