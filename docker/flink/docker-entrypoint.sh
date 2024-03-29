beaba50f3e0a7ee51e1de3bbd83d95c9bef97b25
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-03-29 09:00:38 -0400

    chore(flink): disable shellcheck for testing

--- docker/flink/docker-entrypoint.sh
+++ docker/flink/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=all
 
 ###############################################################################
 #  Licensed to the Apache Software Foundation (ASF) under one
