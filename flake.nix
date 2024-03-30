15ed86a425fa49fed43e511e0a29d68099e4a162
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-30 07:56:38 -0400

    chore(deps): downgrade nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
@@ -138,7 +138,7 @@
         ibis311 = mkDevShell pkgs.ibisDevEnv311;
         ibis312 = mkDevShell pkgs.ibisDevEnv312;
 
-        default = ibis312;
+        default = ibis311;
 
         preCommit = pkgs.mkShell {
           name = "preCommit";
