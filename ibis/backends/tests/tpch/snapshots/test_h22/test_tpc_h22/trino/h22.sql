ba931dac4c11494ed3981bdd5368d6b49afcf92c
Krisztián Szűcs <szucs.krisztian@gmail.com>
2024-04-07 13:24:58 +0200

    fix(sql): avoid excessive inlining during `Select` merge (#8825)

--- ibis/backends/tests/tpch/snapshots/test_h22/test_tpc_h22/trino/h22.sql
+++ ibis/backends/tests/tpch/snapshots/test_h22/test_tpc_h22/trino/h22.sql
@@ -1,67 +1,78 @@
 SELECT
-  "t7"."cntrycode",
-  "t7"."numcust",
-  "t7"."totacctbal"
+  "t8"."cntrycode",
+  "t8"."numcust",
+  "t8"."totacctbal"
 FROM (
   SELECT
-    "t6"."cntrycode",
+    "t7"."cntrycode",
     COUNT(*) AS "numcust",
-    SUM("t6"."c_acctbal") AS "totacctbal"
+    SUM("t7"."c_acctbal") AS "totacctbal"
   FROM (
     SELECT
-      SUBSTRING("t2"."c_phone", IF((
+      SUBSTRING("t6"."c_phone", IF((
         0 + 1
-      ) >= 1, 0 + 1, 0 + 1 + LENGTH("t2"."c_phone")), 2) AS "cntrycode",
-      "t2"."c_acctbal"
+      ) >= 1, 0 + 1, 0 + 1 + LENGTH("t6"."c_phone")), 2) AS "cntrycode",
+      "t6"."c_acctbal"
     FROM (
       SELECT
-        "t0"."c_custkey",
-        "t0"."c_name",
-        "t0"."c_address",
-        "t0"."c_nationkey",
-        "t0"."c_phone",
-        CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) AS "c_acctbal",
-        "t0"."c_mktsegment",
-        "t0"."c_comment"
-      FROM "hive"."ibis_sf1"."customer" AS "t0"
-    ) AS "t2"
-    WHERE
-      SUBSTRING("t2"."c_phone", IF((
-        0 + 1
-      ) >= 1, 0 + 1, 0 + 1 + LENGTH("t2"."c_phone")), 2) IN ('13', '31', '23', '29', '30', '18', '17')
-      AND "t2"."c_acctbal" > (
+        "t2"."c_custkey",
+        "t2"."c_name",
+        "t2"."c_address",
+        "t2"."c_nationkey",
+        "t2"."c_phone",
+        "t2"."c_acctbal",
+        "t2"."c_mktsegment",
+        "t2"."c_comment"
+      FROM (
         SELECT
-          AVG("t3"."c_acctbal") AS "Mean(c_acctbal)"
-        FROM (
+          "t0"."c_custkey",
+          "t0"."c_name",
+          "t0"."c_address",
+          "t0"."c_nationkey",
+          "t0"."c_phone",
+          CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) AS "c_acctbal",
+          "t0"."c_mktsegment",
+          "t0"."c_comment"
+        FROM "hive"."ibis_sf1"."customer" AS "t0"
+      ) AS "t2"
+      WHERE
+        SUBSTRING("t2"."c_phone", IF((
+          0 + 1
+        ) >= 1, 0 + 1, 0 + 1 + LENGTH("t2"."c_phone")), 2) IN ('13', '31', '23', '29', '30', '18', '17')
+        AND "t2"."c_acctbal" > (
           SELECT
-            "t0"."c_custkey",
-            "t0"."c_name",
-            "t0"."c_address",
-            "t0"."c_nationkey",
-            "t0"."c_phone",
-            CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) AS "c_acctbal",
-            "t0"."c_mktsegment",
-            "t0"."c_comment"
-          FROM "hive"."ibis_sf1"."customer" AS "t0"
-          WHERE
-            CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) > CAST(0.0 AS DOUBLE)
-            AND SUBSTRING("t0"."c_phone", IF((
-              0 + 1
-            ) >= 1, 0 + 1, 0 + 1 + LENGTH("t0"."c_phone")), 2) IN ('13', '31', '23', '29', '30', '18', '17')
-        ) AS "t3"
-      )
-      AND NOT (
-        EXISTS(
-          SELECT
-            1
-          FROM "hive"."ibis_sf1"."orders" AS "t1"
-          WHERE
-            "t1"."o_custkey" = "t2"."c_custkey"
+            AVG("t3"."c_acctbal") AS "Mean(c_acctbal)"
+          FROM (
+            SELECT
+              "t0"."c_custkey",
+              "t0"."c_name",
+              "t0"."c_address",
+              "t0"."c_nationkey",
+              "t0"."c_phone",
+              CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) AS "c_acctbal",
+              "t0"."c_mktsegment",
+              "t0"."c_comment"
+            FROM "hive"."ibis_sf1"."customer" AS "t0"
+            WHERE
+              CAST("t0"."c_acctbal" AS DECIMAL(15, 2)) > CAST(0.0 AS DOUBLE)
+              AND SUBSTRING("t0"."c_phone", IF((
+                0 + 1
+              ) >= 1, 0 + 1, 0 + 1 + LENGTH("t0"."c_phone")), 2) IN ('13', '31', '23', '29', '30', '18', '17')
+          ) AS "t3"
         )
-      )
-  ) AS "t6"
+        AND NOT (
+          EXISTS(
+            SELECT
+              1
+            FROM "hive"."ibis_sf1"."orders" AS "t1"
+            WHERE
+              "t1"."o_custkey" = "t2"."c_custkey"
+          )
+        )
+    ) AS "t6"
+  ) AS "t7"
   GROUP BY
     1
-) AS "t7"
+) AS "t8"
 ORDER BY
-  "t7"."cntrycode" ASC
+  "t8"."cntrycode" ASC
