b1858b33c3bed42c35293fcc4907b6459ea68b75
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-04-16 08:35:15 -0400

    test(big): add timeouts for benchmarks

--- ibis/tests/benchmarks/test_benchmarks.py
+++ ibis/tests/benchmarks/test_benchmarks.py
@@ -18,7 +18,7 @@ import ibis.expr.operations as ops
 import ibis.expr.types as ir
 from ibis.backends import _get_backend_names
 
-pytestmark = pytest.mark.benchmark
+pytestmark = [pytest.mark.benchmark, pytest.mark.timeout(30)]
 
 
 def make_t():
@@ -813,6 +813,7 @@ def test_big_join_compile(benchmark, src, diff):
     assert len(t)
 
 
+@pytest.mark.timeout(5)
 def test_big_expression_compile(benchmark):
     from ibis.tests.benchmarks.benchfuncs import clean_names
 
