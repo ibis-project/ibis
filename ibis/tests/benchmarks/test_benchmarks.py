478db769249af72474dc8a522cd8d27e99de9e8a
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-04-16 08:27:17 -0400

    test(perf): add giant expression benchmark

--- ibis/tests/benchmarks/test_benchmarks.py
+++ ibis/tests/benchmarks/test_benchmarks.py
@@ -811,3 +811,23 @@ def test_big_join_compile(benchmark, src, diff):
         warmup_rounds=1,
     )
     assert len(t)
+
+
+def test_big_expression_compile(benchmark):
+    from ibis.tests.benchmarks.benchfuncs import clean_names
+
+    t = ibis.table(
+        schema={
+            "id": "int64",
+            "prefix": "string",
+            "first_name": "string",
+            "middle_name": "string",
+            "last_name": "string",
+            "suffix": "string",
+            "nickname": "string",
+        },
+        name="names",
+    )
+    t2 = clean_names(t)
+
+    assert benchmark(ibis.to_sql, t2, dialect="duckdb")
