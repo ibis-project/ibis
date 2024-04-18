728d568a4b602f37c9f83389299d143102c92ea0
Gil Forsyth <gil@forsyth.dev>
2024-04-18 13:50:28 -0400

    chore(container): add gdal to codespace

--- .devcontainer/updateContent.sh
+++ .devcontainer/updateContent.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+apt update
+apt install libgdal-dev
+
 # install ibis
 python3 -m pip install ipython
-python3 -m pip install -e '.[clickhouse,duckdb,clickhouse,examples,geospatial]'
+python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial]'
