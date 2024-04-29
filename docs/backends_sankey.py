e62cc76514ab576c480b48a7a090efffaf29d645
Cody <cody@dkdc.dev>
2024-04-29 13:50:42 -0400

    better png?

--- docs/backends_sankey.py
+++ docs/backends_sankey.py
@@ -16,97 +16,87 @@ def to_greyish(hex_code, grey_value=128):
     return new_hex_code
 
 
-def create_sanky(theme="light"):
-    theme = "plotly_white" if theme == "light" else "plotly_dark"
+category_colors = {
+    "Ibis API": "#7C65A0",
+    "SQL code": "#6A9BC9",
+    "DataFrame code": "#D58273",
+}
 
-    category_colors = {
-        "Ibis API": "#7C65A0",
-        "SQL code": "#6A9BC9",
-        "DataFrame code": "#D58273",
-    }
+backend_categories = {
+    list(category_colors.keys())[1]: [
+        "BigQuery",
+        "ClickHouse",
+        "DataFusion",
+        "Druid",
+        "DuckDB",
+        "Exasol",
+        "Flink",
+        "Impala",
+        "MSSQL",
+        "MySQL",
+        "Oracle",
+        "PostgreSQL",
+        "PySpark",
+        "RisingWave",
+        "Snowflake",
+        "SQLite",
+        "Trino",
+    ],
+    list(category_colors.keys())[2]: ["Dask", "pandas", "Polars"],
+}
 
-    backend_categories = {
-        list(category_colors.keys())[1]: [
-            "BigQuery",
-            "ClickHouse",
-            "DataFusion",
-            "Druid",
-            "DuckDB",
-            "Exasol",
-            "Flink",
-            "Impala",
-            "MSSQL",
-            "MySQL",
-            "Oracle",
-            "PostgreSQL",
-            "PySpark",
-            "RisingWave",
-            "Snowflake",
-            "SQLite",
-            "Trino",
-        ],
-        list(category_colors.keys())[2]: ["Dask", "pandas", "Polars"],
-    }
+nodes, links = [], []
+node_index = {}
 
-    nodes, links = [], []
-    node_index = {}
+nodes.append({"label": "Ibis API", "color": category_colors["Ibis API"]})
+node_index["Ibis API"] = 0
 
-    nodes.append({"label": "Ibis API", "color": category_colors["Ibis API"]})
-    node_index["Ibis API"] = 0
+idx = 1
+for category, backends in backend_categories.items():
+    nodes.append({"label": category, "color": category_colors[category]})
+    node_index[category] = idx
+    links.append({"source": 0, "target": idx, "value": len(backends)})
+    idx += 1
 
-    idx = 1
-    for category, backends in backend_categories.items():
-        nodes.append({"label": category, "color": category_colors[category]})
-        node_index[category] = idx
-        links.append({"source": 0, "target": idx, "value": len(backends)})
-        idx += 1
+    for backend in backends:
+        if backend not in node_index:
+            nodes.append({"label": backend, "color": category_colors[category]})
+            node_index[backend] = idx
+            idx += 1
+        links.append(
+            {
+                "source": node_index[category],
+                "target": node_index[backend],
+                "value": 1,
+            }
+        )
 
-        for backend in backends:
-            if backend not in node_index:
-                nodes.append({"label": backend, "color": category_colors[category]})
-                node_index[backend] = idx
-                idx += 1
-            links.append(
-                {
-                    "source": node_index[category],
-                    "target": node_index[backend],
-                    "value": 1,
-                }
-            )
+fig = go.Figure(
+    data=[
+        go.Sankey(
+            node=dict(
+                pad=20,
+                thickness=20,
+                line=dict(color="grey", width=0.5),
+                label=[node["label"] for node in nodes],
+                color=[node["color"] for node in nodes],
+            ),
+            link=dict(
+                source=[link["source"] for link in links],
+                target=[link["target"] for link in links],
+                value=[link["value"] for link in links],
+                line=dict(color="grey", width=0.5),
+                color=[to_greyish(nodes[link["target"]]["color"]) for link in links],
+            ),
+        )
+    ],
+)
 
-    fig = go.Figure(
-        data=[
-            go.Sankey(
-                node=dict(
-                    pad=20,
-                    thickness=20,
-                    line=dict(color="grey", width=0.5),
-                    label=[node["label"] for node in nodes],
-                    color=[node["color"] for node in nodes],
-                ),
-                link=dict(
-                    source=[link["source"] for link in links],
-                    target=[link["target"] for link in links],
-                    value=[link["value"] for link in links],
-                    line=dict(color="grey", width=0.5),
-                    color=[
-                        to_greyish(nodes[link["target"]]["color"]) for link in links
-                    ],
-                ),
-            )
-        ],
-    )
-    fig.update_layout(
-        title_text="Ibis backend types",
-        font_size=24,
-        font_family="Arial",
-        title_font_size=30,
-        margin=dict(l=30, r=30, t=80, b=30),
-        template=theme,
-    )
-
-    return fig
-
-
-# fig = create_sanky()
-fig = create_sanky(theme="dark")
+fig.update_layout(
+    title_text="Ibis backend types",
+    font_size=24,
+    # font_family="Arial",
+    title_font_size=30,
+    margin=dict(l=30, r=30, t=80, b=30),
+    template="plotly_dark",
+)
