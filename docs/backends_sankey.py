c68f9d4f53163b5f996da6ad7b9760d487529ac4
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2024-05-02 12:20:45 -0400

    docs: rework the homepage (#9088)
    ## Description of changes
    
    This is a simple rework of the homepage. CSS/fancy things are ignored
    for now, but could be added in later. For now, this:
    
    - adds a codespace button in the getting started tutorial
    - small adjustment to sankey diagram (font size)
    - makes the Ibis homepage have some of the stuff from the GitHub README
    - explain a bit more and show some code examples
    - unhide the quickstart from buttons
    - other edits
    
    ## Issues closed
    
    #8856
    
    ---------
    
    Co-authored-by: Phillip Cloud <417981+cpcloud@users.noreply.github.com>
    Co-authored-by: nick <889427+shicholas@users.noreply.github.com>

--- docs/backends_sankey.py
+++ docs/backends_sankey.py
@@ -94,7 +94,7 @@ fig = go.Figure(
 
 fig.update_layout(
     title_text="Ibis backend types",
-    font_size=24,
+    font_size=20,
     # font_family="Arial",
     title_font_size=30,
     margin=dict(l=30, r=30, t=80, b=30),
