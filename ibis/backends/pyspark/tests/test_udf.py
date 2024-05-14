7b980c6ee059ab735cb8c093f6212fd882307025
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-05-14 07:33:02 -0400

    chore: clean up tests

--- ibis/backends/pyspark/tests/test_udf.py
+++ ibis/backends/pyspark/tests/test_udf.py
@@ -18,22 +18,27 @@ def df(con):
     return con._session.table("basic_table").toPandas()
 
 
-def test_builtin_udf(t, df):
-    @ibis.udf.scalar.builtin
-    def repeat(x, n) -> str: ...
+@ibis.udf.scalar.builtin
+def repeat(x, n) -> str: ...
 
+
+def test_builtin_udf(t, df):
     result = t.mutate(repeated=repeat(t.str_col, 2)).execute()
     expected = df.assign(repeated=df.str_col * 2)
     tm.assert_frame_equal(result, expected)
 
 
 def test_illegal_udf_type(t):
+    @ibis.udf.scalar.pyarrow
+    def my_add_one(x) -> str:
+        import pyarrow.compute as pac
+
+        return pac.add(pac.binary_length(x), 1)
+
+    expr = t.select(repeated=my_add_one(t.str_col))
+
     with pytest.raises(
         NotImplementedError,
-        match="Only Builtin UDFs and Pandas UDFs are support in the PySpark backend",
+        match="Only Builtin UDFs and Pandas UDFs are supported in the PySpark backend",
     ):
-
-        @ibis.udf.scalar.python()
-        def repeat(x, n) -> str: ...
-
-        t.mutate(repeated=repeat(t.str_col, 2)).execute()
+        expr.execute()
