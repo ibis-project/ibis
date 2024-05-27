bea86737be2e968e913d268aec63a9b15cec6caf
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-05-27 10:13:41 -0400

    docs: add array docstrings

--- ibis/expr/operations/arrays.py
+++ ibis/expr/operations/arrays.py
@@ -14,6 +14,8 @@ from ibis.expr.operations.core import Unary, Value
 
 @public
 class Array(Value):
+    """Construct an array."""
+
     exprs: VarTuple[Value]
 
     @attribute
@@ -27,6 +29,8 @@ class Array(Value):
 
 @public
 class ArrayLength(Unary):
+    """Compute the length of an array."""
+
     arg: Value[dt.Array]
 
     dtype = dt.int64
@@ -35,6 +39,8 @@ class ArrayLength(Unary):
 
 @public
 class ArraySlice(Value):
+    """Slice an array element."""
+
     arg: Value[dt.Array]
     start: Value[dt.Integer]
     stop: Optional[Value[dt.Integer]] = None
@@ -45,6 +51,8 @@ class ArraySlice(Value):
 
 @public
 class ArrayIndex(Value):
+    """Return the element of an array at some index."""
+
     arg: Value[dt.Array]
     index: Value[dt.Integer]
 
@@ -57,6 +65,8 @@ class ArrayIndex(Value):
 
 @public
 class ArrayConcat(Value):
+    """Concatenate two or more arrays into a single array."""
+
     arg: VarTuple[Value[dt.Array]]
 
     @attribute
@@ -70,6 +80,8 @@ class ArrayConcat(Value):
 
 @public
 class ArrayRepeat(Value):
+    """Repeat the elements of an array."""
+
     arg: Value[dt.Array]
     times: Value[dt.Integer]
 
@@ -79,6 +91,8 @@ class ArrayRepeat(Value):
 
 @public
 class ArrayMap(Value):
+    """Apply a function to every element of an array."""
+
     arg: Value[dt.Array]
     body: Value
     param: str
@@ -92,6 +106,8 @@ class ArrayMap(Value):
 
 @public
 class ArrayFilter(Value):
+    """Filter array elements with a function."""
+
     arg: Value[dt.Array]
     body: Value[dt.Boolean]
     param: str
@@ -102,6 +118,8 @@ class ArrayFilter(Value):
 
 @public
 class Unnest(Value):
+    """Unnest an array value into a column."""
+
     arg: Value[dt.Array]
 
     shape = ds.columnar
@@ -113,6 +131,8 @@ class Unnest(Value):
 
 @public
 class ArrayContains(Value):
+    """Return whether an array contains a specific value."""
+
     arg: Value[dt.Array]
     other: Value
 
@@ -122,6 +142,8 @@ class ArrayContains(Value):
 
 @public
 class ArrayPosition(Value):
+    """Return the position of a specific value in an array."""
+
     arg: Value[dt.Array]
     other: Value
 
@@ -131,6 +153,8 @@ class ArrayPosition(Value):
 
 @public
 class ArrayRemove(Value):
+    """Remove an element from an array."""
+
     arg: Value[dt.Array]
     other: Value
 
@@ -140,6 +164,8 @@ class ArrayRemove(Value):
 
 @public
 class ArrayDistinct(Value):
+    """Return the unique elements of an array."""
+
     arg: Value[dt.Array]
 
     dtype = rlz.dtype_like("arg")
@@ -148,6 +174,8 @@ class ArrayDistinct(Value):
 
 @public
 class ArraySort(Value):
+    """Sort the values of an array."""
+
     arg: Value[dt.Array]
 
     dtype = rlz.dtype_like("arg")
@@ -156,6 +184,8 @@ class ArraySort(Value):
 
 @public
 class ArrayUnion(Value):
+    """Return the union of two arrays."""
+
     left: Value[dt.Array]
     right: Value[dt.Array]
 
@@ -165,6 +195,8 @@ class ArrayUnion(Value):
 
 @public
 class ArrayIntersect(Value):
+    """Return the intersection of two arrays."""
+
     left: Value[dt.Array]
     right: Value[dt.Array]
 
@@ -174,6 +206,8 @@ class ArrayIntersect(Value):
 
 @public
 class ArrayZip(Value):
+    """Zip two or more arrays into an array of structs."""
+
     arg: VarTuple[Value[dt.Array]]
 
     shape = rlz.shape_like("arg")
@@ -207,6 +241,8 @@ class ArrayFlatten(Value):
 
 
 class Range(Value):
+    """Base class for range-generating operations."""
+
     shape = rlz.shape_like("args")
 
     @attribute
@@ -216,6 +252,8 @@ class Range(Value):
 
 @public
 class IntegerRange(Range):
+    """Produce an array of integers from `start` to `stop`, moving by `step`."""
+
     start: Value[dt.Integer]
     stop: Value[dt.Integer]
     step: Value[dt.Integer]
@@ -223,6 +261,8 @@ class IntegerRange(Range):
 
 @public
 class TimestampRange(Range):
+    """Produce an array of timestamps from `start` to `stop`, moving by `step`."""
+
     start: Value[dt.Timestamp]
     stop: Value[dt.Timestamp]
     step: Value[dt.Interval]
