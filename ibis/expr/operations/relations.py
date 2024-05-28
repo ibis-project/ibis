f2db289cabf92c82f8635158c2fe8b07aaf537e3
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-05-28 09:33:28 -0400

    chore: relations docstrings

--- ibis/expr/operations/relations.py
+++ ibis/expr/operations/relations.py
@@ -34,6 +34,8 @@ NonSortKey = Annotated[T, ~InstanceOf(SortKey)]
 
 @public
 class Relation(Node, Coercible):
+    """Base class for relational operations."""
+
     @classmethod
     def __coerce__(cls, value):
         from ibis.expr.types import Table
@@ -81,6 +83,8 @@ class Relation(Node, Coercible):
 
 @public
 class Field(Value):
+    """A field of a relation."""
+
     rel: Relation
     name: str
 
@@ -115,6 +119,8 @@ def _check_integrity(values, allowed_parents):
 
 @public
 class Project(Relation):
+    """Project a subset of columns from a relation."""
+
     parent: Relation
     values: FrozenOrderedDict[str, NonSortKey[Unaliased[Value]]]
 
@@ -227,6 +233,8 @@ class JoinChain(Relation):
 
 @public
 class Sort(Simple):
+    """Sort a table by a set of keys."""
+
     keys: VarTuple[SortKey]
 
     def __init__(self, parent, keys):
@@ -236,6 +244,8 @@ class Sort(Simple):
 
 @public
 class Filter(Simple):
+    """Filter a table by a set of predicates."""
+
     predicates: VarTuple[Value[dt.Boolean]]
 
     def __init__(self, parent, predicates):
@@ -256,6 +266,8 @@ class Filter(Simple):
 
 @public
 class Limit(Simple):
+    """Limit and/or offset the number of records in a table."""
+
     # TODO(kszucs): dynamic limit should contain ScalarSubqueries rather than
     # plain scalar values
     n: typing.Union[int, Scalar[dt.Integer], None] = None
@@ -264,6 +276,8 @@ class Limit(Simple):
 
 @public
 class Aggregate(Relation):
+    """Aggregate a table by a set of group by columns and metrics."""
+
     parent: Relation
     groups: FrozenOrderedDict[str, Unaliased[Column]]
     metrics: FrozenOrderedDict[str, Unaliased[Scalar]]
@@ -288,6 +302,8 @@ class Aggregate(Relation):
 
 @public
 class Set(Relation):
+    """Base class for set operations."""
+
     left: Relation
     right: Relation
     distinct: bool = False
@@ -322,39 +338,51 @@ class Set(Relation):
 
 @public
 class Union(Set):
-    pass
+    """Union two tables."""
 
 
 @public
 class Intersection(Set):
-    pass
+    """Intersect two tables."""
 
 
 @public
 class Difference(Set):
-    pass
+    """Subtract one table from another."""
 
 
 @public
 class PhysicalTable(Relation):
+    """Base class for tables with a name."""
+
     name: str
     values = FrozenOrderedDict()
 
 
 @public
 class Namespace(Concrete):
+    """Object to model namespaces for tables.
+
+    Maps to the concept of database and/or catalog in SQL databases that support
+    them.
+    """
+
     catalog: Optional[str] = None
     database: Optional[str] = None
 
 
 @public
 class UnboundTable(PhysicalTable):
+    """A table that is not bound to a specific backend."""
+
     schema: Schema
     namespace: Namespace = Namespace()
 
 
 @public
 class DatabaseTable(PhysicalTable):
+    """A table that is bound to a specific backend."""
+
     schema: Schema
     source: Any
     namespace: Namespace = Namespace()
@@ -362,13 +390,15 @@ class DatabaseTable(PhysicalTable):
 
 @public
 class InMemoryTable(PhysicalTable):
+    """A table whose data is stored in memory."""
+
     schema: Schema
     data: TableProxy
 
 
 @public
 class SQLQueryResult(Relation):
-    """A table sourced from the result set of a select query."""
+    """A table sourced from the result set of a SQL SELECT statement."""
 
     query: str
     schema: Schema
@@ -400,6 +430,8 @@ class SQLStringView(Relation):
 
 @public
 class DummyTable(Relation):
+    """A table constructed from literal values."""
+
     values: FrozenOrderedDict[str, Value]
 
     @attribute
@@ -433,7 +465,7 @@ class Sample(Simple):
 
 @public
 class Distinct(Simple):
-    """Distinct is a table-level unique-ing operation."""
+    """Compute the distinct rows of a table."""
 
 
 # TODO(kszucs): support t.select(*t) syntax by implementing Table.__iter__()
