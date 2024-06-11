59d3caa68d5f68ac7ebe41a0e713a36acee4898b
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-06-11 07:49:11 -0600

    refactor(postgres): employ `hash_numeric_extended`

--- ibis/backends/postgres/compiler.py
+++ ibis/backends/postgres/compiler.py
@@ -592,16 +592,8 @@ class PostgresCompiler(SQLGlotCompiler):
     def visit_Hash(self, op, *, arg):
         arg_dtype = op.arg.dtype
 
-        if arg_dtype.is_int16():
-            return self.f.hashint2extended(arg, 0)
-        elif arg_dtype.is_int32():
-            return self.f.hashint4extended(arg, 0)
-        elif arg_dtype.is_int64():
-            return self.f.hashint8extended(arg, 0)
-        elif arg_dtype.is_float32():
-            return self.f.hashfloat4extended(arg, 0)
-        elif arg_dtype.is_float64():
-            return self.f.hashfloat8extended(arg, 0)
+        if arg_dtype.is_numeric():
+            return self.f.hash_numeric_extended(arg, 0)
         elif arg_dtype.is_string():
             return self.f.hashtextextended(arg, 0)
         elif arg_dtype.is_macaddr():
