f1ba157bff01c4243165e0cb5eee943181fa1258
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-06-14 06:56:39 -0400

    test(druid): enable xpassing isin tests

--- ibis/backends/tests/test_generic.py
+++ ibis/backends/tests/test_generic.py
@@ -872,7 +872,6 @@ def test_isin_notin(backend, alltypes, df, ibis_op, pandas_op):
     backend.assert_frame_equal(result, expected)
 
 
-@pytest.mark.notimpl(["druid"])
 @pytest.mark.parametrize(
     ("ibis_op", "pandas_op"),
     [
@@ -880,6 +879,7 @@ def test_isin_notin(backend, alltypes, df, ibis_op, pandas_op):
             _.string_col.isin(_.string_col),
             lambda df: df.string_col.isin(df.string_col),
             id="isin_col",
+            marks=pytest.mark.notimpl(["druid"]),
         ),
         param(
             (_.bigint_col + 1).isin(_.string_col.length() + 1),
@@ -896,7 +896,7 @@ def test_isin_notin(backend, alltypes, df, ibis_op, pandas_op):
             (_.bigint_col + 1).notin(_.string_col.length() + 1),
             lambda df: ~(df.bigint_col.add(1)).isin(df.string_col.str.len().add(1)),
             id="notin_expr",
-            marks=[pytest.mark.notimpl(["datafusion"])],
+            marks=[pytest.mark.notimpl(["datafusion", "druid"])],
         ),
     ],
 )
