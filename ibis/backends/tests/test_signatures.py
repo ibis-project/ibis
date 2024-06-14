815d1238bb80328076d01fd50aa80b17ebf455fb
Gil Forsyth <gil@forsyth.dev>
2024-06-14 16:44:05 -0400

    chore(sigcheck): skip signature checks for do_connect
    We can revisit this, but for now, these can be different enough that I'm
    ok not enforcing strict compatibility.

--- ibis/backends/tests/test_signatures.py
+++ ibis/backends/tests/test_signatures.py
@@ -14,18 +14,31 @@ from ibis.backends import (
 from ibis.backends.sql import SQLBackend
 from ibis.backends.tests.signature.typecheck import compatible
 
-params = []
+SKIP_METHODS = ["do_connect"]
 
-for module in [
-    BaseBackend,
-    CanCreateCatalog,
-    CanCreateDatabase,
-    CanListCatalog,
-    CanListDatabase,
-]:
-    methods = list(filter(lambda x: not x.startswith("_"), dir(module)))
-    for method in methods:
-        params.append((module, method))
+
+def _scrape_methods(modules):
+    params = []
+    for module in modules:
+        methods = list(filter(lambda x: not x.startswith("_"), dir(module)))
+        for method in methods:
+            # Only test methods that are callable (so we can grab the signature)
+            # and skip any methods that we don't want to check
+            if method not in SKIP_METHODS and callable(getattr(module, method)):
+                params.append((module, method))
+
+    return params
+
+
+params = _scrape_methods(
+    [
+        BaseBackend,
+        CanCreateCatalog,
+        CanCreateDatabase,
+        CanListCatalog,
+        CanListDatabase,
+    ]
+)
 
 
 @pytest.mark.parametrize("base_cls, method", params)
@@ -33,24 +46,14 @@ def test_signatures(base_cls, method, backend_cls):
     if not hasattr(backend_cls, method):
         pytest.skip(f"Method {method} not present in {backend_cls}, skipping...")
 
-    if not callable(base_method := getattr(base_cls, method)):
-        pytest.skip(
-            f"Method {method} in {base_cls} isn't callable, can't grab signature"
-        )
-
-    base_sig = inspect.signature(base_method)
+    base_sig = inspect.signature(getattr(base_cls, method))
     backend_sig = inspect.signature(getattr(backend_cls, method))
 
     # Usage is compatible(implementation_signature, defined_interface_signature, ...)
     assert compatible(backend_sig, base_sig, check_annotations=False)
 
 
-sql_backend_params = []
-
-for module in [SQLBackend]:
-    methods = list(filter(lambda x: not x.startswith("_"), dir(module)))
-    for method in methods:
-        sql_backend_params.append((module, method))
+sql_backend_params = _scrape_methods([SQLBackend])
 
 
 @pytest.mark.parametrize("base_cls, method", sql_backend_params)
@@ -58,12 +61,7 @@ def test_signatures_sql_backends(base_cls, method, backend_sql_cls):
     if not hasattr(backend_sql_cls, method):
         pytest.skip(f"Method {method} not present in {backend_sql_cls}, skipping...")
 
-    if not callable(base_method := getattr(base_cls, method)):
-        pytest.skip(
-            f"Method {method} in {base_cls} isn't callable, can't grab signature"
-        )
-
-    base_sig = inspect.signature(base_method)
+    base_sig = inspect.signature(getattr(base_cls, method))
     backend_sig = inspect.signature(getattr(backend_sql_cls, method))
 
     # Usage is compatible(implementation_signature, defined_interface_signature, ...)
