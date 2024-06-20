6a2a9afd660772b19528b40619e083a2a837351c
ncclementi <natyclementi@gmail.com>
2024-06-20 11:29:57 -0400

    chore: preserve nulls last when no desc or asc

--- ibis/expr/operations/sortkeys.py
+++ ibis/expr/operations/sortkeys.py
@@ -19,7 +19,7 @@ class SortKey(Value):
     # TODO(kszucs): rename expr to arg or something else except expr
     expr: Value
     ascending: bool = True
-    nulls_first: bool = True
+    nulls_first: bool = False
 
     dtype = rlz.dtype_like("expr")
     shape = rlz.shape_like("expr")
