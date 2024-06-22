f99f3ee02c4d63da5100d466ee60e890fc4fe9b1
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-06-22 11:05:37 -0400

    chore(sql): regen snapshots for new sqlglot version

--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_literal_year/timestamp/out.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_literal_year/timestamp/out.sql
@@ -1,2 +1,2 @@
 SELECT
-  EXTRACT(year FROM datetime('2017-01-01T04:55:59')) AS `ExtractYear_datetime_datetime_2017_1_1_4_55_59`
+  EXTRACT(year FROM DATETIME('2017-01-01T04:55:59')) AS `ExtractYear_datetime_datetime_2017_1_1_4_55_59`
