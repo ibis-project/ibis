ce48314caeeae78c01bf1d130cd3aff4dd49d274
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-01 11:28:47 -0400

    chore: patch scipy 1.14 for new usage of xcrun

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,1 +1,10 @@
-_final: _prev: { }
+final: prev: {
+  scipy = prev.scipy.overridePythonAttrs (
+    old: final.lib.optionalAttrs (final.stdenv.isDarwin && (final.lib.versionAtLeast old.version "1.14.0")) {
+      prePatch = (old.prePatch or "") + ''
+        substituteInPlace scipy/meson.build \
+          --replace xcrun "${final.pkgs.xcbuild}/bin/xcrun"
+      '';
+    }
+  );
+}
