ca0df272d41ad69eb3bb4dd4f2ee21e43d5ed0ed
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-04 15:35:47 -0400

    chore: avoid rendering srid for bigquery

--- ibis/backends/sql/datatypes.py
+++ ibis/backends/sql/datatypes.py
@@ -879,10 +879,7 @@ class BigQueryType(SqlglotType):
     @classmethod
     def _from_ibis_GeoSpatial(cls, dtype: dt.GeoSpatial) -> sge.DataType:
         if (dtype.geotype, dtype.srid) == ("geography", 4326):
-            return sge.DataType(
-                this=sge.DataType.Type.GEOGRAPHY,
-                expressions=[None, sge.DataTypeParam(this=sge.convert(dtype.srid))],
-            )
+            return sge.DataType(this=sge.DataType.Type.GEOGRAPHY)
         else:
             raise com.UnsupportedBackendType(
                 "BigQuery geography uses points on WGS84 reference ellipsoid."
