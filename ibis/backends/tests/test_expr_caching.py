6d0971b73314df22972df194b4168ae817dfc652
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-17 12:23:37 -0400

    test: add refcount test for exposition

--- ibis/backends/tests/test_expr_caching.py
+++ ibis/backends/tests/test_expr_caching.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import sys
+
 import pytest
 from pytest import mark
 
@@ -71,7 +73,9 @@ def test_persist_expression_multiple_refs(backend, con, alltypes):
     op = non_cached_table.op()
     cached_table = non_cached_table.cache()
 
-    backend.assert_frame_equal(non_cached_table.to_pandas(), cached_table.to_pandas())
+    cached_df = cached_table.to_pandas()
+    backend.assert_frame_equal(non_cached_table.to_pandas(), cached_df)
+    assert sys.getrefcount(op) == 2
 
     name = cached_table.op().name
     nested_cached_table = non_cached_table.cache()
