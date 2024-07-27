8d5dedbcae9eee9955b35d31f271d02bcf758bb6
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-27 08:57:52 -0400

    feat(bigquery): implement array aggregations

--- ibis/backends/sql/compilers/bigquery.py
+++ ibis/backends/sql/compilers/bigquery.py
@@ -774,3 +774,29 @@ class BigQueryCompiler(SQLGlotCompiler):
             origin = self.f.anon[f"{funcname}_add"](origin, offset)
 
         return func(arg, interval, origin)
+
+    def visit_ArrayReduction(self, op, *, arg, reduction):
+        name = sg.to_identifier(util.gen_name(f"bq_arr_{reduction}"))
+        return (
+            sg.select(self.f[reduction](name))
+            .from_(self._unnest(arg, as_=name))
+            .subquery()
+        )
+
+    def visit_ArrayMin(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="min")
+
+    def visit_ArrayMax(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="max")
+
+    def visit_ArraySum(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="sum")
+
+    def visit_ArrayMean(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="avg")
+
+    def visit_ArrayAny(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="logical_or")
+
+    def visit_ArrayAll(self, op, *, arg):
+        return self.visit_ArrayReduction(arg, reduction="logical_and")
