0396fb2310a6ad3ca770530a989b60eb82f21f4f
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-08-07 11:54:22 -0400

    test: add test for qualify syntax in backends that support it

---
+++ ibis/backends/tests/snapshots/test_sql/test_mixed_qualified_and_unqualified_predicates/mssql/out.sql
@@ -0,0 +1,19 @@
+SELECT
+  [x],
+  [y]
+FROM (
+  SELECT
+    [t1].[x] AS [x],
+    [t1].[y] AS [y],
+    AVG([t1].[x]) OVER (ORDER BY CASE WHEN [t1].[x] IS NULL THEN 1 ELSE 0 END, [t1].[x] ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS _w
+  FROM (
+    SELECT
+      [t0].[x],
+      SUM([t0].[x]) OVER (ORDER BY CASE WHEN [t0].[x] IS NULL THEN 1 ELSE 0 END, [t0].[x] ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS [y]
+    FROM [t] AS [t0]
+  ) AS [t1]
+  WHERE
+    [t1].[y] <= 37
+) AS _t
+WHERE
+  _w IS NOT NULL
