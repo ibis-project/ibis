94856e0dc8c7f30b843de4eda4b3a5b24ef0ba98
Jim Crist-Harif <jcristharif@gmail.com>
2024-08-13 15:22:06 -0500

    refactor(impala): remove `ImpalaTable` subclass
    BREAKING CHANGE: The `ImpalaTable` subclass has been removed. Relevant
    Impala-specific methods that were on the `ImpalaTable` class have been
    moved to the `Backend` class, please use those instead (e.g.
    `con.add_partition` instead of `table.add_partition`).

--- ibis/backends/impala/tests/test_patched.py
+++ ibis/backends/impala/tests/test_patched.py
@@ -20,10 +20,6 @@ def test_invalidate_metadata(con, spy, test_data_db, qname):
     con.invalidate_metadata()
     spy.assert_called_with("INVALIDATE METADATA")
 
-    con.invalidate_metadata("functional_alltypes")
-    t = con.table("functional_alltypes")
-    t.invalidate_metadata()
-
     con.invalidate_metadata("functional_alltypes", database=test_data_db)
     spy.assert_called_with(f"INVALIDATE METADATA {qname}")
 
@@ -33,32 +29,34 @@ def test_refresh(con, spy, qname):
     con.refresh(tname)
     spy.assert_called_with(f"REFRESH {qname}")
 
-    t = con.table(tname)
-    t.refresh()
-    spy.assert_called_with(f"REFRESH {qname}")
-
 
 def test_describe_formatted(con, spy, qname):
-    t = con.table("functional_alltypes")
-    desc = t.describe_formatted()
+    desc = con.describe_formatted("functional_alltypes")
     spy.assert_called_with(f"DESCRIBE FORMATTED {qname}")
     assert isinstance(desc, metadata.TableMetadata)
 
 
 def test_show_files(con, spy, qname):
-    t = con.table("functional_alltypes")
-    desc = t.files()
+    desc = con.show_files("functional_alltypes")
     spy.assert_called_with(f"SHOW FILES IN {qname}")
     assert isinstance(desc, pd.DataFrame)
 
 
-def test_table_column_stats(con, spy, qname):
-    t = con.table("functional_alltypes")
+def test_column_stats(con, spy, qname):
+    desc = con.column_stats("functional_alltypes")
+    spy.assert_called_with(f"SHOW COLUMN STATS {qname}")
+    assert isinstance(desc, pd.DataFrame)
 
-    desc = t.stats()
+
+def test_table_stats(con, spy, qname):
+    desc = con.table_stats("functional_alltypes")
     spy.assert_called_with(f"SHOW TABLE STATS {qname}")
     assert isinstance(desc, pd.DataFrame)
 
-    desc = t.column_stats()
-    spy.assert_called_with(f"SHOW COLUMN STATS {qname}")
-    assert isinstance(desc, pd.DataFrame)
+
+def test_compute_stats(con, spy, qname):
+    con.compute_stats("functional_alltypes")
+    spy.assert_called_with(f"COMPUTE STATS {qname}")
+
+    con.compute_stats("functional_alltypes", incremental=True)
+    spy.assert_called_with(f"COMPUTE INCREMENTAL STATS {qname}")
