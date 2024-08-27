66bd98ce7c5bf0c64e02ae945c834a88ed5dc465
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-08-27 06:49:26 -0400

    chore(tpcds): use `date_add` to allow correct transpilation

--- ibis/backends/tests/tpc/queries/duckdb/ds/72.sql
+++ ibis/backends/tests/tpc/queries/duckdb/ds/72.sql
@@ -24,7 +24,7 @@ LEFT OUTER JOIN catalog_returns ON (cr_item_sk = cs_item_sk
                                     AND cr_order_number = cs_order_number)
 WHERE d1.d_week_seq = d2.d_week_seq
   AND inv_quantity_on_hand < cs_quantity
-  AND d3.d_date > d1.d_date + 5 -- SQL Server: DATEADD(day, 5, d1.d_date)
+  AND d3.d_date > date_add(d1.d_date, 5)
   AND hd_buy_potential = '>10000'
   AND d1.d_year = 1999
   AND cd_marital_status = 'D'
