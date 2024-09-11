2b42d0bf72b1fd6bf67a1c4cb05f94c0ab44ebea
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-09-11 08:08:08 -0400

    chore: add exasol doctest

--- ibis/backends/exasol/__init__.py
+++ ibis/backends/exasol/__init__.py
@@ -81,6 +81,33 @@ class Backend(SQLBackend, CanCreateDatabase, CanCreateSchema):
         kwargs
             Additional keyword arguments passed to `pyexasol.connect`.
 
+        Examples
+        --------
+        >>> import os
+        >>> import ibis
+        >>> host = os.environ.get("IBIS_TEST_EXASOL_HOST", "localhost")
+        >>> user = os.environ.get("IBIS_TEST_EXASOL_USER", "sys")
+        >>> password = os.environ.get("IBIS_TEST_EXASOL_PASSWORD", "exasol")
+        >>> schema = os.environ.get("IBIS_TEST_EXASOL_DATABASE", "EXASOL")
+        >>> con = ibis.exasol.connect(schema=schema, host=host, user=user, password=password)
+        >>> con.list_tables()  # doctest: +ELLIPSIS
+        [...]
+        >>> t = con.table("functional_alltypes")
+        >>> t
+        DatabaseTable: functional_alltypes
+          id : int32
+          bool_col : boolean
+          tinyint_col : int16
+          smallint_col : int16
+          int_col : int32
+          bigint_col : int64
+          float_col : float32
+          double_col : float64
+          date_string_col : string
+          string_col : string
+          timestamp_col : timestamp
+          year : int32
+          month : int32
         """
         if kwargs.pop("quote_ident", None) is not None:
             raise com.UnsupportedArgumentError(
