a3fce3bc45b93ba8e2c790808f491e11e565280b
Guilherme Martins Crocetti <24530683+gmcrocetti@users.noreply.github.com>
2024-09-10 22:43:58 -0300

    fix: pandas connect doctests

--- ibis/backends/pandas/__init__.py
+++ ibis/backends/pandas/__init__.py
@@ -48,8 +48,8 @@ class BasePandasBackend(BaseBackend, NoUrl):
         Examples
         --------
         >>> import ibis
-        >>> ibis.pandas.connect({"t": pd.DataFrame({"a": [1, 2, 3]})})
-        <ibis.backends.pandas.Backend at 0x...>
+        >>> ibis.pandas.connect({"t": pd.DataFrame({"a": [1, 2, 3]})}) # doctest: +ELLIPSIS
+        <ibis.backends.pandas.Backend object at 0x...>
 
         """
         warnings.warn(
