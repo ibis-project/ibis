d01a36a3e78f86eb533b9af7d3971f509e55aedc
Gil Forsyth <gil@forsyth.dev>
2024-09-11 14:26:11 -0400

    Revert "fix(datafusion): raise when attempting to create temp table (#10072)"
    This reverts commit 1cf54399c94849cf27782b2446efe3c2e31e2467.

--- ibis/backends/tests/test_client.py
+++ ibis/backends/tests/test_client.py
@@ -119,14 +119,7 @@ def test_create_table(backend, con, temp_table, func, sch):
             marks=[
                 pytest.mark.notyet(["clickhouse"], reason="Can't specify both"),
                 pytest.mark.notyet(
-                    [
-                        "pyspark",
-                        "trino",
-                        "exasol",
-                        "risingwave",
-                        "impala",
-                        "datafusion",
-                    ],
+                    ["pyspark", "trino", "exasol", "risingwave", "impala"],
                     reason="No support for temp tables",
                 ),
                 pytest.mark.notyet(
@@ -152,14 +145,7 @@ def test_create_table(backend, con, temp_table, func, sch):
             id="temp, no overwrite",
             marks=[
                 pytest.mark.notyet(
-                    [
-                        "pyspark",
-                        "trino",
-                        "exasol",
-                        "risingwave",
-                        "impala",
-                        "datafusion",
-                    ],
+                    ["pyspark", "trino", "exasol", "risingwave", "impala"],
                     reason="No support for temp tables",
                 ),
                 pytest.mark.notimpl(["mssql"], reason="Incorrect temp table syntax"),
@@ -321,9 +307,6 @@ def test_create_table_from_schema(con, new_schema, temp_table):
     ["flink"],
     raises=com.IbisError,
     reason="`tbl_properties` is required when creating table with schema",
-)
-@pytest.mark.notimpl(
-    ["datafusion"], raises=NotImplementedError, reason="no temp table support"
 )
 def test_create_temporary_table_from_schema(con_no_data, new_schema):
     if con_no_data.name == "snowflake" and os.environ.get("SNOWFLAKE_SNOWPARK"):
@@ -1579,9 +1562,6 @@ def test_json_to_pyarrow(con):
     assert result == expected
 
 
-@pytest.mark.notimpl(
-    ["datafusion"], raises=NotImplementedError, reason="no temp table support"
-)
 @pytest.mark.notyet(
     ["risingwave", "exasol"],
     raises=com.UnsupportedOperationError,
