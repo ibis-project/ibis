8e1c12d9a890040bdf44a3b7758fb343ac7d0ca8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-09-11 09:42:18 -0400

    mysql

--- ibis/backends/mysql/__init__.py
+++ ibis/backends/mysql/__init__.py
@@ -134,19 +134,19 @@ class Backend(SQLBackend, CanCreateDatabase):
         >>> t = con.table("functional_alltypes")
         >>> t
         DatabaseTable: functional_alltypes
-          id : int32
-          bool_col : int8
-          tinyint_col : int8
-          smallint_col : int16
-          int_col : int32
-          bigint_col : int64
-          float_col : float32
-          double_col : float64
-          date_string_col : string
-          string_col : string
-          timestamp_col : timestamp
-          year : int32
-          month : int32
+          id              int32
+          bool_col        int8
+          tinyint_col     int8
+          smallint_col    int16
+          int_col         int32
+          bigint_col      int64
+          float_col       float32
+          double_col      float64
+          date_string_col string
+          string_col      string
+          timestamp_col   timestamp
+          year            int32
+          month           int32
         """
         self.con = pymysql.connect(
             user=user,
