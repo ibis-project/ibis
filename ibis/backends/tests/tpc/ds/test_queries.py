1a4f18cc6ba00b95925cf8f9658d9f1334279dc8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-14 07:54:48 -0400

    test(datafusion): xfail on broken tpc q39

--- ibis/backends/tests/tpc/ds/test_queries.py
+++ ibis/backends/tests/tpc/ds/test_queries.py
@@ -1994,6 +1994,7 @@ def test_38(store_sales, catalog_sales, web_sales, date_dim, customer):
 
 
 @tpc_test("ds")
+@pytest.mark.notyet(["datafusion"], reason="incorrect results", raises=AssertionError)
 def test_39(inventory, item, warehouse, date_dim):
     inv = (
         inventory.join(item, [("inv_item_sk", "i_item_sk")])
