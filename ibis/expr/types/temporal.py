388e110b2d4df3a0ddf1045fbb873a99e1ea287b
Tyler White <50381805+IndexSeek@users.noreply.github.com>
2024-10-24 12:01:38 +0000

    chore: linting strftime doc examples

--- ibis/expr/types/temporal.py
+++ ibis/expr/types/temporal.py
@@ -346,11 +346,13 @@ class DateValue(Value, _DateComponentMixin):
         >>> import ibis
         >>> ibis.options.interactive = True
         >>> t = ibis.memtable(
-        ...     {"date_col": [
-        ...         date(2020, 10, 5),
-        ...         date(2020, 11, 10),
-        ...         date(2020, 12, 15),
-        ...     ]},
+        ...     {
+        ...         "date_col": [
+        ...             date(2020, 10, 5),
+        ...             date(2020, 11, 10),
+        ...             date(2020, 12, 15),
+        ...         ]
+        ...     },
         ... )
 
         Return a string with the year and month.
@@ -584,11 +586,13 @@ class TimestampValue(_DateComponentMixin, _TimeComponentMixin, Value):
         >>> import ibis
         >>> ibis.options.interactive = True
         >>> t = ibis.memtable(
-        ...     {"timestamp_col": [
-        ...         (datetime(2020, 10, 5, 8, 0, 0)),
-        ...         (datetime(2020, 11, 10, 10, 2, 15)),
-        ...         (datetime(2020, 12, 15, 12, 4, 30)),
-        ...     ]},
+        ...     {
+        ...         "timestamp_col": [
+        ...             (datetime(2020, 10, 5, 8, 0, 0)),
+        ...             (datetime(2020, 11, 10, 10, 2, 15)),
+        ...             (datetime(2020, 12, 15, 12, 4, 30)),
+        ...         ]
+        ...     },
         ... )
 
         Return a string with the year and month.
