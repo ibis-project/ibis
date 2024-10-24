627e7606415f9f09931d6d6f684dc0c3f8866c4d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-24 08:17:01 -0400

    chore: remove unnecessary parentheses

--- ibis/expr/types/temporal.py
+++ ibis/expr/types/temporal.py
@@ -588,9 +588,9 @@ class TimestampValue(_DateComponentMixin, _TimeComponentMixin, Value):
         >>> t = ibis.memtable(
         ...     {
         ...         "timestamp_col": [
-        ...             (datetime(2020, 10, 5, 8, 0, 0)),
-        ...             (datetime(2020, 11, 10, 10, 2, 15)),
-        ...             (datetime(2020, 12, 15, 12, 4, 30)),
+        ...             datetime(2020, 10, 5, 8, 0, 0),
+        ...             datetime(2020, 11, 10, 10, 2, 15),
+        ...             datetime(2020, 12, 15, 12, 4, 30),
         ...         ]
         ...     },
         ... )
