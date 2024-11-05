f6b4ca49922bb7feeb98562ca8dfc0e49d3d9f87
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-11-05 09:13:23 -0500

    fix(datafusion): handle array concat like everything else

--- ibis/backends/sql/compilers/datafusion.py
+++ ibis/backends/sql/compilers/datafusion.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import calendar
 import math
-from functools import partial
+from functools import partial, reduce
 from itertools import starmap
 
 import sqlglot as sg
@@ -532,6 +532,14 @@ class DataFusionCompiler(SQLGlotCompiler):
 
     def visit_RandomUUID(self, op):
         return self.f.anon.uuid()
+
+    def visit_ArrayConcat(self, op, *, arg):
+        return reduce(
+            lambda x, y: self.if_(
+                x.is_(NULL).or_(y.is_(NULL)), NULL, self.f.array_cat(x, y)
+            ),
+            map(partial(self.cast, to=op.dtype), arg),
+        )
 
 
 compiler = DataFusionCompiler()
