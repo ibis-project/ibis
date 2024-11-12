91fbcaabeb494879d04f46e7eef99e498246977f
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-11-12 08:23:49 -0500

    fix(geometry): make output handling robust to text or bytes

--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_geospatial_binary/difference/out.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_geospatial_binary/difference/out.sql
@@ -1,8 +1,3 @@
 SELECT
-  *
-  REPLACE (st_asbinary(`tmp`) AS `tmp`)
-FROM (
-  SELECT
-    st_difference(`t0`.`geog0`, `t0`.`geog1`) AS `tmp`
-  FROM `t` AS `t0`
-)
+  st_difference(`t0`.`geog0`, `t0`.`geog1`) AS `tmp`
+FROM `t` AS `t0`
--- ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_geospatial_simplify/out.sql
+++ ibis/backends/bigquery/tests/unit/snapshots/test_compiler/test_geospatial_simplify/out.sql
@@ -1,8 +1,3 @@
 SELECT
-  *
-  REPLACE (st_asbinary(`tmp`) AS `tmp`)
-FROM (
-  SELECT
-    st_simplify(`t0`.`geog`, 5.2) AS `tmp`
-  FROM `t` AS `t0`
-)
+  st_simplify(`t0`.`geog`, 5.2) AS `tmp`
+FROM `t` AS `t0`
