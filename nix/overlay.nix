ddeecce2821dc477e03bf426aa47467b6c557698
adisbladis <adisbladis@gmail.com>
2024-11-12 11:21:08 +0000

    chore(deps): implement Nix testing as separate derivation from env (#10473)
    Co-authored-by: Phillip Cloud <417981+cpcloud@users.noreply.github.com>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -7,19 +7,43 @@ let
     sourcePreference = "wheel";
   };
 
+  # Create an overlay enabling editable mode for all local dependencies.
+  # This is for usage with `nix develop`
   editableOverlay =
-    # Create an overlay enabling editable mode for all local dependencies.
-    # This is for usage with nix-nix develop
     workspace.mkEditablePyprojectOverlay {
       root = "$REPO_ROOT";
     };
 
+  # Build fixups overlay
   pyprojectOverrides = import ./pyproject-overrides.nix { inherit pkgs; };
 
-  mkEnv = python: { deps ? workspace.deps.all, editable ? true }:
-    # This devShell uses uv2nix to construct a virtual environment purely from Nix, using the same dependency specification as the application.
-    #
-    # This means that any changes done to your local files do not require a rebuild.
+  # Adds tests to ibis-framework.passthru.tests
+  testOverlay = import ./tests.nix {
+    inherit pkgs;
+    deps = defaultDeps;
+  };
+
+  # Default dependencies for env
+  defaultDeps = {
+    ibis-framework = [
+      "duckdb"
+      "datafusion"
+      "sqlite"
+      "polars"
+      "decompiler"
+      "visualization"
+    ];
+  };
+
+  mkEnv' =
+    {
+      # Python dependency specification
+      deps
+    , # Installs ibis-framework as an editable package for use with `nix develop`.
+      # This means that any changes done to your local files do not require a rebuild.
+      editable
+    ,
+    }: python:
     let
       # Construct package set
       pythonSet =
@@ -30,10 +54,29 @@ let
           (lib.composeManyExtensions ([
             envOverlay
             pyprojectOverrides
-          ] ++ lib.optional editable editableOverlay));
+          ]
+          ++ lib.optionals editable [ editableOverlay ]
+          ++ lib.optionals (!editable) [ testOverlay ]));
     in
     # Build virtual environment
-    pythonSet.mkVirtualEnv "ibis-${python.pythonVersion}" deps;
+    (pythonSet.mkVirtualEnv "ibis-${python.pythonVersion}" deps).overrideAttrs (_old: {
+      # Add passthru.tests from ibis-framework to venv passthru.
+      # This is used to build tests by CI.
+      passthru = {
+        inherit (pythonSet.ibis-framework.passthru) tests;
+      };
+    });
+
+  mkEnv = mkEnv' {
+    deps = defaultDeps;
+    editable = false;
+  };
+
+  mkDevEnv = mkEnv' {
+    # Enable all dependencies for development shell
+    deps = workspace.deps.all;
+    editable = true;
+  };
 
   inherit (pkgs) lib stdenv;
 in
@@ -46,30 +89,22 @@ in
     sha256 = "sha256-1fenQNQB+Q0pbb0cbK2S/UIwZDE4PXXG15MH3aVbyLU=";
   };
 
-  ibis310 = pkgs.callPackage ./ibis.nix {
-    python = pkgs.python310;
-    inherit mkEnv;
-  };
+  ibis310 = mkEnv pkgs.python310;
+  ibis311 = mkEnv pkgs.python311;
+  ibis312 = mkEnv pkgs.python312;
 
-  ibis311 = pkgs.callPackage ./ibis.nix {
-    python = pkgs.python311;
-    inherit mkEnv;
-  };
+  ibisDevEnv310 = mkDevEnv pkgs.python310;
+  ibisDevEnv311 = mkDevEnv pkgs.python311;
+  ibisDevEnv312 = mkDevEnv pkgs.python312;
 
-  ibis312 = pkgs.callPackage ./ibis.nix {
-    python = pkgs.python312;
-    inherit mkEnv;
-  };
-
-  ibisDevEnv310 = mkEnv pkgs.python310 { };
-  ibisDevEnv311 = mkEnv pkgs.python311 { };
-  ibisDevEnv312 = mkEnv pkgs.python312 { };
-
-  ibisSmallDevEnv = mkEnv pkgs.python312 {
-    deps = {
-      ibis-framework = [ "dev" ];
-    };
-  };
+  ibisSmallDevEnv = mkEnv'
+    {
+      deps = {
+        ibis-framework = [ "dev" ];
+      };
+      editable = false;
+    }
+    pkgs.python312;
 
   duckdb = super.duckdb.overrideAttrs (
     _: lib.optionalAttrs (stdenv.isAarch64 && stdenv.isLinux) {
