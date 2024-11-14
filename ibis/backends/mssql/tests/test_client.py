f241371e6cdd80b2c7e970bc87247ae89ed75f4c
Grieve <grieve54706@gmail.com>
2024-11-14 16:19:43 +0800

    fix(mssql): use nvarchar to avoid non ascii be question mark

--- ibis/backends/mssql/tests/test_client.py
+++ ibis/backends/mssql/tests/test_client.py
@@ -311,3 +311,11 @@ def test_escape_special_characters():
     assert test_func("1bis}Testing!") == "{1bis}}Testing!}"
     assert test_func("{R;3G1/8Al2AniRye") == "{{R;3G1/8Al2AniRye}"
     assert test_func("{R;3G1/8Al2AniRye}") == "{{R;3G1/8Al2AniRye}}}"
+
+
+def test_non_ascii_column_name(con):
+    expr = con.sql("SELECT 1 AS [калона]")
+    schema = expr.schema()
+    names = schema.names
+    assert len(names) == 1
+    assert names[0] == "калона"
