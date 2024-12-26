976e52017f175534ed258a2475f456c98c946661
Venkat Allam <venkat.allam@dell.com>
2024-12-26 03:10:00 -0600

    merge upstream changes

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+source /opt/conda/etc/profile.d/conda.sh
+
 # install ibis
-python3 -m pip install ipython
+python3 -m pip install ipyflow ipython
 
 # avoid using dynamic versioning by grabbing the version from pyproject.toml
 POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
-  python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial]'
+  python3 -m pip install -e '.[clickhouse,datafusion,duckdb,examples]'
