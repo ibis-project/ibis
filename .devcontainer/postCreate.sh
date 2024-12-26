40a4937c2550390c30d05c5a7eb093ecd62b740e
Venkat Allam <venkat.allam@dell.com>
2024-12-26 03:18:58 -0600

    revert changes to .devcontainer config

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,8 +3,8 @@
 source /opt/conda/etc/profile.d/conda.sh
 
 # install ibis
-python3 -m pip install ipyflow ipython
+python3 -m pip install ipython
 
 # avoid using dynamic versioning by grabbing the version from pyproject.toml
 POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
-  python3 -m pip install -e '.[clickhouse,datafusion,duckdb,examples]'
+  python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial,datafusion]'
