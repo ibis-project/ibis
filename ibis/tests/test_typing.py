2827077304cfd9c339f2824ecdf2d69cfdded44a
Kevin Mills <35641675+millsks@users.noreply.github.com>
2025-01-10 15:07:26 -0600

    Update test_typing.py

--- ibis/tests/test_typing.py
+++ ibis/tests/test_typing.py
@@ -4,14 +4,18 @@ from typing import Any
 
 from ibis.common.typing import get_type_hints
 
+
 class TestClass:
     a: int
+
     @property
     def b(self) -> str:
         return "test"
+
     @property
     def c(self) -> Any:
         raise AttributeError("Property 'c' raises AttributeError")
+
 
 def test_get_type_hints_with_attribute_error():
     hints = get_type_hints(TestClass, include_properties=True)
