86b2b1d91079129e7e18950aada365b940340e11
Nick Crews <nicholas.b.crews@gmail.com>
2025-02-19 19:27:12 -0900

    feat: upcast schemas if needed during set ops

---
+++ ibis/backends/tests/sql/snapshots/test_sql/test_union_unified_schemas/distinct/out.sql
@@ -0,0 +1,27 @@
+SELECT
+  *
+FROM (
+  SELECT
+    *
+  FROM (
+    SELECT
+      "t0"."id",
+      CAST("t0"."tinyint_col" AS BIGINT) AS "i",
+      CAST(CAST("t0"."string_col" AS TEXT) AS TEXT) AS "s"
+    FROM "functional_alltypes" AS "t0"
+  ) AS "t2"
+  UNION
+  SELECT
+    *
+  FROM (
+    SELECT
+      "t0"."id",
+      "t0"."bigint_col" + 256 AS "i",
+      "t0"."string_col" AS "s"
+    FROM "functional_alltypes" AS "t0"
+  ) AS "t1"
+) AS "t3"
+ORDER BY
+  "t3"."id" ASC,
+  "t3"."i" ASC,
+  "t3"."s" ASC
