cf551be7fccf874e4bce4a9c85f8e18082931bd1
Nick Crews <nicholas.b.crews@gmail.com>
2025-03-13 14:13:55 -0800

    test(perf): add example of non-performant struct field compilation

---
+++ ibis/backends/tests/sql/snapshots/test_sql/test_struct_field_simplified/getitem/out.sql
@@ -0,0 +1,128 @@
+SELECT
+  (
+    {'i': (
+      {'i': (
+        {'i': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."i" + 1, 's': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."s" || 'bar'}
+      )."i" + 1, 's': (
+        {'i': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."i" + 1, 's': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."s" || 'bar'}
+      )."s" || 'bar'}
+    )."i" + 1, 's': (
+      {'i': (
+        {'i': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."i" + 1, 's': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."s" || 'bar'}
+      )."i" + 1, 's': (
+        {'i': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."i" + 1, 's': (
+          {'i': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."i" + 1, 's': (
+            {'i': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."i" + 1, 's': (
+              CAST(ROW(123, 'foo') AS STRUCT("i" TINYINT, "s" TEXT))
+            )."s" || 'bar'}
+          )."s" || 'bar'}
+        )."s" || 'bar'}
+      )."s" || 'bar'}
+    )."s" || 'bar'}
+  )."i" AS "i"
