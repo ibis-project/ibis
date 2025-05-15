2c11db7c92868bed9c8da4acdeaa7fe32ca1f128
Daniel Mesejo <mesejoleon@gmail.com>
2025-05-15 11:54:52 +0200

    feat(datafusion): add BitwiseNot, Clip and IntervalFromInteger

--- ibis/backends/sql/compilers/datafusion.py
+++ ibis/backends/sql/compilers/datafusion.py
@@ -33,11 +33,8 @@ class DataFusionCompiler(SQLGlotCompiler):
         ops.ArrayFilter,
         ops.ArrayMap,
         ops.ArrayZip,
-        ops.BitwiseNot,
-        ops.Clip,
         ops.CountDistinctStar,
         ops.DateDelta,
-        ops.IntervalFromInteger,
         ops.RowID,
         ops.Strftime,
         ops.TimeDelta,
@@ -599,11 +596,13 @@ class DataFusionCompiler(SQLGlotCompiler):
             ),
         )
 
-    def visit_Greatest(self, op, *, arg):
-        return self.f.greatest(*arg)
+    def visit_BitwiseNot(self, op, *, arg):
+        # https://stackoverflow.com/q/69648488/4001592
+        return sge.BitwiseXor(this=arg, expression=sge.Literal.number(-1))
 
-    def visit_Least(self, op, *, arg):
-        return self.f.least(*arg)
+    def visit_IntervalFromInteger(self, op, *, arg, unit):
+        unit = unit.name.lower()
+        return sg.cast(self.f.concat(self.cast(arg, dt.string), f" {unit}"), "interval")
 
 
 compiler = DataFusionCompiler()
