d7052a5cd74f377d4d678c1eb30fb4af8486ed3c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-07-16 06:28:16 -0400

    chore(deps): bump quarto to 1.8.17

--- nix/quarto/default.nix
+++ nix/quarto/default.nix
@@ -20,9 +20,9 @@ let
     x86_64-darwin = aarch64-darwin;
   };
   shas = rec {
-    x86_64-linux = "sha256-vnrjbv5N/J+jtyhJrOAnMeUfnFeLMj+ivDEsM9AmiB8=";
-    aarch64-linux = "sha256-LpU3ZdOBnCZevR6bZn8UttCRjmwbfS/ryrneA8mdUDg=";
-    aarch64-darwin = "sha256-SYc0fq9x9HJeT84osQb0lXE9AClw7YfnwAgh53KWooI=";
+    x86_64-linux = "sha256-daXUWf7yRnzPMxgvparvox7aqqv+xTneEMSsL+oZTyM=";
+    aarch64-linux = "sha256-cuyEMv40+99yNGjhjAASvWweaqhdmnaq9ycCIkg52Hw=";
+    aarch64-darwin = "sha256-DPIBZDV0pkfpvKty+YjrGegf3p92Xk4huh66VVK2cws=";
     # hashes are the same for both macos architectures, because the packages
     # are identical
     x86_64-darwin = aarch64-darwin;
@@ -31,7 +31,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "quarto";
-  version = "1.8.16";
+  version = "1.8.17";
   src = fetchurl {
     url = "https://github.com/quarto-dev/quarto-cli/releases/download/v${version}/quarto-${version}-${platforms.${system}}.tar.gz";
     sha256 = shas.${system};
