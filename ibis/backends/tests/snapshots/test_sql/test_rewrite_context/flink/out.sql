82e0f9baed8cb5461628afc92792c6b2a9d13959
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-07-23 12:13:47 -0400

    test(flink/pyspark): fix tests

--- ibis/backends/tests/snapshots/test_sql/test_rewrite_context/flink/out.sql
+++ ibis/backends/tests/snapshots/test_sql/test_rewrite_context/flink/out.sql
@@ -1,13 +1,4 @@
 SELECT
   NTILE(2) OVER (ORDER BY RAND() ASC NULLS LAST) - 1 AS `new_col`
-FROM (
-  SELECT
-    `test`
-  FROM (VALUES
-    (1),
-    (2),
-    (3),
-    (4),
-    (5)) AS `test`(`test`)
-) AS `t0`
+FROM `test` AS `t0`
 LIMIT 10
