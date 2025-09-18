6c8162b1b77e5d0c665c26854530f90be66ecf03
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-09-17 22:34:42 -0600

    fix(mssql): attempt to support `MERGE` for backend

--- ibis/backends/mssql/__init__.py
+++ ibis/backends/mssql/__init__.py
@@ -424,6 +424,8 @@ class Backend(
         with contextlib.suppress(AttributeError):
             query = query.sql(self.dialect)
 
+        query = f"{query};"
+
         with self.begin() as cur:
             cur.execute(query, *args, **kwargs)
             yield cur
