18f3c4352159deb201e64a1844e015fffaa80024
Daniel Mesejo <mesejoleon@gmail.com>
2025-11-11 21:36:22 +0100

    feat(datafusion): add compilation rule for Strftime

--- ibis/backends/datafusion/tests/test_temporal.py
+++ ibis/backends/datafusion/tests/test_temporal.py
@@ -6,6 +6,10 @@ import pytest
 from pytest import param
 
 import ibis
+from ibis.backends.datafusion.tests.conftest import BackendTest
+
+pd = pytest.importorskip("pandas")
+pa = pytest.importorskip("pyarrow")
 
 
 @pytest.mark.parametrize(
@@ -36,3 +40,30 @@ import ibis
 def test_time_extract_literal(con, func, expected):
     value = ibis.time("14:48:05.359")
     assert con.execute(func(value).name("tmp")) == expected
+
+
+@pytest.mark.parametrize(
+    "pattern",
+    [
+        "%Y-%m-%d",
+        "%Y:%m:%d",
+        "%Y%m%d",
+        "%d-%m-%Y",
+        "%Y-%b-%d",
+        "%Y-%B-%d",
+        "%Y-%B-%d-%a",
+        "%F",
+        "%Y:%m:%d:%H:%M:%S",
+    ],
+)
+def test_strftime(con, pattern):
+    df = pd.DataFrame({"time_col": pa.array([18506, 18507, 18508, 18509], pa.date32())})
+
+    t = ibis.memtable(df)
+
+    name = "formatted"
+    expr = t.time_col.strftime(pattern).name(name)
+    expected = df.time_col.dt.strftime(pattern).rename(name)
+
+    result = con.execute(expr)
+    BackendTest.assert_series_equal(result, expected)
