cd629a59e2d1e09d13eaba2bdf70d42d0742e0a7
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2026-01-15 09:45:17 -0500

    chore: more iteration a bit more concise

--- ibis/backends/sql/compilers/_compat.py
+++ ibis/backends/sql/compilers/_compat.py
@@ -6,9 +6,8 @@ import sqlglot.expressions as sge
 
 
 def _get_arg_name(expr_cls: type[sge.Expression], *args: str) -> str:
-    for arg in args:
-        if arg in expr_cls.arg_types:
-            return arg
+    if (arg := next(filter(expr_cls.arg_types.__contains__, args), None)) is not None:
+        return arg
     raise ValueError(f"None of {args} are valid arguments for {expr_cls.__name__}")
 
 
