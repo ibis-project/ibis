bd48d4c301ddaee921d46a777748d0039abef454
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2026-02-01 10:21:50 -0500

    chore: regen sql formatting snapshots

--- ibis/backends/tests/sql/snapshots/test_sql/test_binop_parens/xor-bool/out.sql
+++ ibis/backends/tests/sql/snapshots/test_sql/test_binop_parens/xor-bool/out.sql
@@ -1,10 +1,60 @@
 SELECT
-  (("t0"."a" AND (NOT "t0"."b")) OR ((NOT "t0"."a") AND "t0"."b") AND (NOT "t0"."c")) OR ((NOT ("t0"."a" AND (NOT "t0"."b")) OR ((NOT "t0"."a") AND "t0"."b")) AND "t0"."c") AS "x"
+  (
+    "t0"."a" XOR "t0"."b" AND (
+      NOT "t0"."c"
+    )
+  )
+  OR (
+    (
+      NOT (
+        (
+          "t0"."a" AND (
+            NOT "t0"."b"
+          )
+        )
+        OR (
+          (
+            NOT "t0"."a"
+          ) AND "t0"."b"
+        )
+      )
+    )
+    AND "t0"."c"
+  ) AS "x"
 FROM "t" AS "t0" --- op(op(a, b), c);
 SELECT
-  ("t0"."a" AND (NOT (
-    ("t0"."b" AND (NOT "t0"."c")) OR ((NOT "t0"."b") AND "t0"."c")
-  ))) OR ((NOT "t0"."a") AND (
-    ("t0"."b" AND (NOT "t0"."c")) OR ((NOT "t0"."b") AND "t0"."c")
-  )) AS "x"
+  (
+    "t0"."a"
+    AND (
+      NOT (
+        (
+          "t0"."b" AND (
+            NOT "t0"."c"
+          )
+        )
+        OR (
+          (
+            NOT "t0"."b"
+          ) AND "t0"."c"
+        )
+      )
+    )
+  )
+  OR (
+    (
+      NOT "t0"."a"
+    )
+    AND (
+      (
+        "t0"."b" AND (
+          NOT "t0"."c"
+        )
+      )
+      OR (
+        (
+          NOT "t0"."b"
+        ) AND "t0"."c"
+      )
+    )
+  ) AS "x"
 FROM "t" AS "t0" --- op(a, op(b, c));
