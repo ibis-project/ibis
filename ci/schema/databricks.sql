5928f0b24cdba411de00bb225be72238456cde9d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2026-02-04 11:19:06 -0500

    ci(databricks): re-enable

--- ci/schema/databricks.sql
+++ ci/schema/databricks.sql
@@ -1,19 +1,19 @@
-CREATE VIEW IF NOT EXISTS `ibis_testing`.`default`.`diamonds` AS
-SELECT * FROM parquet.`/Volumes/ibis_testing/default/{user}_{python_version}/diamonds.parquet`;
+CREATE VIEW IF NOT EXISTS `{workspace}`.`default`.`diamonds` AS
+SELECT * FROM parquet.`/Volumes/workspace/default/{user}_{python_version}/diamonds.parquet`;
 
-CREATE VIEW IF NOT EXISTS `ibis_testing`.`default`.`batting` AS
-SELECT * FROM parquet.`/Volumes/ibis_testing/default/{user}_{python_version}/batting.parquet`;
+CREATE VIEW IF NOT EXISTS `{workspace}`.`default`.`batting` AS
+SELECT * FROM parquet.`/Volumes/workspace/default/{user}_{python_version}/batting.parquet`;
 
-CREATE VIEW IF NOT EXISTS `ibis_testing`.`default`.`awards_players` AS
-SELECT * FROM parquet.`/Volumes/ibis_testing/default/{user}_{python_version}/awards_players.parquet`;
+CREATE VIEW IF NOT EXISTS `{workspace}`.`default`.`awards_players` AS
+SELECT * FROM parquet.`/Volumes/workspace/default/{user}_{python_version}/awards_players.parquet`;
 
-CREATE VIEW IF NOT EXISTS `ibis_testing`.`default`.`functional_alltypes` AS
-SELECT * FROM parquet.`/Volumes/ibis_testing/default/{user}_{python_version}/functional_alltypes.parquet`;
+CREATE VIEW IF NOT EXISTS `{workspace}`.`default`.`functional_alltypes` AS
+SELECT * FROM parquet.`/Volumes/workspace/default/{user}_{python_version}/functional_alltypes.parquet`;
 
-CREATE VIEW IF NOT EXISTS `ibis_testing`.`default`.`astronauts` AS
-SELECT * FROM parquet.`/Volumes/ibis_testing/default/{user}_{python_version}/astronauts.parquet`;
+CREATE VIEW IF NOT EXISTS `{workspace}`.`default`.`astronauts` AS
+SELECT * FROM parquet.`/Volumes/workspace/default/{user}_{python_version}/astronauts.parquet`;
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`array_types` (
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`array_types` (
   `x` ARRAY<BIGINT>,
   `y` ARRAY<STRING>,
   `z` ARRAY<DOUBLE>,
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`array_types` (
   `scalar_column` DOUBLE,
   `multi_dim` ARRAY<ARRAY<BIGINT>>
 );
-DELETE FROM `ibis_testing`.`default`.`array_types`;
-INSERT INTO `ibis_testing`.`default`.`array_types`
+DELETE FROM `{workspace}`.`default`.`array_types`;
+INSERT INTO `{workspace}`.`default`.`array_types`
     VALUES (ARRAY(1, 2, 3), ARRAY('a', 'b', 'c'), ARRAY(1.0, 2.0, 3.0), 'a', 1.0, ARRAY(ARRAY(), ARRAY(1, 2, 3), NULL)),
            (ARRAY(4, 5), ARRAY('d', 'e'), ARRAY(4.0, 5.0), 'a', 2.0, ARRAY()),
            (ARRAY(6, NULL), ARRAY('f', NULL), ARRAY(6.0, NULL), 'a', 3.0, ARRAY(NULL, ARRAY(), NULL)),
@@ -31,11 +31,11 @@ INSERT INTO `ibis_testing`.`default`.`array_types`
            (ARRAY(4, NULL, NULL, 5), ARRAY('d', NULL, NULL, 'e'), ARRAY(4.0, NULL, NULL, 5.0), 'c', 6.0, ARRAY(ARRAY(1, 2, 3)))
         AS (`x`, `y`, `z`, `grouper`, `scalar_column`, `multi_dim`);
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`map` AS
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`map` AS
     VALUES (CAST(1 AS BIGINT), map('a', CAST(1 AS BIGINT), 'b', 2, 'c', 3)),
            (2, map('d', 4, 'e', 5, 'f', 6)) AS (`idx`, `kv`);
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`struct` AS
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`struct` AS
     VALUES (named_struct('a', 1.0, 'b', 'banana', 'c', CAST(2 AS BIGINT))),
            (named_struct('a', 2.0, 'b', 'apple', 'c', 3)),
            (named_struct('a', 3.0, 'b', 'orange', 'c', 4)),
@@ -44,10 +44,10 @@ CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`struct` AS
            (NULL),
            (named_struct('a', 3.0, 'b', 'orange', 'c', NULL)) AS (`abc`);
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`json_t` AS
-    VALUES (CAST(1 AS BIGINT), parse_json('{"a": [1,2,3,4], "b": 1}')),
-           (2, parse_json('{"a":null,"b":2}')),
-           (3, parse_json('{"a":"foo", "c":null}')),
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`json_t` AS
+    VALUES (CAST(1 AS BIGINT), parse_json('{{"a": [1,2,3,4], "b": 1}}')),
+           (2, parse_json('{{"a":null,"b":2}}')),
+           (3, parse_json('{{"a":"foo", "c":null}}')),
            (4, parse_json('null')),
            (5, parse_json('[42,47,55]')),
            (6, parse_json('[]')),
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`json_t` AS
            (13, parse_json('42')),
            (14, parse_json('37.37')) AS (`rowid`, `js`);
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`win` AS
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`win` AS
 VALUES
     ('a', CAST(0 AS BIGINT), CAST(3 AS BIGINT)),
     ('a', 1, 2),
@@ -68,5 +68,5 @@ VALUES
     ('a', 3, 1),
     ('a', 4, 1) AS (`g`, `x`, `y`);
 
-CREATE TABLE IF NOT EXISTS `ibis_testing`.`default`.`topk` AS
+CREATE TABLE IF NOT EXISTS `{workspace}`.`default`.`topk` AS
 VALUES (CAST(1 AS BIGINT)), (1), (NULL) AS (`x`);
