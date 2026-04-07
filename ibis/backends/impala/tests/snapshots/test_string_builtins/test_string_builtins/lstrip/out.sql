798797dfe3366628f360be5de9c54cc3ff7191de
Deepyaman Datta <deepyaman.datta@utexas.edu>
2026-04-07 09:05:16 -0600

    fix(impala/pyspark): use `regexp_replace` to strip

--- ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/lstrip/out.sql
+++ ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/lstrip/out.sql
@@ -1,3 +1,3 @@
 SELECT
-  LTRIM(`t0`.`string_col`, ' \t\n\r\v\f') AS `LStrip(string_col)`
+  REGEXP_REPLACE(`t0`.`string_col`, '^\\s+', '') AS `LStrip(string_col)`
 FROM `functional_alltypes` AS `t0`
--- ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/strip/out.sql
+++ ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/strip/out.sql
@@ -1,3 +1,3 @@
 SELECT
-  RTRIM(LTRIM(`t0`.`string_col`, ' \t\n\r\v\f'), ' \t\n\r\v\f') AS `Strip(string_col)`
+  REGEXP_REPLACE(`t0`.`string_col`, '^\\s+|\\s+$', '') AS `Strip(string_col)`
 FROM `functional_alltypes` AS `t0`
