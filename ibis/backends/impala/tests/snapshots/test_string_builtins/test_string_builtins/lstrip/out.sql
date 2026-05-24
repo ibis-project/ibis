163f52a1754896b41beb73e1afc85f1b0bda61ad
Deepyaman Datta <deepyaman.datta@utexas.edu>
2026-05-24 14:37:14 -0600

    revert(impala): use trim operations to strip again

--- ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/lstrip/out.sql
+++ ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/lstrip/out.sql
@@ -1,3 +1,3 @@
 SELECT
-  REGEXP_REPLACE(`t0`.`string_col`, '^\\s+', '') AS `LStrip(string_col)`
+  LTRIM(`t0`.`string_col`, ' \t\n\r\v\f') AS `LStrip(string_col)`
 FROM `functional_alltypes` AS `t0`
