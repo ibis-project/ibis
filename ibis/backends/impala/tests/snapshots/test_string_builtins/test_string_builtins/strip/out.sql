2f5ee78438f57b80eb74c1b635fa224f063a0dbb
Deepyaman Datta <deepyaman.datta@utexas.edu>
2026-05-24 17:44:14 -0600

    test(impala): update snapshots for `STRIP` methods

--- ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/strip/out.sql
+++ ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/strip/out.sql
@@ -1,3 +1,6 @@
 SELECT
-  RTRIM(LTRIM(`t0`.`string_col`, ' \t\n\r\v\f'), ' \t\n\r\v\f') AS `Strip(string_col)`
+  RTRIM(
+    LTRIM(`t0`.`string_col`, CONCAT(' \t\n\r', CHR(  11), CHR(  12))),
+    CONCAT(' \t\n\r', CHR(  11 ), CHR(  12))
+  ) AS `Strip(string_col)`
 FROM `functional_alltypes` AS `t0`
